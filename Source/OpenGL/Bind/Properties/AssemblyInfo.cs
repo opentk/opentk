3ff2b848e25b905673efc239a0e317ef54b45db0
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-04-20 19:03:11 +0000

    Synced with latest Tao.GlBindGen source. OpenTK.OpenGL.Bind now uses CodeDOM internally for code generation. Many bugfixes. Supports newest specs.

--- Source/OpenGL/Bind/Properties/AssemblyInfo.cs
+++ Source/OpenGL/Bind/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.7.0")]
-[assembly: AssemblyFileVersion("0.7.7.0")]
+[assembly: AssemblyVersion("0.9.0.3")]
+[assembly: AssemblyFileVersion("0.9.0.3")]
