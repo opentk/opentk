43a37e3ac2b73d233d5fa997833eccb02e2cddf4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-07-27 01:37:12 +0000

    Fixed Raw Input keyboard registration under windows. Fixed marshalling in GetRawInputData.

--- Source/OpenTK/Input/IKeyboard.cs
+++ Source/OpenTK/Input/IKeyboard.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿#region --- License ---
+/* Copyright (c) 2007 Stefanos Apostolopoulos
+ * See license.txt for license info
+ */
+#endregion
 
 namespace OpenTK.Input
 {
