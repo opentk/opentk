6898d88a96f7ea2c1227e5c0279e406a9408d79e
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-08-04 23:46:06 +0000

    Synced with gl3 branch (added Wgl Generator.cs). Bind now builds.

---
+++ Source/Bind/Wgl/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bind.Wgl
+{
+    class Generator : Bind.GL2.Generator
+    {
+        public Generator(string path)
+            : base(path)
+        {
+            glTypemap = "wgl.tm";
+            csTypemap = "..\\gl2\\csharp.tm";
+            enumSpec = "wglenum.spec";
+            enumSpecExt = "wglenumext.spec";
+            glSpec = "wgl.spec";
+            glSpecExt = "wglext.spec";
+        }
+
+        public override void Process()
+        {
+            base.Process();
+        }
+    }
+}
