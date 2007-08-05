13c04f4bbd36cf3e79da898f7adc0b0f6bced977
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-08-05 18:26:14 +0000

    Hooked up Keyboard event handling in X11Input Added ProcessEvents() function to IInputDriver. Does nothing on WinRawInput, but is needed by X11Input

--- Source/OpenTK/Input/IInputDriver.cs
+++ Source/OpenTK/Input/IInputDriver.cs
@@ -7,7 +7,7 @@ namespace OpenTK.Input
     public interface IInputDriver : IKeyboardDriver, IMouseDriver
     {
         IList<IInputDevice> InputDevices { get; }
-        //void ProcessEvents
+        void ProcessEvents();
         //IEnumerable<IMouse> Mice { get; }
         //IEnumerable<IHid> Hids { get; }
     }
