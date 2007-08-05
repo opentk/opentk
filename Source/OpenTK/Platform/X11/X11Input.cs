e34ee87e4e943b41b63ce95d8c890647a9f3e5dd
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-08-05 09:03:22 +0000

    Started implementing X11Input.cs, X11Keyboard.cs and X11Mouse.cs drivers. Removed some warnings from X11Api.cs

---
+++ Source/OpenTK/Platform/X11/X11Input.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTK.Input;
+
+namespace OpenTK.Platform.X11
+{
+    public class X11Input : IInputDriver
+    {
+        public X11Input(IntPtr windowHandle)
+        {
+        }
+
+        #region --- IInputDriver Members ---
+
+        #region public IList<IInputDevice> InputDevices
+
+        public IList<IInputDevice> InputDevices
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        #endregion
+
+        #region public IList<Keyboard> Keyboard
+
+        public IList<Keyboard> Keyboard
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        #endregion
+
+        #region public IList<Mouse> Mouse
+
+        public IList<Mouse> Mouse
+        {
+            get { throw new Exception("The method or operation is not implemented."); }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
