702eada6284a6dbf126338c5ade65fc96f344a53
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-08-11 06:47:07 +0000

    Bumped version number. Updated changelog and todo items.

--- Source/OpenTK/Properties/AssemblyInfo.cs
+++ Source/OpenTK/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OpenTK")]
-[assembly: AssemblyCopyright("Copyright ©  2006-2007 Stefanos Apostolopoulos")]
+[assembly: AssemblyCopyright("Copyright �  2006-2007 Stefanos Apostolopoulos")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.3.9.6")]
-[assembly: AssemblyFileVersion("0.3.9.6")]
+[assembly: AssemblyVersion("0.3.9.7")]
+[assembly: AssemblyFileVersion("0.3.9.7")]
