7403987e5c9d1e4faa1ec7351603f2fcacc6ed4a
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-02 08:09:01 +0000

    Added DummyGLContext.cs amd DummyGLControl.cs drivers.

---
+++ Source/OpenTK/Platform/DummyGLContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Platform
+{
+    /// <summary>
+    /// A dummy GLContext to be used inside the Visual Studio designer.
+    /// </summary>
+    internal class DummyGLContext : IGLContext
+    {
+        #region --- IGLContext Members ---
+
+        public void SwapBuffers()
+        {
+        }
+
+        public void MakeCurrent()
+        {
+        }
+
+        public IntPtr GetAddress(string function)
+        {
+            return IntPtr.Zero;
+        }
+
+        public IEnumerable<DisplayMode> GetDisplayModes()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        #endregion
+
+        #region --- IDisposable Members ---
+
+        public void Dispose()
+        {
+        }
+
+        #endregion
+    }
+}
