6fb1fe1e1bca0f7b8f23310ef0e698cadec2d066
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-02 13:34:44 +0000

    Updated T10_GLSL_Cube to remove NRE exception on Mono 1.2.x. Added debug info to the GL.LoadAll function. Added W03_Extensions.cs example.

--- Source/Examples/WinForms/W03_Extensions.Designer.cs
+++ Source/Examples/WinForms/W03_Extensions.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Examples
+﻿namespace Examples.WinForms
 {
-    partial class ExampleLauncher
+    partial class W03_Extensions
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.runButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -37,37 +36,18 @@
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(1);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(284, 264);
             this.listBox1.TabIndex = 0;
-            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
-            this.listBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyUp);
             // 
-            // runButton
-            // 
-            this.runButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.runButton.Location = new System.Drawing.Point(0, 220);
-            this.runButton.Margin = new System.Windows.Forms.Padding(1);
-            this.runButton.Name = "runButton";
-            this.runButton.Size = new System.Drawing.Size(284, 44);
-            this.runButton.TabIndex = 0;
-            this.runButton.Text = "Run Example";
-            this.runButton.UseVisualStyleBackColor = true;
-            this.runButton.Click += new System.EventHandler(this.runButton_Click);
-            // 
-            // ExampleLauncher
+            // W03_Extensions
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 264);
-            this.Controls.Add(this.runButton);
             this.Controls.Add(this.listBox1);
-            this.MinimumSize = new System.Drawing.Size(300, 300);
-            this.Name = "ExampleLauncher";
-            this.Text = "OpenTK Example Launcher";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExampleLauncher_FormClosing);
-            this.Load += new System.EventHandler(this.ExampleLauncher_Load);
+            this.Name = "W03_Extensions";
+            this.Text = "W03_Extensions";
             this.ResumeLayout(false);
 
         }
@@ -75,7 +55,5 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button runButton;
-
     }
 }
