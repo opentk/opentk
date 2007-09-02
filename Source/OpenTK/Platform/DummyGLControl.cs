8c66c39a808dbe647e531bcc75185818f32af229
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-02 08:09:01 +0000

    Added DummyGLContext.cs amd DummyGLControl.cs drivers.

---
+++ Source/OpenTK/Platform/DummyGLControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Platform
+{
+    /// <summary>
+    /// A dummy GLControl to be used inside the Visual Studio designer.
+    /// </summary>
+    internal class DummyGLControl : IGLControl
+    {
+        bool fullscreen;
+        IGLContext glContext = new DummyGLContext();
+
+        #region --- IGLControl Members ---
+
+        public bool IsIdle
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public bool Fullscreen
+        {
+            get
+            {
+                return fullscreen;
+            }
+            set
+            {
+                fullscreen = value;
+            }
+        }
+
+        public IGLContext Context
+        {
+            get
+            {
+                return glContext;
+            }
+        }
+
+        #endregion
+    }
+}
