09ca1b6b0a365ecc2e3f6741893b4a9de69d8a55
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-02 07:50:46 +0000

    Added Glu and Glx generators to Bind.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@391 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Bind/Glx/Generator.cs
+++ Source/Bind/Glx/Generator.cs
@@ -1,10 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace Bind.Glx
 {
-    class Generator
+    class Generator : Bind.GL2.Generator
     {
+        #region --- Constructors ---
+
+        public Generator()
+            : base()
+        {
+            glTypemap = "Glx\\glx.tm";
+            csTypemap = "csharp.tm";
+            enumSpec = "Glx\\glxenum.spec";
+            enumSpecExt = "Glx\\glxenumext.spec";
+            glSpec = "Glx\\glx.spec";
+            glSpecExt = "Glx\\glxext.spec";
+
+            importsFile = "GlxCore.cs";
+            delegatesFile = "GlxDelegates.cs";
+            enumsFile = "GlxEnums.cs";
+            wrappersFile = "Glx.cs";
+
+            Settings.OutputClass = "Glx";
+            Settings.FunctionPrefix = "glX";
+            Settings.ConstantPrefix = "GLX_";
+
+
+            if (Settings.Compatibility == Settings.Legacy.Tao)
+            {
+                Settings.OutputNamespace = "Tao.Platform.Glx";
+                //Settings.WindowsGDI = "Tao.Platform.Windows.Gdi";
+            }
+            else
+            {
+                Settings.OutputNamespace = "OpenTK.Platform.X11";
+            }
+        }
+
+        #endregion
+
+        public override void Process()
+        {
+            Bind.Structures.Type.Initialize(glTypemap, csTypemap);
+            Bind.Structures.Enum.Initialize(enumSpec, enumSpecExt);
+            Bind.Structures.Function.Initialize();
+            Bind.Structures.Delegate.Initialize(glSpec, glSpecExt);
+
+            // Process enums and delegates - create wrappers.
+            Trace.WriteLine("Processing specs, please wait...");
+            this.Translate();
+
+            this.WriteBindings(
+                Bind.Structures.Delegate.Delegates,
+                Bind.Structures.Function.Wrappers,
+                Bind.Structures.Enum.GLEnums);
+        }
     }
 }
