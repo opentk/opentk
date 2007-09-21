791055b569337377b300eacc1664d39e45693cd2
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-21 23:38:19 +0000

    Added initial OpenTK.Math implementation. Contains Vector2, Vector3 and Vector4 structs.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@436 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/OpenTK/Math/Vector2.cs
@@ -0,0 +1,161 @@
+﻿#region --- License ---
+/* Copyright (c) 2006, 2007 Stefanos Apostolopoulos
+ * See license.txt for license info
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OpenTK.Math
+{
+    /// <summary>
+    /// Represents a 2D vector.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vector2
+    {
+        /// <summary>
+        /// The X coordinate of the Vector2.
+        /// </summary>
+        public float X;
+            
+        /// <summary>
+        /// The Y coordinate of the Vector2.
+        /// </summary>
+        public float Y;
+
+        /// <summary>
+        /// Constructs a new Vector2.
+        /// </summary>
+        /// <param name="x">The x coordinate of the net Vector2.</param>
+        /// <param name="y">The y coordinate of the net Vector2.</param>
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Adds the given Vector2 to the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the addition.</param>
+        /// <returns>A new Vector2 containing the result of the addition.</returns>
+        public Vector2 Add(Vector2 right)
+        {
+            return new Vector2(X + right.X, Y + right.Y);
+        }
+
+        /// <summary>
+        /// Adds the given Vector3 to the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the addition.</param>
+        /// <returns>A new Vector3 containing the result of the addition.</returns>
+        public Vector3 Add(Vector3 right)
+        {
+            return new Vector3(X + right.X, Y + right.Y, right.Z);
+        }
+
+        /// <summary>
+        /// Adds the given Vector4 to the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the addition.</param>
+        /// <returns>A new Vector4 containing the result of the addition.</returns>
+        public Vector4 Add(Vector4 right)
+        {
+            return new Vector4(X + right.X, Y + right.Y, right.Z, right.W);
+        }
+
+        /// <summary>
+        /// Subtracts the given Vector2 from the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the subtraction.</param>
+        /// <returns>A new Vector2 containing the result of the subtraction.</returns>
+        public Vector2 Sub(Vector2 right)
+        {
+            return new Vector2(X - right.X, Y - right.Y);
+        }
+
+        /// <summary>
+        /// Subtracts the given Vector3 from the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the subtraction.</param>
+        /// <returns>A new Vector3 containing the result of the subtraction.</returns>
+        public Vector3 Sub(Vector3 right)
+        {
+            return new Vector3(X - right.X, Y - right.Y, -right.Z);
+        }
+
+        /// <summary>
+        /// Subtracts the given Vector4 from the current Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the subtraction.</param>
+        /// <returns>A new Vector4 containing the result of the subtraction.</returns>
+        public Vector4 Sub(Vector4 right)
+        {
+            return new Vector4(X - right.X, Y - right.Y, -right.Z, -right.W);
+        }
+
+        /// <summary>
+        /// Computes the dot product between the curren Vector2 and the given Vector2.
+        /// </summary>
+        /// <param name="right">The right operand of the dot product.</param>
+        /// <returns>A float containing the result of the dot product.</returns>
+        public float Dot(Vector2 right)
+        {
+            return X * right.X + Y * right.Y;
+        }
+
+        /// <summary>
+        /// Computes the dot product between the curren Vector2 and the given Vector3.
+        /// </summary>
+        /// <param name="right">The right operand of the dot product.</param>
+        /// <returns>A float containing the result of the dot product.</returns>
+        public float Dot(Vector3 right)
+        {
+            return X * right.X + Y * right.Y;
+        }
+
+        /// <summary>
+        /// Computes the dot product between the curren Vector2 and the given Vector4.
+        /// </summary>
+        /// <param name="right">The right operand of the dot product.</param>
+        /// <returns>A float containing the result of the dot product.</returns>
+        public float Dot(Vector4 right)
+        {
+            return X * right.X + Y * right.Y;
+        }
+
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return left.Add(right);
+        }
+
+        public static Vector3 operator +(Vector2 left, Vector3 right)
+        {
+            return left.Add(right);
+        }
+
+        public static Vector4 operator +(Vector2 left, Vector4 right)
+        {
+            return left.Add(right);
+        }
+
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return left.Sub(right);
+        }
+
+        public static Vector3 operator -(Vector2 left, Vector3 right)
+        {
+            return left.Sub(right);
+        }
+
+        public static Vector4 operator -(Vector2 left, Vector4 right)
+        {
+            return left.Sub(right);
+        }
+    }
+}
