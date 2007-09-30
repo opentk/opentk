5cdf9a75895238763accb159a8fe78bea8a527b4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-09-30 12:34:20 +0000

    Fixed memory corruption issue with (Cube's vertices were being moved by the GC). I think.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@506 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Examples/Shapes/Cube.cs
+++ Source/Examples/Shapes/Cube.cs
@@ -10,61 +10,65 @@ using System.Text;
 using System.Drawing;
 
 using OpenTK.Math;
+using System.Runtime.InteropServices;
 
 namespace Examples.Shapes
 {
-    public static class Cube
+    public class Cube : Shape
     {
-        public static readonly Vector3[] Vertices = new Vector3[8]
+        public Cube()
         {
-            new Vector3(-1.0f, -1.0f,  1.0f),
-            new Vector3( 1.0f, -1.0f,  1.0f),
-            new Vector3( 1.0f,  1.0f,  1.0f),
-            new Vector3(-1.0f,  1.0f,  1.0f),
-            new Vector3(-1.0f, -1.0f, -1.0f),
-            new Vector3( 1.0f, -1.0f, -1.0f), 
-            new Vector3( 1.0f,  1.0f, -1.0f),
-            new Vector3(-1.0f,  1.0f, -1.0f),
-        };
+            Vertices = new Vector3[]
+            {
+                new Vector3(-1.0f, -1.0f,  1.0f),
+                new Vector3( 1.0f, -1.0f,  1.0f),
+                new Vector3( 1.0f,  1.0f,  1.0f),
+                new Vector3(-1.0f,  1.0f,  1.0f),
+                new Vector3(-1.0f, -1.0f, -1.0f),
+                new Vector3( 1.0f, -1.0f, -1.0f), 
+                new Vector3( 1.0f,  1.0f, -1.0f),
+                new Vector3(-1.0f,  1.0f, -1.0f)
+            };
 
-        public static readonly ushort[] Indices =
-        {
-            // front face
-            0, 1, 2, 2, 3, 0,
-            // top face
-            3, 2, 6, 6, 7, 3,
-            // back face
-            7, 6, 5, 5, 4, 7,
-            // left face
-            4, 0, 3, 3, 7, 4,
-            // bottom face
-            0, 1, 5, 5, 4, 0,
-            // right face
-            1, 5, 6, 6, 2, 1,
-        };
+            Indices = new int[]
+            {
+                // front face
+                0, 1, 2, 2, 3, 0,
+                // top face
+                3, 2, 6, 6, 7, 3,
+                // back face
+                7, 6, 5, 5, 4, 7,
+                // left face
+                4, 0, 3, 3, 7, 4,
+                // bottom face
+                0, 1, 5, 5, 4, 0,
+                // right face
+                1, 5, 6, 6, 2, 1,
+            };
 
-        public static readonly Vector3[] Normals =
-        {
-            new Vector3(-1.0f, -1.0f,  1.0f),
-            new Vector3( 1.0f, -1.0f,  1.0f),
-            new Vector3( 1.0f,  1.0f,  1.0f),
-            new Vector3(-1.0f,  1.0f,  1.0f),
-            new Vector3(-1.0f, -1.0f, -1.0f),
-            new Vector3( 1.0f, -1.0f, -1.0f),
-            new Vector3( 1.0f,  1.0f, -1.0f),
-            new Vector3(-1.0f,  1.0f, -1.0f),
-        };
+            Normals = new Vector3[]
+            {
+                new Vector3(-1.0f, -1.0f,  1.0f),
+                new Vector3( 1.0f, -1.0f,  1.0f),
+                new Vector3( 1.0f,  1.0f,  1.0f),
+                new Vector3(-1.0f,  1.0f,  1.0f),
+                new Vector3(-1.0f, -1.0f, -1.0f),
+                new Vector3( 1.0f, -1.0f, -1.0f),
+                new Vector3( 1.0f,  1.0f, -1.0f),
+                new Vector3(-1.0f,  1.0f, -1.0f),
+            };
 
-        public static readonly int[] Colors =
-        {
-            Color.Firebrick.ToArgb(),
-            Color.Honeydew.ToArgb(),
-            Color.Moccasin.ToArgb(),
-            Color.Yellow.ToArgb(),
-            Color.Crimson.ToArgb(),
-            Color.DarkGoldenrod.ToArgb(),
-            Color.ForestGreen.ToArgb(),
-            Color.Sienna.ToArgb(),
-        };
+            Colors = new int[]
+            {
+                Color.Firebrick.ToArgb(),
+                Color.Honeydew.ToArgb(),
+                Color.Moccasin.ToArgb(),
+                Color.Yellow.ToArgb(),
+                Color.Crimson.ToArgb(),
+                Color.DarkGoldenrod.ToArgb(),
+                Color.ForestGreen.ToArgb(),
+                Color.Sienna.ToArgb(),
+            };
+        }
     }
 }
