da96b91cd6a45becb5b5eb0ef4f42ed64239f5ac
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-10-15 11:03:57 +0000

    Added Exceptions.cs which contains OpenTK exceptions.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@517 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/OpenTK/Exceptions.cs
@@ -0,0 +1,36 @@
+#region --- License ---
+/* Copyright (c) 2006, 2007 Stefanos Apostolopoulos
+ * See license.txt for license info
+ */
+#endregion
+
+using System;
+
+namespace OpenTK
+{
+
+    #region public class ContextExistsException : ApplicationException
+
+    /// <summary>
+    /// This exception is thrown when a GLContext property cannot be changed after creation.
+    /// </summary>
+    public class ContextExistsException : ApplicationException
+    {
+        string msg;
+
+        public ContextExistsException(string message)
+        {
+            msg = message;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return msg;
+            }
+        }
+    }
+
+    #endregion
+}
