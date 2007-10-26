720e846f0f6f8e39ae7a9422aec2a4b3a4fb7704
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-10-26 15:55:24 +0000

    Updated font rendering code.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@575 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Examples/Tutorial/Fonts.cs
+++ Source/Examples/Tutorial/Fonts.cs
@@ -24,7 +24,7 @@ namespace Examples.Tutorial
             this.VSync = VSyncMode.On;
         }
 
-        TextureFont serif;
+        TextureFont serif = new TextureFont(new Font(FontFamily.GenericSerif, 16.0f));
 
         string[] poem = new StreamReader("Data/Poem.txt").ReadToEnd().Replace('\r', ' ').Split('\n');
         float scroll_speed;
@@ -33,24 +33,56 @@ namespace Examples.Tutorial
         float warparound_position;
         float current_position;
 
+        int display_list;
+
         public override void OnLoad(EventArgs e)
         {
             GL.Enable(GL.Enums.EnableCap.TEXTURE_2D);
-            serif = new TextureFont(new Font(FontFamily.GenericSerif, 24, FontStyle.Regular, GraphicsUnit.Pixel));
-            serif.LoadGlyphs("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz,.!?");
+            serif.LoadGlyphs("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz,.!?;()\'- ");
 
-            scroll_speed = 4/(float)Height;
-            initial_position = -1.0f - 64 / (float)Height;  // 64 pixels below the bottom of the screen;
-            warparound_position = 7.0f;
             current_position = initial_position;
+            scroll_speed = -1.0f;
+
+            display_list = GL.GenLists(1);
+            GL.NewList(1, GL.Enums.ListMode.COMPILE);
+
+            GL.PushMatrix();
+
+            GL.MatrixMode(GL.Enums.MatrixMode.PROJECTION);
+            GL.LoadIdentity();
+            GL.Ortho(0.0, Width, Height, 0.0, 0.0, 1.0);
+            GL.MatrixMode(GL.Enums.MatrixMode.MODELVIEW);
+            GL.LoadIdentity();
+            
+            int i = 0, line = 0;
+            float x_pos, accum_x_pos = 0.0f;
+            foreach (string str in poem)
+            {
+                GL.Translate(0.0f, serif.LineSpacing * line, 0.0f);
+                foreach (char c in str)
+                {
+                    serif.PrintFast(c);
+                    x_pos = serif.MeasureWidth(str.Substring(i++, 1));
+                    accum_x_pos += x_pos;
+
+                    GL.Translate((int)(x_pos + 0.5f), 0.0f, 0.0f);
+                }
+                GL.LoadIdentity();
+                i = 0;
+                ++line;
+            }
+
+            GL.PopMatrix();
+
+            GL.EndList();
         }
 
         protected override void OnResize(OpenTK.Platform.ResizeEventArgs e)
         {
             GL.Viewport(0, 0, Width, Height);
-            GL.MatrixMode(GL.Enums.MatrixMode.PROJECTION);
-            GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 16.0);
+
+            initial_position = Height + serif.LineSpacing;
+            warparound_position = -(poem.Length + 1) * serif.LineSpacing;
         }
 
         public override void OnUpdateFrame(UpdateFrameEventArgs e)
@@ -58,38 +90,29 @@ namespace Examples.Tutorial
             if (Keyboard[Key.Space])
                 scroll_speed = 0.0f;
             if (Keyboard[Key.Down])
-                scroll_speed -= 1 / (float)Height;
+                scroll_speed += 1;
             if (Keyboard[Key.Up])
-                scroll_speed += 1 / (float)Height;
+                scroll_speed -= 1;
             if (Keyboard[Key.Escape])
                 this.Exit();
         }
 
         public override void OnRenderFrame(RenderFrameEventArgs e)
         {
-            GL.MatrixMode(GL.Enums.MatrixMode.MODELVIEW);
-            GL.LoadIdentity();
-
             // We'll start printing from the lower left corner of the screen. The text
             // will slowly move updwards - the user can control the movement speed with
             // the keyboard arrows and the space bar.
             current_position += scroll_speed * (float)e.ScaleFactor;
-            if (current_position > 0.0f && current_position > warparound_position)
-                current_position = initial_position;
-            else if (current_position < 0.0f && current_position < initial_position)
+            if (scroll_speed > 0.0f && current_position > initial_position)
                 current_position = warparound_position;
-            scroll_position = ((int)(current_position * (float)Height)) / (float)Height;    // Round to closest pixel.
-
-            GL.Translate(-1.0f, scroll_position, 0.0f);
+            else if (scroll_speed < 0.0f && current_position < warparound_position)
+                current_position = initial_position;
+            scroll_position = current_position;
 
             GL.Clear(GL.Enums.ClearBufferMask.COLOR_BUFFER_BIT);
-            
-            float line_spacing = -2.0f * serif.LineSpacing / (float)Height;
-            foreach (string line in poem)
-            {
-                serif.Print(line);
-                GL.Translate(0.0f, line_spacing, 0.0f);   // Move to the next line.
-            }
+
+            //GL.Translate(0.0f, scroll_position, 0.0f);
+            GL.CallList(display_list);
 
             SwapBuffers();
         }
@@ -99,7 +122,7 @@ namespace Examples.Tutorial
 
         public void Launch() 
         {
-            Run(30.0, 85.0);
+            Run(30.0, 0.0);
         }
 
         public static readonly int order = 6;
