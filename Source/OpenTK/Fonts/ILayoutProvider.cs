38a6f8f8ead73e20d05dce0395518e275be1699c
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-01 23:23:19 +0000

    Added ILayoutProvider class.

---
+++ Source/OpenTK/Fonts/ILayoutProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace OpenTK.Fonts
+{
+    interface ILayoutProvider
+    {
+        void PerformLayout(string text, IFont font, RectangleF layoutRect, StringAlignment alignment, bool rightToLeft);
+    }
+}
