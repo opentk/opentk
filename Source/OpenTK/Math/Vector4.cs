f96b2e4fbcd6539da02a182015af201d7145d707
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-04 15:34:04 +0000

    New math classes by george.

--- Source/OpenTK/Math/Vector4.cs
+++ Source/OpenTK/Math/Vector4.cs
@@ -1,5 +1,6 @@
 ﻿#region --- License ---
 /* Copyright (c) 2006, 2007 Stefanos Apostolopoulos
+ * Contributions by Andy Gill.
  * See license.txt for license info
  */
 #endregion
@@ -38,6 +39,12 @@ namespace OpenTK.Math
         /// The Z component of the Vector4.
         /// </summary>
         public float W;
+
+		public static Vector4 UnitX = new Vector4(1, 0, 0, 0);
+		public static Vector4 UnitY = new Vector4(0, 1, 0, 0);
+		public static Vector4 UnitZ = new Vector4(0, 0, 1, 0);
+		public static Vector4 UnitW = new Vector4(0, 0, 0, 1);
+		public static Vector4 Zero = new Vector4(0, 0, 0, 0);
 
         #endregion
 
@@ -97,56 +104,6 @@ namespace OpenTK.Math
         #endregion
 
         #region Functions
-
-        #region public Vector4 Add(Vector4 right)
-
-        /// <summary>
-        /// Adds the given Vector4 to the current Vector4. W-coordinate remains unaffected.
-        /// </summary>
-        /// <param name="right">The right operand of the addition.</param>
-        /// <returns>A new Vector4 containing the result of the addition.</returns>
-        public Vector4 Add(Vector4 right)
-        {
-            X += right.X;
-            Y += right.Y;
-            Z += right.Z;
-            W += right.W;
-            return this;
-        }
-
-        #endregion
-
-        #region public Vector4 Sub(Vector4 right)
-
-        /// <summary>
-        /// Subtracts the given Vector4 from the current Vector4.
-        /// </summary>
-        /// <param name="right">The right operand of the subtraction.</param>
-        /// <returns>A new Vector4 containing the result of the subtraction.</returns>
-        public Vector4 Sub(Vector4 right)
-        {
-            X -= right.X;
-            Y -= right.Y;
-            Z -= right.Z;
-            W -= right.W;
-            return this;
-        }
-
-        #endregion
-
-        #region public float Dot(Vector4 right)
-
-        /// <summary>
-        /// Computes the dot product between the current Vector4 and the given Vector4.
-        /// </summary>
-        /// <param name="right">The right operand of the dot product.</param>
-        /// <returns>A float containing the result of the dot product.</returns>
-        public float Dot(Vector4 right)
-        {
-            return X * right.X + Y * right.Y + Z * right.Z + W * right.W;
-        }
-
-        #endregion
 
         #region public float Length
 
@@ -208,43 +165,39 @@ namespace OpenTK.Math
 
         #endregion
 
-        #region public Vector4 Normalize()
+        #region public void Normalize()
 
         /// <summary>
         /// Scales the Vector4 to unit length.
         /// </summary>
-        /// <returns>The normalized version of the current vector.</returns>
-        public Vector4 Normalize()
+        public void Normalize()
         {
             float scale = 1.0f / this.Length;
             X *= scale;
             Y *= scale;
             Z *= scale;
             W *= scale;
-            return this;
         }
 
         #endregion
 
-        #region public Vector4 NormalizeFast()
+        #region public void NormalizeFast()
 
         /// <summary>
         /// Scales the Vector4 to approximately unit length.
         /// </summary>
-        /// <returns>The normalized version of the current vector.</returns>
-        public Vector4 NormalizeFast()
+        public void NormalizeFast()
         {
-            float scale = Functions.InverseSqrtFast(X * X + Y * Y + Z * Z);
+            float scale = Functions.InverseSqrtFast(X * X + Y * Y + Z * Z + W * W);
             X *= scale;
             Y *= scale;
             Z *= scale;
             W *= scale;
-            return this;
         }
 
         #endregion
 
-        #region public Vector4 Scale(float sx, float sy, float sz, float sw)
+        #region public void Scale(float sx, float sy, float sz, float sw)
 
         /// <summary>
         /// Scales the current Vector4 by the given amounts.
@@ -253,11 +206,13 @@ namespace OpenTK.Math
         /// <param name="sy">The scale of the Y component.</param>
         /// <param name="sz">The scale of the Z component.</param>
         /// <param name="sw">The scale of the Z component.</param>
-        /// <returns>The current Vector4, scaled.</returns>
-        public Vector4 Scale(float sx, float sy, float sz, float sw)
+        public void Scale(float sx, float sy, float sz, float sw)
         {
-            return new Vector4(X * sx, Y * sy, Z * sz, W * sw);
-        }
+            this.X = X * sx;
+            this.Y = Y * sy;
+            this.Z = Z * sz;
+            this.W = W * sw;
+       }
 
         #endregion
 
@@ -267,13 +222,58 @@ namespace OpenTK.Math
 
         public static Vector4 operator +(Vector4 left, Vector4 right)
         {
-            return left.Add(right);
+			left.X += right.X;
+			left.Y += right.Y;
+			left.Z += right.Z;
+			left.W += right.W;
+			return left;
         }
 
         public static Vector4 operator -(Vector4 left, Vector4 right)
         {
-            return left.Sub(right);
+			left.X -= right.X;
+			left.Y -= right.Y;
+			left.Z -= right.Z;
+			left.W -= right.W;
+			return left;
         }
+
+		public static Vector4 operator -(Vector4 vec)
+		{
+			vec.X = -vec.X;
+			vec.Y = -vec.Y;
+			vec.Z = -vec.Z;
+			vec.W = -vec.W;
+			return vec;
+		}
+
+		public static Vector4 operator *(Vector4 vec, float f)
+		{
+			vec.X *= f;
+			vec.Y *= f;
+			vec.Z *= f;
+			vec.W *= f;
+			return vec;
+		}
+
+		public static Vector4 operator *(float f, Vector4 vec)
+		{
+			vec.X *= f;
+			vec.Y *= f;
+			vec.Z *= f;
+			vec.W *= f;
+			return vec;
+		}
+
+		public static Vector4 operator /(Vector4 vec, float f)
+		{
+			float mult = 1.0f / f;
+			vec.X *= mult;
+			vec.Y *= mult;
+			vec.Z *= mult;
+			vec.W *= mult;
+			return vec;
+		}
 
         [CLSCompliant(false)]
         unsafe public static explicit operator float*(Vector4 v)
@@ -289,12 +289,397 @@ namespace OpenTK.Math
             }
         }
 
-
         #endregion
 
-        public override string ToString()
+		#region Static functions
+
+		#region Add
+
+		/// <summary>
+		/// Add two Vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <returns>Result of addition</returns>
+		public static Vector4 Add(Vector4 a, Vector4 b)
+		{
+			a.X += b.X;
+			a.Y += b.Y;
+			a.Z += b.Z;
+			a.W += b.W;
+			return a;
+		}
+	
+		/// <summary>
+		/// Add two Vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <param name="result">Result of addition</param>
+		public static void Add(ref Vector4 a, ref Vector4 b, out Vector4 result)
+		{
+			result.X = a.X + b.X;
+			result.Y = a.Y + b.Y;
+			result.Z = a.Z + b.Z;
+			result.W = a.W + b.W;
+		}
+
+		#endregion
+
+		#region Sub
+
+		/// <summary>
+		/// Subtract one Vector from another
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <returns>Result of subtraction</returns>
+		public static Vector4 Sub(Vector4 a, Vector4 b)
+		{
+			a.X -= b.X;
+			a.Y -= b.Y;
+			a.Z -= b.Z;
+			a.W -= b.W;
+			return a;
+		}
+
+		/// <summary>
+		/// Subtract one Vector from another
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <param name="result">Result of subtraction</param>
+		public static void Sub(ref Vector4 a, ref Vector4 b, out Vector4 result)
+		{
+			result.X = a.X - b.X;
+			result.Y = a.Y - b.Y;
+			result.Z = a.Z - b.Z;
+			result.W = a.W - b.W;
+		}
+
+		#endregion
+
+		#region Mult
+
+		/// <summary>
+		/// Multiply a vector and a scalar
+		/// </summary>
+		/// <param name="a">Vector operand</param>
+		/// <param name="f">Scalar operand</param>
+		/// <returns>Result of the multiplication</returns>
+		public static Vector4 Mult(Vector4 a, float f)
+		{
+			a.X *= f;
+			a.Y *= f;
+			a.Z *= f;
+			a.W *= f;
+			return a;
+		}
+
+		/// <summary>
+		/// Multiply a vector and a scalar
+		/// </summary>
+		/// <param name="a">Vector operand</param>
+		/// <param name="f">Scalar operand</param>
+		/// <param name="result">Result of the multiplication</param>
+		public static void Mult(ref Vector4 a, float f, out Vector4 result)
+		{
+			result.X = a.X * f;
+			result.Y = a.Y * f;
+			result.Z = a.Z * f;
+			result.W = a.W * f;
+		}
+
+		#endregion
+
+		#region Div
+
+		/// <summary>
+		/// Divide a vector by a scalar
+		/// </summary>
+		/// <param name="a">Vector operand</param>
+		/// <param name="f">Scalar operand</param>
+		/// <returns>Result of the division</returns>
+		public static Vector4 Div(Vector4 a, float f)
+		{
+			float mult = 1.0f / f;
+			a.X *= mult;
+			a.Y *= mult;
+			a.Z *= mult;
+			a.W *= mult;
+			return a;
+		}
+
+		/// <summary>
+		/// Divide a vector by a scalar
+		/// </summary>
+		/// <param name="a">Vector operand</param>
+		/// <param name="f">Scalar operand</param>
+		/// <param name="result">Result of the division</param>
+		public static void Div(ref Vector4 a, float f, out Vector4 result)
+		{
+			float mult = 1.0f / f;
+			result.X = a.X * mult;
+			result.Y = a.Y * mult;
+			result.Z = a.Z * mult;
+			result.W = a.W * mult;
+		}
+
+		#endregion
+
+		#region Min
+
+		/// <summary>
+		/// Calculate the component-wise minimum of two vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <returns>The component-wise minimum</returns>
+		public static Vector4 Min(Vector4 a, Vector4 b)
+		{
+			a.X = a.X < b.X ? a.X : b.X;
+			a.Y = a.Y < b.Y ? a.Y : b.Y;
+			a.Z = a.Z < b.Z ? a.Z : b.Z;
+			a.W = a.W < b.W ? a.W : b.W;
+			return a;
+		}
+
+		/// <summary>
+		/// Calculate the component-wise minimum of two vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <param name="result">The component-wise minimum</param>
+		public static void Min(ref Vector4 a, ref Vector4 b, out Vector4 result)
+		{
+			result.X = a.X < b.X ? a.X : b.X;
+			result.Y = a.Y < b.Y ? a.Y : b.Y;
+			result.Z = a.Z < b.Z ? a.Z : b.Z;
+			result.W = a.W < b.W ? a.W : b.W;
+		}
+
+		#endregion
+
+		#region Max
+
+		/// <summary>
+		/// Calculate the component-wise maximum of two vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <returns>The component-wise maximum</returns>
+		public static Vector4 Max(Vector4 a, Vector4 b)
+		{
+			a.X = a.X > b.X ? a.X : b.X;
+			a.Y = a.Y > b.Y ? a.Y : b.Y;
+			a.Z = a.Z > b.Z ? a.Z : b.Z;
+			a.W = a.W > b.W ? a.W : b.W;
+			return a;
+		}
+
+		/// <summary>
+		/// Calculate the component-wise maximum of two vectors
+		/// </summary>
+		/// <param name="a">First operand</param>
+		/// <param name="b">Second operand</param>
+		/// <param name="result">The component-wise maximum</param>
+		public static void Max(ref Vector4 a, ref Vector4 b, out Vector4 result)
+		{
+			result.X = a.X > b.X ? a.X : b.X;
+			result.Y = a.Y > b.Y ? a.Y : b.Y;
+			result.Z = a.Z > b.Z ? a.Z : b.Z;
+			result.W = a.W > b.W ? a.W : b.W;
+		}
+
+		#endregion
+
+		#region Clamp
+
+		/// <summary>
+		/// Clamp a vector to the given minimum and maximum vectors
+		/// </summary>
+		/// <param name="vec">Input vector</param>
+		/// <param name="min">Minimum vector</param>
+		/// <param name="max">Maximum vector</param>
+		/// <returns>The clamped vector</returns>
+		public static Vector4 Clamp(Vector4 vec, Vector4 min, Vector4 max)
+		{
+			vec.X = vec.X < min.X ? min.X : vec.X > max.X ? max.X : vec.X;
+			vec.Y = vec.Y < min.Y ? min.Y : vec.Y > max.Y ? max.Y : vec.Y;
+			vec.Z = vec.X < min.Z ? min.Z : vec.Z > max.Z ? max.Z : vec.Z;
+			vec.W = vec.Y < min.W ? min.W : vec.W > max.W ? max.W : vec.W;
+			return vec;
+		}
+
+		/// <summary>
+		/// Clamp a vector to the given minimum and maximum vectors
+		/// </summary>
+		/// <param name="vec">Input vector</param>
+		/// <param name="min">Minimum vector</param>
+		/// <param name="max">Maximum vector</param>
+		/// <param name="result">The clamped vector</param>
+		public static void Clamp(ref Vector4 vec, ref Vector4 min, ref Vector4 max, out Vector4 result)
+		{
+			result.X = vec.X < min.X ? min.X : vec.X > max.X ? max.X : vec.X;
+			result.Y = vec.Y < min.Y ? min.Y : vec.Y > max.Y ? max.Y : vec.Y;
+			result.Z = vec.X < min.Z ? min.Z : vec.Z > max.Z ? max.Z : vec.Z;
+			result.W = vec.Y < min.W ? min.W : vec.W > max.W ? max.W : vec.W;
+		}
+
+		#endregion
+
+		#region Normalize
+
+		/// <summary>
+		/// Scale a vector to unit length
+		/// </summary>
+		/// <param name="vec">The input vector</param>
+		/// <returns>The normalized vector</returns>
+		public static Vector4 Normalize(Vector4 vec)
+		{
+			float scale = 1.0f / vec.Length;
+			vec.X *= scale;
+			vec.Y *= scale;
+			vec.Z *= scale;
+			vec.W *= scale;
+			return vec;
+		}
+
+		/// <summary>
+		/// Scale a vector to unit length
+		/// </summary>
+		/// <param name="vec">The input vector</param>
+		/// <param name="result">The normalized vector</param>
+		public static void Normalize(ref Vector4 vec, out Vector4 result)
+		{
+			float scale = 1.0f / vec.Length;
+			result.X = vec.X * scale;
+			result.Y = vec.Y * scale;
+			result.Z = vec.Z * scale;
+			result.W = vec.W * scale;
+		}
+
+		#endregion
+
+		#region NormalizeFast
+
+		/// <summary>
+		/// Scale a vector to approximately unit length
+		/// </summary>
+		/// <param name="vec">The input vector</param>
+		/// <returns>The normalized vector</returns>
+		public static Vector4 NormalizeFast(Vector4 vec)
+		{
+			float scale = Functions.InverseSqrtFast(vec.X * vec.X + vec.Y * vec.Y + vec.Z * vec.Z + vec.W * vec.W);
+			vec.X *= scale;
+			vec.Y *= scale;
+			vec.Z *= scale;
+			vec.W *= scale;
+			return vec;
+		}
+
+		/// <summary>
+		/// Scale a vector to approximately unit length
+		/// </summary>
+		/// <param name="vec">The input vector</param>
+		/// <param name="result">The normalized vector</param>
+		public static void NormalizeFast(ref Vector4 vec, out Vector4 result)
+		{
+			float scale = Functions.InverseSqrtFast(vec.X * vec.X + vec.Y * vec.Y + vec.Z * vec.Z + vec.W * vec.W);
+			result.X = vec.X * scale;
+			result.Y = vec.Y * scale;
+			result.Z = vec.Z * scale;
+			result.W = vec.W * scale;
+		}
+
+		#endregion
+
+		#region Dot
+
+		/// <summary>
+		/// Caclulate the dot product of two vectors
+		/// </summary>
+		/// <param name="left">First operand</param>
+		/// <param name="right">Second operand</param>
+		/// <returns>The dot product of the two inputs</returns>
+		public static float Dot(Vector4 left, Vector4 right)
+		{
+			return left.X * right.X + left.Y * right.Y + left.Z * right.Z + left.W * right.W;
+		}
+
+		#endregion
+
+		#region Lerp
+
+		/// <summary>
+		/// Returns a new Vector that is the linear blend of the 2 given Vectors
+		/// </summary>
+		/// <param name="a">First input vector</param>
+		/// <param name="b">Second input vector</param>
+		/// <param name="blend">The blend factor</param>
+		/// <returns>a when blend=0, b when blend=1, and a linear combination otherwise</returns>
+		public static Vector4 Lerp(Vector4 a, Vector4 b, float blend)
+		{
+			a.X = blend * (b.X - a.X) + a.X;
+			a.Y = blend * (b.Y - a.Y) + a.Y;
+			a.Z = blend * (b.Z - a.Z) + a.Z;
+			a.W = blend * (b.W - a.W) + a.W;
+			return a;
+		}
+
+		#endregion
+
+		#region Barycentric
+
+		/// <summary>
+		/// Interpolate 3 Vectors using Barycentric coordinates
+		/// </summary>
+		/// <param name="a">First input Vector</param>
+		/// <param name="b">Second input Vector</param>
+		/// <param name="c">Third input Vector</param>
+		/// <param name="u">First Barycentric Coordinate</param>
+		/// <param name="v">Second Barycentric Coordinate</param>
+		/// <returns>a when u=v=0, b when u=1,v=0, c when u=0,v=1, and a linear combination of a,b,c otherwise</returns>
+		public static Vector4 BaryCentric(Vector4 a, Vector4 b, Vector4 c, float u, float v)
+		{
+			return a + u * (b - a) + v * (c - a);
+		}
+
+		#endregion
+
+		#region Transform
+
+		/// <summary>
+		/// Transform a Vector by the given Matrix
+		/// </summary>
+		/// <param name="pos">The vector to transform</param>
+		/// <param name="mat">The desired transformation</param>
+		/// <returns>The transformed vector</returns>
+		public static Vector4 Transform(Vector4 vec, Matrix4 mat)
+		{
+			Vector4 result;
+			result.X = Vector4.Dot(vec, mat.Column0);
+			result.Y = Vector4.Dot(vec, mat.Column1);
+			result.Z = Vector4.Dot(vec, mat.Column2);
+			result.W = Vector4.Dot(vec, mat.Column3);
+			return result;
+		}
+
+		#endregion
+
+		#endregion
+
+		#region public override string ToString()
+
+		/// <summary>
+		/// Returns a System.String that represents the current Vector4.
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
         {
             return String.Format("({0}, {1}, {2}, {3})", X, Y, Z, W);
-        }
-    }
+		}
+
+		#endregion
+	}
 }
