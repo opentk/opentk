50eda683e2ebcbeacac103018c307778901bbdae
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-04 15:33:43 +0000

    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@611 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/Examples/WinForms/W04_Multiple_GLControls.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenTK;
+using OpenTK.OpenGL;
+
+namespace Examples.WinForms
+{
+    public partial class W04_Multiple_GLControls : Form, IExample
+    {
+        public W04_Multiple_GLControls()
+        {
+            InitializeComponent();
+        }
+
+        #region IExample Members
+
+        public void Launch()
+        {
+            
+        }
+
+        public static readonly int order = 4;
+
+        #endregion
+    }
+}
