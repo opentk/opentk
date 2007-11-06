f683cd5977e8b46fc061bde5b0a6ff0a915c3dcc
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-06 13:32:17 +0000

    Added Box2 class.

---
+++ Source/OpenTK/Math/Box2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace OpenTK.Math
+{
+    /// <summary>
+    /// Defines a rectangle.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Box2
+    {
+        public Box2(Vector2 topLeft, Vector2 bottomRight)
+        {
+            Left = topLeft.X;
+            Top = topLeft.Y;
+            Right = topLeft.X;
+            Bottom = topLeft.Y;
+        }
+        public Box2(float left, float top, float right, float bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+        public float Left, Right, Top, Bottom;
+        public float Width { get { return (float)System.Math.Abs(Right - Left); } }
+        public float Height { get { return (float)System.Math.Abs(Bottom - Top); } }
+        public static Box2 FromTLRB(float top, float left, float right, float bottom)
+        {
+            return new Box2(left, top, right, bottom);
+        }
+    }
+}
