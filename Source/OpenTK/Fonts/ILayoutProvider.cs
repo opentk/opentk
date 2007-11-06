bf512836cd80cec24fe6a1ea71576b46e4b59d84
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-06 13:30:25 +0000

    Added the necessary methods.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@624 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Fonts/ILayoutProvider.cs
+++ Source/OpenTK/Fonts/ILayoutProvider.cs
@@ -5,8 +5,15 @@ using System.Drawing;
 
 namespace OpenTK.Fonts
 {
-    interface ILayoutProvider
+    /// <summary>
+    /// Defines the interface for a TextPrinter.
+    /// </summary>
+    public interface ITextPrinter
     {
-        void PerformLayout(string text, IFont font, RectangleF layoutRect, StringAlignment alignment, bool rightToLeft);
+        void Prepare(string text, TextureFont font, out TextHandle handle);
+        void Prepare(string text, TextureFont font, out TextHandle handle, float width, bool wordWarp);
+        void Prepare(string text, TextureFont font, out TextHandle handle, float width, bool wordWarp, StringAlignment alignment);
+        void Prepare(string text, TextureFont font, out TextHandle handle, float width, bool wordWarp, StringAlignment alignment, bool rightToLeft);
+        void Draw(TextHandle handle);
     }
 }
