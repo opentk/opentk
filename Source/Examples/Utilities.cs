9f1593aada8f5872136fff74b4c87ed9751bb779
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2007-11-10 23:31:30 +0000

    Initial check-in.

---
+++ Source/Examples/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Examples
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Converts a System.Drawing.Color to a System.Int32.
+        /// </summary>
+        /// <param name="c">The System.Drawing.Color to convert.</param>
+        /// <returns>A System.Int32 containing the R, G, B, A values of the
+        /// given System.Drawing.Color in the Rbga32 format.</returns>
+        public static int ColorToRgba32(Color c)
+        {
+            return (int)((c.A << 24) | (c.B << 16) | (c.G << 8) | c.R);
+        }
+    }
+}
