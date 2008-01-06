bcd982302a5632369be70d46bb8f0436c72428c0
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-06 02:19:53 +0000

    Changed internal IPrinterImplementation.cs interface to facilitate non-cached rendering.

--- Source/OpenTK/Fonts/DisplayListTextPrinter.cs
+++ Source/OpenTK/Fonts/DisplayListTextPrinter.cs
@@ -19,17 +19,18 @@ namespace OpenTK.Fonts
     {
         #region IPrinter Members
 
-        public TextHandle Load(OpenTK.Math.Vector2[] vertices, ushort[] indices)
+        public TextHandle Load(OpenTK.Math.Vector2[] vertices, ushort[] indices, int index_count)
         {
             DisplayListTextHandle handle = new DisplayListTextHandle(GL.GenLists(1));
 
             GL.NewList(handle.Handle, OpenTK.OpenGL.Enums.ListMode.Compile);
             GL.Begin(OpenTK.OpenGL.Enums.BeginMode.Triangles);
 
-            foreach (ushort index in indices)
+            for (int i = 0; i < index_count; i++)
+            //foreach (ushort index in indices)
             {
-                GL.TexCoord2(vertices[index + 1]);
-                GL.Vertex2(vertices[index]);
+                GL.TexCoord2(vertices[indices[i] + 1]);
+                GL.Vertex2(vertices[indices[i]]);
             }
 
             GL.End();
