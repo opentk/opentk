71384b9ff3582fb818953e6dcd8cea4cabd2670b
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-06 02:19:08 +0000

    Bumped version number.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@769 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Examples/Properties/AssemblyInfo.cs
+++ Source/Examples/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.3.13.3")]
-[assembly: AssemblyFileVersion("0.3.13.3")]
+[assembly: AssemblyVersion("0.3.14.0")]
+[assembly: AssemblyFileVersion("0.3.14.0")]
