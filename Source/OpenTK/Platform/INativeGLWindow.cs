1606cbf0e4a608874c047e4da7bad9e40cd85dc9
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-11 20:22:08 +0000

    Updated INativeGLWindow.CreateWindow to accept a GLContext as argument.

--- Source/OpenTK/Platform/INativeGLWindow.cs
+++ Source/OpenTK/Platform/INativeGLWindow.cs
@@ -17,7 +17,7 @@ namespace OpenTK.Platform
     /// </summary>
     interface INativeGLWindow : IResizable, IDisposable
     {
-        void CreateWindow(DisplayMode mode);
+        void CreateWindow(DisplayMode mode, GLContext context);
         void DestroyWindow();
         void ProcessEvents();
         void PointToClient(ref System.Drawing.Point p);
