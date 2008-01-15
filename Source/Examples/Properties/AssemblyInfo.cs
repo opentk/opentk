13e2bbbac5d63c17062dcbdad396ac418a4b43a8
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-15 00:10:12 +0000

    Bumped version number.

--- Source/Examples/Properties/AssemblyInfo.cs
+++ Source/Examples/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.3.14.0")]
-[assembly: AssemblyFileVersion("0.3.14.0")]
+[assembly: AssemblyVersion("0.3.14.1")]
+[assembly: AssemblyFileVersion("0.3.14.1")]
