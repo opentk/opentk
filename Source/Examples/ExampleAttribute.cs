65019c5c53efab8d2448863a80dc6f2d31385c31
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-18 13:59:26 +0000

    ExampleAttribute.ToString() now ignores ExampleAttribute.Difficulty if it is zero. Simplified ExampleAttribute constructors.

--- Source/Examples/ExampleAttribute.cs
+++ Source/Examples/ExampleAttribute.cs
@@ -19,12 +19,11 @@ namespace Examples
         public readonly int Difficulty;
         public readonly bool Visible = true;
 
+        public ExampleAttribute(string title, ExampleCategory category)
+            : this(title, category, 0, true) { }
+
         public ExampleAttribute(string title, ExampleCategory category, int difficulty)
-        {
-            this.Title = title;
-            this.Category = category;
-            this.Difficulty = difficulty;
-        }
+            : this(title, category, difficulty, true) { }
 
         public ExampleAttribute(string title, ExampleCategory category, int difficulty, bool visible)
         {
@@ -36,17 +35,20 @@ namespace Examples
 
         public override string ToString()
         {
-            return String.Format("{0} {1}: {2}", Category, Difficulty, Title);
+            if (Difficulty != 0)
+                return String.Format("{0} {1}: {2}", Category, Difficulty, Title);
+            return String.Format("{0}: {1}", Category, Title);
         }
     }
 
     public enum ExampleCategory
     {
-        OpenGL,
+        OpenGL = 0,
         OpenAL,
         Tutorial,
         GLSL,
         WinForms,
         Test,
+        Last
     }
 }
