ab1b65520dd58f242af8f251b4081450b8b40123
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-18 14:15:37 +0000

    Moved ContextHandle to its own class.

---
+++ Source/OpenTK/ContextHandle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK
+{
+    internal class ContextHandle : /*System.Runtime.InteropServices.SafeHandle,*/ IComparable<ContextHandle>
+    {
+        IntPtr handle;
+        public IntPtr Handle { get { return handle; } }
+
+        public ContextHandle() /*: base(IntPtr.Zero, true)*/ { }
+        public ContextHandle(IntPtr h) { handle = h; }
+
+        #region IComparable<ContextHandle> Members
+
+        public int CompareTo(ContextHandle other)
+        {
+            unsafe { return (int)((int*)other.handle.ToPointer() - (int*)this.handle.ToPointer()); }
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            return Handle.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ContextHandle)
+                return this.Handle == ((ContextHandle)obj).Handle;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Handle.GetHashCode();
+        }
+
+        /*
+        public override bool IsInvalid
+        {
+            get { return handle == IntPtr.Zero; }
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            throw new NotImplementedException();
+        }
+        */
+
+        public static implicit operator IntPtr(ContextHandle c)
+        {
+            return c.handle;
+        }
+
+        public static implicit operator ContextHandle(IntPtr p)
+        {
+            return new ContextHandle(p);
+        }
+    }
+}
