bf5e9fe5bd5300bd8f1756e0ea36b7e3fef0094d
chrisbrandtner <chrisbrandtner@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-19 16:06:47 +0000

    removed unused AL_XYZ_NULL.

--- Source/OpenTK/OpenAL/EfxTokens.cs
+++ Source/OpenTK/OpenAL/EfxTokens.cs
@@ -307,16 +307,12 @@ namespace OpenTK.OpenAL.Enums
 
     public enum EfxAuxiliaryi : int
     {
-        // Auxiliary Slot object definitions to be used with alAuxiliaryEffectSlot functions.
         /// <summary>This property is used to attach an Effect object to the Auxiliary Effect Slot object. After the attachment, the Auxiliary Effect Slot object will contain the effect type and have the same effect parameters that were stored in the Effect object. Any Sources feeding the Auxiliary Effect Slot will immediate feed the new effect type and new effect parameters.</summary>
         EffectslotEffect = 0x0001,
 
         /// <summary>This property is used to enable or disable automatic send adjustments based on the physical positions of the sources and the listener. This property should be enabled when an application wishes to use a reverb effect to simulate the environment surrounding a listener or a collection of Sources. Range [False .. True] Default: True </summary>
         EffectslotAuxiliarySendAuto = 0x0003,
-
-        // Value to be used as an Auxiliary Slot ID to disable a source send..
-        //  AL_EFFECTSLOT_NULL = 0x0000, // remove, seems not to belong here. it's a target, not a token
-    }
+  }
 
     public enum EfxAuxiliaryf : int
     {
