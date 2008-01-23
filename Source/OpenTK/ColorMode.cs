65869c114bb0212d0305da422328042f32e52ecb
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-23 12:41:10 +0000

    Addd public properties and documentation.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@975 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/ColorMode.cs
+++ Source/OpenTK/ColorMode.cs
@@ -10,12 +10,23 @@ using System.Globalization;
 
 namespace OpenTK
 {
+    /// <summary>Defines a color mode for OpenGL rendering.</summary>
+    /// <remarks>
+    /// <para>A ColorMode contains Red, Green, Blue and Alpha components that descibe
+    /// the allocated bits per pixel for the corresponding color.</para>
+    /// </remarks>
     public class ColorMode
     {
-        public byte Red, Green, Blue, Alpha;
-        public bool IsIndexed = false;
-        public int BitsPerPixel;
-        
+        byte red, green, blue, alpha;
+        bool isIndexed = false;
+        int bitsPerPixel;
+
+        #region --- Constructors ---
+
+        /// <summary>
+        /// Constructs a new ColorMode with the specified aggregate bits per pixel.
+        /// </summary>
+        /// <param name="bpp">The bits per pixel sum for the Red, Green, Blue and Alpha color channels.</param>
         public ColorMode(int bpp)
         {
             Red = Green = Blue = Alpha = 0;
@@ -37,16 +48,33 @@ namespace OpenTK
                     Red = Green = Blue = 5;
                     break;
                 case 8:
+                    Red = Green = 3;
+                    Blue = 2;
                     IsIndexed = true;
                     break;
                 case 4:
+                    Red = Green = 2;
+                    Blue = 1;
+                    IsIndexed = true;
+                    break;
+                case 1:
                     IsIndexed = true;
                     break;
                 default:
+                    Red = Blue = Alpha = (byte)(bpp / 4);
+                    Green = (byte)((bpp / 4) + (bpp % 4));
                     break;
             }
         }
 
+        /// <summary>
+        /// Constructs a new ColorMode with the specified bits per pixel for 
+        /// the Red, Green, Blue and Alpha color channels.
+        /// </summary>
+        /// <param name="red">Bits per pixel for the Red color channel.</param>
+        /// <param name="green">Bits per pixel for the Green color channel.</param>
+        /// <param name="blue">Bits per pixel for the Blue color channel.</param>
+        /// <param name="alpha">Bits per pixel for the Alpha color channel.</param>
         public ColorMode(int red, int green, int blue, int alpha)
         {
             Red = (byte)red;
@@ -54,7 +82,30 @@ namespace OpenTK
             Blue = (byte)blue;
             Alpha = (byte)alpha;
             BitsPerPixel = red + green + blue + alpha;
+            if (BitsPerPixel < 15)
+                IsIndexed = true;
         }
+
+        #endregion
+
+        #region --- Public Methods ---
+
+        /// <summary>Gets the bits per pixel for the Red channel.</summary>
+        public int Red { get { return red; } private set { red = (byte)value; } }
+        /// <summary>Gets the bits per pixel for the Green channel.</summary>
+        public int Green { get { return green; } private set { green = (byte)value; } }
+        /// <summary>Gets the bits per pixel for the Blue channel.</summary>
+        public int Blue { get { return blue; } private set { blue = (byte)value; } }
+        /// <summary>Gets the bits per pixel for the Alpha channel.</summary>
+        public int Alpha { get { return alpha; } private set { alpha = (byte)value; } }
+        /// <summary>Gets a System.Boolean indicating whether this ColorMode is indexed.</summary>
+        public bool IsIndexed { get { return isIndexed; } private set { isIndexed = value; } }
+        /// <summary>Gets the sum of Red, Green, Blue and Alpha bits per pixel.</summary>
+        public int BitsPerPixel { get { return bitsPerPixel; } private set { bitsPerPixel = value; } }
+
+        #endregion
+
+        #region --- Overrides ---
 
         public override bool Equals(object obj)
         {
@@ -90,5 +141,7 @@ namespace OpenTK
         {
             return string.Format("{0} ({1})", BitsPerPixel, (IsIndexed ? " indexed" : Red.ToString() + Green.ToString() + Blue.ToString() + Alpha.ToString()));
         }
+
+        #endregion
     }
 }
