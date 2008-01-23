6cd33ee9a833f9e18271f9380bcf600879397f81
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-23 00:26:58 +0000

    Added LoadDelegate method.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@974 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Platform/X11/GlxHelper.cs
+++ Source/OpenTK/Platform/X11/GlxHelper.cs
@@ -7,6 +7,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace OpenTK.Platform.X11
 {
@@ -16,6 +18,59 @@ namespace OpenTK.Platform.X11
 
         // Disable BeforeFieldInit optimization.
         static Glx() { }
+
+        #region static Delegate LoadDelegate(string name, Type signature)
+
+        /// <summary>
+        /// Creates a System.Delegate that can be used to call an OpenGL function, core or extension.
+        /// </summary>
+        /// <param name="name">The name of the Wgl function (eg. "wglNewList")</param>
+        /// <param name="signature">The signature of the OpenGL function.</param>
+        /// <returns>
+        /// A System.Delegate that can be used to call this OpenGL function, or null if the specified
+        /// function name did not correspond to an OpenGL function.
+        /// </returns>
+        static Delegate LoadDelegate(string name, Type signature)
+        {
+            Delegate d;
+            string realName = name.StartsWith("glx") ? name.Substring(3) : name;
+
+            if (typeof(Glx).GetMethod(realName,
+                BindingFlags.NonPublic | BindingFlags.Static) != null)
+                d = GetExtensionDelegate(name, signature) ??
+                    Delegate.CreateDelegate(signature, typeof(Glx), realName);
+            else
+                d = GetExtensionDelegate(name, signature);
+
+            return d;
+        }
+
+        #endregion
+
+        #region private static Delegate GetExtensionDelegate(string name, Type signature)
+
+        /// <summary>
+        /// Creates a System.Delegate that can be used to call a dynamically exported OpenGL function.
+        /// </summary>
+        /// <param name="name">The name of the OpenGL function (eg. "glNewList")</param>
+        /// <param name="signature">The signature of the OpenGL function.</param>
+        /// <returns>
+        /// A System.Delegate that can be used to call this OpenGL function or null
+        /// if the function is not available in the current OpenGL context.
+        /// </returns>
+        private static Delegate GetExtensionDelegate(string name, Type signature)
+        {
+            IntPtr address = Glx.GetProcAddress(name);
+
+            if (address == IntPtr.Zero ||
+                address == new IntPtr(1) ||     // Workaround for buggy nvidia drivers which return
+                address == new IntPtr(2))       // 1 or 2 instead of IntPtr.Zero for some extensions.
+                return null;
+            else
+                return Marshal.GetDelegateForFunctionPointer(address, signature);
+        }
+
+        #endregion
 
         #region internal static void LoadAll
 
