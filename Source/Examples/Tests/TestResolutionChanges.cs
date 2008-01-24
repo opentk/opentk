23e059dad0221caace9c177b274fbb9ce4bc316e
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-24 09:25:43 +0000

    Added test for DisplayDevice.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1000 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/Examples/Tests/TestResolutionChanges.cs
@@ -0,0 +1,33 @@
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK team.
+ * This notice may not be removed.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using OpenTK.Graphics;
+
+namespace Examples.Tests
+{
+    [Example("Test Resolution Changes", ExampleCategory.Test)]
+    public class TestResolutionChanges
+    {
+        public static void Main()
+        {
+            foreach (DisplayDevice dev in DisplayDevice.AvailableDisplays)
+            {
+                Trace.WriteLine(dev.ToString());
+                //MessageBox.Show(dev.ToString());
+                //dev.ChangeResolution(640, 480, 32, 60.0f);
+                //dev.RestoreResolution();
+            }
+        }
+    }
+}
