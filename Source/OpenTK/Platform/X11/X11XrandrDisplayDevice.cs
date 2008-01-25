f35e6e1e0741b7c511b4ca406ff2b2f519169fac
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-25 10:02:05 +0000

    Added X11XrandrDisplayDevice.cs implementation.

---
+++ Source/OpenTK/Platform/X11/X11XrandrDisplayDevice.cs
@@ -0,0 +1,45 @@
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK team.
+ * This notice may not be removed.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTK.Graphics;
+
+namespace OpenTK.Platform.X11
+{
+    internal class X11XrandrDisplayDevice : IDisplayDeviceDriver
+    {
+        #region --- Constructors ---
+
+        static X11XrandrDisplayDevice()
+        {
+        }
+
+        public X11XrandrDisplayDevice()
+        {
+        }
+
+        #endregion
+
+        #region --- IDisplayDeviceDriver Members ---
+
+        public bool TryChangeResolution(DisplayDevice device, DisplayResolution resolution)
+        {
+            
+        }
+
+        public void RestoreResolution(DisplayDevice device)
+        {
+            
+        }
+
+        #endregion
+    }
+}
