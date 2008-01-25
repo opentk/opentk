4cec210c4bb63e4961e27c29f239bfb1316028ea
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-25 13:13:05 +0000

    Added debug code to dump the number of screens.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1025 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Platform/X11/X11XrandrDisplayDevice.cs
+++ Source/OpenTK/Platform/X11/X11XrandrDisplayDevice.cs
@@ -11,15 +11,38 @@ using System.Collections.Generic;
 using System.Text;
 
 using OpenTK.Graphics;
+using System.Diagnostics;
 
 namespace OpenTK.Platform.X11
 {
     internal class X11XrandrDisplayDevice : IDisplayDeviceDriver
     {
+        static object display_lock = new object();
+
         #region --- Constructors ---
 
         static X11XrandrDisplayDevice()
         {
+            lock (display_lock)
+            {
+                for (int i = 0; i < API.ScreenCount; i++)
+                {
+                    unsafe
+                    {
+                        XRRScreenSize[] array = Functions.XRRSizes(API.DefaultDisplay, i);
+                        Debug.Print("{0} resolutions.", array.Length);
+                        Debug.Indent();
+                        int count = array.Length;
+                        while (--count != 0)
+                            Debug.Print(array[count].ToString());
+                        Debug.Unindent();
+                    }
+                }
+                //Functions.XRRSizes(API.DefaultDisplay, API.DefaultScreen, 
+                //Functions.XRRGetScreenInfo(API.DefaultDisplay);
+                
+            }
+
             // Construct a default device for testing purposes.
             new DisplayDevice(new DisplayResolution(800, 600, 24, 0), true,
                 new DisplayResolution[]
