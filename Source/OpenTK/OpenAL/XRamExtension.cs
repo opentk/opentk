a4c481fe3004af0d5e0f94527f53f25267533d8d
chrisbrandtner <chrisbrandtner@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-01-25 16:22:07 +0000

    Finalized X-Ram.

--- Source/OpenTK/OpenAL/XRamExtension.cs
+++ Source/OpenTK/OpenAL/XRamExtension.cs
@@ -8,14 +8,16 @@
 #endregion
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace OpenTK.OpenAL
 {
 
+    /// <summary>The X-Ram Extension is provided on the top-end Sound Blaster X-Fi solutions (Sound Blaster X-Fi Fatal1ty, Sound Blaster X-Fi Elite Pro, or later). These products feature 64MB of X-Ram that can only be used for audio purposes, which can be controlled by this Extension.</summary>
     public class XRamExtension
     {
-        #region instance state
+        #region Instance state
 
         private bool _valid = false;
 
@@ -25,16 +27,16 @@ namespace OpenTK.OpenAL
             get { return _valid; }
         }
 
-        #endregion instance state
+        #endregion Instance state
 
         #region X-RAM Function pointer definitions
 
         [CLSCompliant(false)]
-        public unsafe delegate bool Delegate_SetBufferMode( int n,ref uint buffers,int value );
+        private delegate bool Delegate_SetBufferMode( int n,ref uint buffers,int value );
         //typedef ALboolean (__cdecl *EAXSetBufferMode)(ALsizei n, ALuint *buffers, ALint value);
 
         [CLSCompliant(false)]
-        public delegate int Delegate_GetBufferMode( uint buffer,out int value );
+        private delegate int Delegate_GetBufferMode( uint buffer,IntPtr value );
         //typedef ALenum    (__cdecl *EAXGetBufferMode)(ALuint buffer, ALint *value);
 
         //[CLSCompliant(false)]
@@ -65,7 +67,7 @@ namespace OpenTK.OpenAL
             AL_STORAGE_HARDWARE = AL.GetEnumValue("AL_STORAGE_HARDWARE");
             AL_STORAGE_ACCESSIBLE = AL.GetEnumValue("AL_STORAGE_ACCESSIBLE");
 
-            Console.WriteLine("RamSize: {0} RamFree: {1} StorageAuto: {2} StorageHW: {3} StorageAccess: {4}",AL_EAX_RAM_SIZE,AL_EAX_RAM_FREE,AL_STORAGE_AUTOMATIC,AL_STORAGE_HARDWARE,AL_STORAGE_ACCESSIBLE);
+            // Console.WriteLine("RamSize: {0} RamFree: {1} StorageAuto: {2} StorageHW: {3} StorageAccess: {4}",AL_EAX_RAM_SIZE,AL_EAX_RAM_FREE,AL_STORAGE_AUTOMATIC,AL_STORAGE_HARDWARE,AL_STORAGE_ACCESSIBLE);
 
             if ( AL_EAX_RAM_SIZE == 0 ||
                  AL_EAX_RAM_FREE == 0 ||
@@ -73,11 +75,11 @@ namespace OpenTK.OpenAL
                  AL_STORAGE_HARDWARE == 0 ||
                  AL_STORAGE_ACCESSIBLE == 0 )
             {
-                Console.WriteLine("Token values could not be retrieved.");
+                Trace.WriteLine("Token values could not be retrieved.");
                 return;
             }
 
-            Console.WriteLine("Free Ram: {0} / {1}",GetRamFree( ),GetRamSize( ));
+            // Console.WriteLine("Free Ram: {0} / {1}",GetRamFree( ),GetRamSize( ));
 
             try
             {
@@ -85,7 +87,7 @@ namespace OpenTK.OpenAL
                 Imported_SetBufferMode = (Delegate_SetBufferMode) Marshal.GetDelegateForFunctionPointer(AL.GetProcAddress("EAXSetBufferMode"),typeof(Delegate_SetBufferMode));
             } catch ( Exception e )
             {
-                Console.WriteLine("Attempt to marshal AL.GetProcAddress failed. " + e.ToString( ));
+                Trace.WriteLine("Attempt to marshal function pointers with AL.GetProcAddress failed. " + e.ToString( ));
                 return;
             }
 
@@ -96,59 +98,62 @@ namespace OpenTK.OpenAL
 
         #region Public Methods
 
-        /// <summary>Query total amount of X-RAM.</summary>
+        /// <summary>Query total amount of X-RAM in bytes.</summary>
         public int GetRamSize( )
         {
             return AL.Get((Enums.ALGetInteger) AL_EAX_RAM_SIZE);
         }
 
-        /// <summary>Query free X-RAM available.</summary>
+        /// <summary>Query free X-RAM available in bytes.</summary>
         public int GetRamFree( )
         {
             return AL.Get((Enums.ALGetInteger) AL_EAX_RAM_FREE);
         }
 
+        /// <summary>This enum is used to abstract the need of using AL.GetEnumValue() with the Extension. The values do NOT correspond to AL_STORAGE_* tokens!</summary>
         public enum XRamStorage : byte
         {
-            /// <summary>Put an Open AL Buffer into X-RAM if memory is available, otherwise use host RAM.  This is the default mode.
-            /// alGenBuffers(1, &uiBuffer);
-            /// eaxSetBufferMode(1, &uiBuffer, alGetEnumValue("AL_STORAGE_AUTOMATIC"));
-            /// alBufferData(...);</summary>
-            Automatic,
-            /// <summary>Force an Open AL Buffer into X-RAM (good for non-streaming buffers)
-            // alGenBuffers(1, &uiBuffer);
-            // eaxSetBufferMode(1, &uiBuffer, alGetEnumValue("AL_STORAGE_HARDWARE"));
-            // alBufferData(...);</summary>
+            /// <summary>Put an Open AL Buffer into X-RAM if memory is available, otherwise use host RAM.  This is the default mode.</summary>
+            Automatic = 0,
+            /// <summary>Force an Open AL Buffer into X-RAM (good for non-streaming buffers)</summary>
             Hardware = 1,
-            /// <summary>Force an Open AL Buffer into 'accessible' (currently host) RAM (good for streaming buffers)
-            /// alGenBuffers(1, &uiBuffer);
-            /// eaxSetBufferMode(1, &uiBuffer, alGetEnumValue("AL_STORAGE_ACCESSIBLE"));
-            /// alBufferData(...);</summary>
-            Acessible = 2,
+            /// <summary>Force an Open AL Buffer into 'accessible' (currently host) RAM (good for streaming buffers)</summary>
+            Accessible = 2,
         }
 
+        /// <summary>This function is used to set the storage Mode of an array of OpenAL Buffers.</summary>
+        /// <param name="n">The number of OpenAL Buffers pointed to by buffer.</param>
+        /// <param name="buffer">An array of OpenAL Buffer handles.</param>
+        /// <param name="mode">The storage mode that should be used for all the given buffers. Should be the value of one of the following enum names: XRamStorage.Automatic, XRamStorage.Hardware, XRamStorage.Accessible</param>
+        /// <returns>True if all the Buffers were successfully set to the requested storage mode, False otherwise.</returns>
         [CLSCompliant(false)]
-        public void SetBufferMode( int n,ref uint buffer,XRamStorage mode )
+        public bool SetBufferMode( int n,ref uint buffer,XRamStorage mode )
         {
             switch ( mode )
             {
-            case XRamStorage.Acessible:
-                Imported_SetBufferMode(1,ref buffer,AL_STORAGE_ACCESSIBLE);
-                break;
+            case XRamStorage.Accessible:
+                return Imported_SetBufferMode(n,ref buffer,AL_STORAGE_ACCESSIBLE);
             case XRamStorage.Hardware:
-                Imported_SetBufferMode(1,ref buffer,AL_STORAGE_HARDWARE);
-                break;
+                return Imported_SetBufferMode(n,ref buffer,AL_STORAGE_HARDWARE);
             default:
-                Imported_SetBufferMode(1,ref buffer,AL_STORAGE_AUTOMATIC);
-                break;
+                return Imported_SetBufferMode(n,ref buffer,AL_STORAGE_AUTOMATIC);
             }
         }
 
+        /// <summary>This function is used to retrieve the storage Mode of a single OpenAL Buffer.</summary>
+        /// <param name="buffer">The handle of an OpenAL Buffer.</param>
+        /// <returns>The current Mode of the Buffer.</returns>
         [CLSCompliant(false)]
         public XRamStorage GetBufferMode( ref uint buffer )
         {
-            int t; // this is improper, find sample codes using it and figure out what 2nd param does.
-            return (XRamStorage) Imported_GetBufferMode(buffer,out t);
+            int tempresult= Imported_GetBufferMode(buffer,IntPtr.Zero); // IntPtr.Zero due to the parameter being unused/reserved atm
+
+            if ( tempresult == AL_STORAGE_ACCESSIBLE )
+                return XRamStorage.Accessible;
+            if ( tempresult == AL_STORAGE_HARDWARE )
+                return XRamStorage.Hardware;
+            // default:
+            return XRamStorage.Automatic;
         }
 
         #endregion Public Methods
