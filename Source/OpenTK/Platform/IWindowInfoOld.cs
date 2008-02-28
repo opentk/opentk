97ccade80f5255a047e3b52c1640f670b6f5e88c
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-02-28 13:53:28 +0000

    Obsoleted.

---
+++ Source/OpenTK/Platform/IWindowInfoOld.cs
@@ -0,0 +1,27 @@
+﻿#region --- License ---
+/* Copyright (c) 2006, 2007 Stefanos Apostolopoulos
+ * See license.txt for license info
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Platform
+{
+    /// <summary>
+    /// Provides a platform independent mechanism to interact with System.Windows.Forms.Control,
+    /// System.Windows.Forms.NativeWindow and OpenTK.GameWindow low-level implementation data.
+    /// </summary>
+    [Obsolete("Use OpenTK.Platform.IWindowInfo instead.")]
+    public interface IWindowInfoOld
+    {
+        IntPtr Handle { get; }
+        IWindowInfoOld Parent { get; }
+        IWindowInfoOld GetInfoFrom(System.Windows.Forms.Control control);
+        IWindowInfoOld GetInfoFrom(System.Windows.Forms.NativeWindow window);
+        IWindowInfoOld GetInfoFrom(OpenTK.GameWindow window);
+        IWindowInfoOld GetInfoFrom(IWindowInfoOld info);
+    }
+}
