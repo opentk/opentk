58e910d47850991194bfcfe639ac7d5dcf9cab29
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-03-03 12:46:17 +0000

    Initial commit.

---
+++ Source/OpenTK/Graphics/IGraphicsMode.cs
@@ -0,0 +1,20 @@
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK Team.
+ * This notice may not be removed from any source distribution.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Graphics
+{
+    internal interface IGraphicsMode
+    {
+        GraphicsMode SelectGraphicsMode(ColorFormat color, int depth, int stencil, int samples, ColorFormat accum, int buffers,
+                                        bool stereo);
+    }
+}
