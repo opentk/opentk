9f7d85813650add5fe10c3ace3976214bda45078
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-03-03 12:48:00 +0000

    Initial commit.

---
+++ Source/OpenTK/Platform/IGLControl.cs
@@ -0,0 +1,23 @@
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK Team.
+ * This notice may not be removed from any source distribution.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using OpenTK.Graphics;
+
+namespace OpenTK.Platform
+{
+    internal interface IGLControl
+    {
+        GraphicsContext CreateContext();
+        bool IsIdle { get; }
+        IWindowInfo WindowInfo { get; }
+    }
+}
