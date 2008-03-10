f23d7929be072df6812a680041e6df8c862ddf07
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-03-10 08:50:19 +0000

    Can now distinguish between left and right control, shift, alt and enter keys.

--- Source/OpenTK/Platform/Windows/API.cs
+++ Source/OpenTK/Platform/Windows/API.cs
@@ -521,11 +521,31 @@ namespace OpenTK.Platform.Windows
 
         #endregion
 
-        #region GetASsyncKeyState
+        #region GetAsyncKeyState
 
         [System.Security.SuppressUnmanagedCodeSecurity]
         [DllImport("user32.dll", SetLastError = true)]
         internal static extern SHORT GetAsyncKeyState(VirtualKeys vKey);
+
+        #endregion
+
+        #region GetKeyState
+
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern SHORT GetKeyState(VirtualKeys vKey);
+
+        #endregion
+
+        #region MapVirtualKey
+
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern UINT MapVirtualKey(UINT uCode, MapVirtualKeyType uMapType);
+
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern UINT MapVirtualKey(VirtualKeys vkey, MapVirtualKeyType uMapType);
 
         #endregion
 
@@ -3502,6 +3522,23 @@ namespace OpenTK.Platform.Windows
         Russian = 204,
         Mac = 77,
         Baltic = 186,
+    }
+
+    #endregion
+
+    #region MapVirtualKeyType
+
+    internal enum MapVirtualKeyType
+    {
+        /// <summary>uCode is a virtual-key code and is translated into a scan code. If it is a virtual-key code that does not distinguish between left- and right-hand keys, the left-hand scan code is returned. If there is no translation, the function returns 0.</summary>
+        VirtualKeyToScanCode = 0,
+        /// <summary>uCode is a scan code and is translated into a virtual-key code that does not distinguish between left- and right-hand keys. If there is no translation, the function returns 0.</summary>
+        ScanCodeToVirtualKey = 1,
+        /// <summary>uCode is a virtual-key code and is translated into an unshifted character value in the low-order word of the return value. Dead keys (diacritics) are indicated by setting the top bit of the return value. If there is no translation, the function returns 0.</summary>
+        VirtualKeyToCharacter = 2,
+        /// <summary>Windows NT/2000/XP: uCode is a scan code and is translated into a virtual-key code that distinguishes between left- and right-hand keys. If there is no translation, the function returns 0.</summary>
+        ScanCodeToVirtualKeyExtended = 3,
+        VirtualKeyToScanCodeExtended = 4,
     }
 
     #endregion
