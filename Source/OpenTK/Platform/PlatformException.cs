560f132764364df47f4c0caf5359dee376eb3f6a
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-03-24 19:01:35 +0000

    Initial commit.

---
+++ Source/OpenTK/Platform/PlatformException.cs
@@ -0,0 +1,16 @@
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK Team.
+ * This notice may not be removed from any source distribution.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
+
+/// <summary>Defines a plaftorm specific exception.</summary>
+public class PlatformException : Exception
+{
+    /// <summary>Constructs a new PlatformException.</summary>
+    public PlatformException(string s) : base(s) { }
+}
