87faa870cc4a1eb36a3346c39d9dfaa5b2fdab13
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-04-20 15:09:30 +0000

    Display a scrollbar when too many examples are loaded.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1246 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Examples/ExampleLauncher.Designer.cs
+++ Source/Examples/ExampleLauncher.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.runButton = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -39,7 +43,7 @@
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Margin = new System.Windows.Forms.Padding(1);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(284, 264);
+            this.listBox1.Size = new System.Drawing.Size(292, 212);
             this.listBox1.Sorted = true;
             this.listBox1.TabIndex = 0;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
@@ -47,28 +51,50 @@
             // 
             // runButton
             // 
-            this.runButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.runButton.Location = new System.Drawing.Point(0, 220);
+            this.runButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.runButton.Location = new System.Drawing.Point(0, 0);
             this.runButton.Margin = new System.Windows.Forms.Padding(1);
             this.runButton.Name = "runButton";
-            this.runButton.Size = new System.Drawing.Size(284, 44);
+            this.runButton.Size = new System.Drawing.Size(292, 52);
             this.runButton.TabIndex = 0;
             this.runButton.Text = "Run Example";
             this.runButton.UseVisualStyleBackColor = true;
             this.runButton.Click += new System.EventHandler(this.runButton_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.runButton);
+            this.splitContainer1.Size = new System.Drawing.Size(292, 266);
+            this.splitContainer1.SplitterDistance = 213;
+            this.splitContainer1.SplitterWidth = 1;
+            this.splitContainer1.TabIndex = 1;
+            // 
             // ExampleLauncher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 264);
-            this.Controls.Add(this.runButton);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Controls.Add(this.splitContainer1);
             this.MinimumSize = new System.Drawing.Size(300, 300);
             this.Name = "ExampleLauncher";
             this.Text = "OpenTK Example Launcher";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExampleLauncher_FormClosing);
             this.Load += new System.EventHandler(this.ExampleLauncher_Load);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExampleLauncher_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -77,6 +103,7 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.SplitContainer splitContainer1;
 
     }
 }
