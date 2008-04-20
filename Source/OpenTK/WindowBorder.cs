0e2b89c3695531e14eae3b5521eacfb1fc923f58
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-04-20 17:48:19 +0000

    Added WindowBorder enum.

---
+++ Source/OpenTK/WindowBorder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK
+{
+    /// <summary>
+    /// Enumerates available window borders.
+    /// </summary>
+    public enum WindowBorder
+    {
+        /// <summary>
+        /// The window has a resizable border. A window with a resizable border can be resized by the user or programmatically.
+        /// </summary>
+        Resizable = 0,
+        /// <summary>
+        /// The window has a fixed border. A window with a fixed border can only be resized programmatically.
+        /// </summary>
+        Fixed,
+        /// <summary>
+        /// The window does not have a border. A window with a hidden border can only be resized programmatically.
+        /// </summary>
+        Hidden
+    }
+}
