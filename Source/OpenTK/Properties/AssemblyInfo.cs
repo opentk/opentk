776bbcd2dc5286272df402dd94959fb308cc6849
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-05-04 16:55:06 +0000

    Updated assembly information.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1295 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Properties/AssemblyInfo.cs
+++ Source/OpenTK/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenTK")]
-[assembly: AssemblyDescription("An open-source game development toolkit for .Net/Mono")]
+[assembly: AssemblyDescription("Open source game development toolkit for .Net/Mono.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenTK")]
+[assembly: AssemblyProduct("OpenTK Core Library")]
 [assembly: AssemblyCopyright("Copyright ©  2006-2008 the OpenTK team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
