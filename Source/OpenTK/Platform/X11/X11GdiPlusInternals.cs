244de27aa277b55ea32634d8e75c031699bb7278
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-11-03 23:55:53 +0000

    Implemented X11GdiPlusInternals. Added missing license headers.

--- Source/OpenTK/Platform/X11/X11GdiPlusInternals.cs
+++ Source/OpenTK/Platform/X11/X11GdiPlusInternals.cs
@@ -1,26 +1,50 @@
-﻿using System;
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK Team.
+ * This notice may not be removed from any source distribution.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using System.Reflection;
 
 namespace OpenTK.Platform.X11
 {
     class X11GdiPlusInternals : IGdiPlusInternals
     {
-        #region IGdiPlusInternals Members
+        static readonly PropertyInfo native_graphics_property, native_font_property, native_string_format_property;
+
+        static X11GdiPlusInternals()
+        {
+            native_graphics_property =
+                typeof(System.Drawing.Graphics).GetProperty("NativeObject", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            native_font_property =
+                typeof(System.Drawing.Font).GetProperty("NativeObject", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            native_string_format_property =
+                typeof(System.Drawing.StringFormat).GetProperty("NativeObject", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        #region --- IGdiPlusInternals Members ---
 
         public IntPtr GetNativeGraphics(System.Drawing.Graphics graphics)
         {
-            throw new NotImplementedException();
+            return (IntPtr)native_graphics_property.GetValue(graphics, null);
         }
 
-        public IntPtr GetNativeFont(System.Drawing.Font font)
+        public IntPtr GetNativeFont(Font font)
         {
-            throw new NotImplementedException();
+            return (IntPtr)native_font_property.GetValue(font, null);
         }
 
-        public IntPtr GetNativeStringFormat(System.Drawing.StringFormat format)
+        public IntPtr GetNativeStringFormat(StringFormat format)
         {
-            throw new NotImplementedException();
+            return (IntPtr)native_string_format_property.GetValue(format, null);
         }
 
         #endregion
