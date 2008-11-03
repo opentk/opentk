bca0eef6d4ea5faca1463fb5e7165d867441b923
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-11-03 23:55:53 +0000

    Implemented X11GdiPlusInternals. Added missing license headers.

--- Source/OpenTK/Platform/IGdiPlusInternals.cs
+++ Source/OpenTK/Platform/IGdiPlusInternals.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿#region --- License ---
+/* Licensed under the MIT/X11 license.
+ * Copyright (c) 2006-2008 the OpenTK Team.
+ * This notice may not be removed from any source distribution.
+ * See license.txt for licensing detailed licensing details.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
