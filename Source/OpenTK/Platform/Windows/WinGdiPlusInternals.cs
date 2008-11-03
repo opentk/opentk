ebc6a610e3fdbfa646f8e447d7bbe81e5c775210
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-11-03 23:34:54 +0000

    Use different implementations for accessing GDI+ internals on .Net and Mono.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1373 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/OpenTK/Platform/Windows/WinGdiPlusInternals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Reflection;
+
+namespace OpenTK.Platform.Windows
+{
+    class WinGdiPlusInternals : IGdiPlusInternals
+    {
+        const string gdi_plus_library = "gdiplus.dll";
+        static readonly PropertyInfo native_graphics_property, native_font_property;
+        static readonly FieldInfo native_string_format_field;
+
+        static WinGdiPlusInternals()
+        {
+            native_graphics_property =
+                typeof(System.Drawing.Graphics).GetProperty("NativeGraphics", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            native_font_property =
+                typeof(System.Drawing.Font).GetProperty("NativeFont", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            native_string_format_field =
+                typeof(System.Drawing.StringFormat).GetField("nativeFormat", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        #region --- IGdiPlusInternals Members ---
+
+        public IntPtr GetNativeGraphics(System.Drawing.Graphics graphics)
+        {
+            return (IntPtr)native_graphics_property.GetValue(graphics, null);
+        }
+
+        public IntPtr GetNativeFont(Font font)
+        {
+            return (IntPtr)native_font_property.GetValue(font, null);
+        }
+
+        public IntPtr GetNativeStringFormat(StringFormat format)
+        {
+            return (IntPtr)native_string_format_field.GetValue(format);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/OpenTK/Platform/X11/X11GdiPlusInternals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Platform.X11
+{
+    class X11GdiPlusInternals : IGdiPlusInternals
+    {
+        #region IGdiPlusInternals Members
+
+        public IntPtr GetNativeGraphics(System.Drawing.Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntPtr GetNativeFont(System.Drawing.Font font)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IntPtr GetNativeStringFormat(System.Drawing.StringFormat format)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
