a8d350eba138bf403d5f3e539e4302a54e046db3
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-11-20 22:24:11 +0000

    Added xml docs. Added GraphicsContextMissingException for use when a GraphicsContext is expected but not found.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1402 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Graphics/GraphicsExceptions.cs
+++ Source/OpenTK/Graphics/GraphicsExceptions.cs
@@ -15,14 +15,32 @@ namespace OpenTK.Graphics
     /// <summary>Represents errors related to unavailable graphics parameters..</summary>
     public class GraphicsModeException : Exception
     {
+        /// <summary>Constructs a new GraphicsModeException.</summary>
         public GraphicsModeException() : base() { }
+        /// <summary>Constructs a new GraphicsModeException with the given error message.</summary>
         public GraphicsModeException(string message) : base(message) { }
     }
 
     /// <summary>Represents errors related to a GraphicsContext.</summary>
     public class GraphicsContextException : Exception
     {
+        /// <summary>Constructs a new GraphicsContextException.</summary>
         public GraphicsContextException() : base() { }
+        /// <summary>Constructs a new GraphicsContextException with the given error message..</summary>
         public GraphicsContextException(string message) : base(message) { }
+    }
+
+    /// <summary>
+    /// Thrown when an operation that required GraphicsContext is performed, when no
+    /// GraphicsContext is current in the calling thread.
+    /// </summary>
+    public class GraphicsContextMissingException : GraphicsContextException
+    {
+        /// <summary>Constructs a new GraphicsContextMissingException.</summary>
+        public GraphicsContextMissingException()
+            : base(String.Format(
+                "No context is current in the calling thread (ThreadId: {0}).",
+                System.Threading.Thread.CurrentThread.ManagedThreadId))
+        { }
     }
 }
