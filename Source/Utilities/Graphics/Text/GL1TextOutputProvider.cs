30f75d4389035e185c6f6d431bb9e596513343ad
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2008-11-25 14:04:24 +0000

    Added missing license notice.

--- Source/Utilities/Graphics/Text/GL1TextOutputProvider.cs
+++ Source/Utilities/Graphics/Text/GL1TextOutputProvider.cs
@@ -1,12 +1,47 @@
+#region License
+//
+// The Open Toolkit Library License
+//
+// Copyright (c) 2006 - 2008 the Open Toolkit library, except where noted.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to 
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+#endregion
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
 
+using OpenTK.Math;
+
 namespace OpenTK.Graphics.Text
 {
     class GL1TextOutputProvider : ITextOutputProvider
     {
+        #region Fields
+
+        SortedList<int, List<Vector2>> vertices = new SortedList<int,List<Vector2>>();
+
+        #endregion
+
         #region Constructors
 
         public GL1TextOutputProvider() { }
@@ -23,7 +58,7 @@ namespace OpenTK.Graphics.Text
                 if (c != '\n' && c != '\r' && !Char.IsWhiteSpace(c) && !cache.Contains(c, block.Font))
                     cache.Add(c, block.Font);
 
-            GL.BindTexture(TextureTarget.Texture2D, 2);
+            //GL.BindTexture(TextureTarget.Texture2D, 2);
 
             //GL.Begin(BeginMode.Quads);
 
