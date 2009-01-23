49e132911d34ba03746cc75841d3b5bac1c2311f
chrisbrandtner <chrisbrandtner@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-01-23 21:55:21 +0000

    Added instance methods to all single and double precision Vector structs: Add(ref vec) Sub(ref vec) Mult(float) Div(float) Scale(ref vec)

--- Source/OpenTK/Math/Vector4.cs
+++ Source/OpenTK/Math/Vector4.cs
@@ -148,6 +148,63 @@ namespace OpenTK.Math
 
         #region Instance
 
+        #region public void Add()
+
+        /// <summary>Add the Vector passed as parameter to this instance.</summary>
+        /// <param name="right">Right operand. This parameter is only read from.</param>
+        public void Add( ref Vector4 right )
+        {
+            this.X += right.X;
+            this.Y += right.Y;
+            this.Z += right.Z;
+            this.W += right.W;
+        }
+
+        #endregion public void Add()
+
+        #region public void Sub()
+
+        /// <summary>Subtract the Vector passed as parameter from this instance.</summary>
+        /// <param name="right">Right operand. This parameter is only read from.</param>
+        public void Sub( ref Vector4 right )
+        {
+            this.X -= right.X;
+            this.Y -= right.Y;
+            this.Z -= right.Z;
+            this.W -= right.W;
+        }
+
+        #endregion public void Sub()
+
+        #region public void Mult()
+
+        /// <summary>Multiply this instance by a scalar.</summary>
+        /// <param name="f">Scalar operand.</param>
+        public void Mult( float f )
+        {
+            this.X *= f;
+            this.Y *= f;
+            this.Z *= f;
+            this.W *= f;
+        }
+
+        #endregion public void Mult()
+
+        #region public void Div()
+
+        /// <summary>Divide this instance by a scalar.</summary>
+        /// <param name="f">Scalar operand.</param>
+        public void Div( float f )
+        {
+            float mult = 1.0f / f;
+            this.X *= mult;
+            this.Y *= mult;
+            this.Z *= mult;
+            this.W *= mult;
+        }
+
+        #endregion public void Div()
+
         #region public float Length
 
         /// <summary>
@@ -240,7 +297,7 @@ namespace OpenTK.Math
 
         #endregion
 
-        #region public void Scale(float sx, float sy, float sz, float sw)
+        #region public void Scale()
 
         /// <summary>
         /// Scales the current Vector4 by the given amounts.
@@ -249,15 +306,25 @@ namespace OpenTK.Math
         /// <param name="sy">The scale of the Y component.</param>
         /// <param name="sz">The scale of the Z component.</param>
         /// <param name="sw">The scale of the Z component.</param>
-        public void Scale(float sx, float sy, float sz, float sw)
+        public void Scale( float sx, float sy, float sz, float sw )
         {
             this.X = X * sx;
             this.Y = Y * sy;
             this.Z = Z * sz;
             this.W = W * sw;
-       }
+        }
 
-        #endregion
+        /// <summary>Scales this instance by the given parameter.</summary>
+        /// <param name="scale">The scaling of the individual components.</param>
+        public void Scale( ref Vector4 scale )
+        {
+            this.X *= scale.X;
+            this.Y *= scale.Y;
+            this.Z *= scale.Z;
+            this.W *= scale.W;
+        }
+
+        #endregion public void Scale()
 
         #endregion
 
