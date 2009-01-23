9f25573a0bf26226335b09f164825a9384fad583
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-01-23 15:50:31 +0000

    Made the Color4 structure serializable and added == and != operators.

--- Source/OpenTK/Graphics/Color4.cs
+++ Source/OpenTK/Graphics/Color4.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿#region License
+//
+// The Open Toolkit Library License
+//
+// Copyright (c) 2006 - 2008 the Open Toolkit library, except where noted.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to 
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
+using System.Xml.Serialization;
 
 namespace OpenTK.Graphics
 {
     /// <summary>
     /// Represents a color with 4 floating-point components (R, G, B, A).
     /// </summary>
+    [Serializable]
     public struct Color4 : IEquatable<Color4>
     {
         #region Fields
@@ -15,22 +44,22 @@ namespace OpenTK.Graphics
         /// <summary>
         /// The red component of this Color4 structure.
         /// </summary>
-        public readonly float R;
+        public float R;
         
         /// <summary>
         /// The green component of this Color4 structure.
         /// </summary>
-        public readonly float G;
+        public float G;
         
         /// <summary>
         /// The blue component of this Color4 structure.
         /// </summary>
-        public readonly float B;
+        public float B;
         
         /// <summary>
         /// The alpha component of this Color4 structure.
         /// </summary>
-        public readonly float A;
+        public float A;
 
         #endregion
 
@@ -65,7 +94,29 @@ namespace OpenTK.Graphics
 
         #endregion
 
-        #region Conversions
+        #region Operators
+
+        /// <summary>
+        /// Compares the specified Color4 structures for equality.
+        /// </summary>
+        /// <param name="left">The left-hand side of the comparison.</param>
+        /// <param name="right">The right-hand side of the comparison.</param>
+        /// <returns>True if left is equal to right; false otherwise.</returns>
+        public static bool operator ==(Color4 left, Color4 right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Compares the specified Color4 structures for inequality.
+        /// </summary>
+        /// <param name="left">The left-hand side of the comparison.</param>
+        /// <param name="right">The right-hand side of the comparison.</param>
+        /// <returns>True if left is not equal to right; false otherwise.</returns>
+        public static bool operator !=(Color4 left, Color4 right)
+        {
+            return !left.Equals(right);
+        }
 
         /// <summary>
         /// Converts the specified System.Drawing.Color to a Color4 structure.
