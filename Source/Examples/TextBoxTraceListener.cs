f39628bbb2b1e4544eda0213301626917252917d
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-05-30 17:51:19 +0000

    Fixed exception while the Sample Browser is shutting down.

--- Source/Examples/TextBoxTraceListener.cs
+++ Source/Examples/TextBoxTraceListener.cs
@@ -19,20 +19,25 @@ namespace Examples
 
         public override void Write(string message)
         {
-            _target.Invoke(_invokeWrite, new object[] { message });
+            if (_target.Created)
+                _target.Invoke(_invokeWrite, new object[] { message });
         }
 
         public override void WriteLine(string message)
         {
-            _target.Invoke(_invokeWrite, new object[] { message + Environment.NewLine });
+            if (_target.Created)
+                _target.Invoke(_invokeWrite, new object[] { message + Environment.NewLine });
         }
 
         private delegate void StringSendDelegate(string message);
         private void SendString(string message)
         {
-            // No need to lock text box as this function will only 
-            // ever be executed from the UI thread
-            _target.Text += message;
+            if (_target.Created)
+            {
+                // No need to lock text box as this function will only 
+                // ever be executed from the UI thread
+                _target.Text += message;
+            }
         }
     }
 }
