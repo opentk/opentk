8604bd54c00755dd24613ef06d9e1525a34c412e
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-05-31 15:17:12 +0000

    Updated changelog, release notes and bumped version numbers in preparation for release.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@1822 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/Build/Properties/AssemblyInfo.cs
+++ Source/Build/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
