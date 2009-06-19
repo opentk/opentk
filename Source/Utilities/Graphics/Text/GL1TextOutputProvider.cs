089108c4310355b324ef77c6e497fb422b5352a4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-06-19 13:01:14 +0000

    GL1TextOutputProvider.Clear() should clear the display list cache.

--- Source/Utilities/Graphics/Text/GL1TextOutputProvider.cs
+++ Source/Utilities/Graphics/Text/GL1TextOutputProvider.cs
@@ -205,6 +205,9 @@ namespace OpenTK.Graphics.Text
         public void Clear()
         {
             Cache.Clear();
+			foreach (int display_list in block_cache.Keys)
+				GL.DeleteLists(display_list, 1);
+			block_cache.Clear();
         }
 
         #endregion
