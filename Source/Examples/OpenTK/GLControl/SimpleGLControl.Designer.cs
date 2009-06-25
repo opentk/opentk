258f2a4e5792de67bcfb04e3012b7e980483fec4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-06-25 22:47:59 +0000

    * OpenGL/1.1/DisplayLists.cs: Avoid Glu in favor of OpenTK.Matrix4. * OpenAL/Test/TestAudioContext.cs: Remove deprecated Alut calls. * OpenTK/GLControl/SimpleGLControl.Designer.cs: Use DockStyle.Fill instead of anchoring.

--- Source/Examples/OpenTK/GLControl/SimpleGLControl.Designer.cs
+++ Source/Examples/OpenTK/GLControl/SimpleGLControl.Designer.cs
@@ -69,11 +69,9 @@
             // 
             // glControl1
             // 
-            this.glControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+			this.glControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.glControl1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.glControl1.Location = new System.Drawing.Point(1, 0);
+            this.glControl1.Location = new System.Drawing.Point(0, 0);
             this.glControl1.Name = "glControl1";
             this.glControl1.Size = new System.Drawing.Size(629, 565);
             this.glControl1.TabIndex = 0;
