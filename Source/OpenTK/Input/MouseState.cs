4e28071a268237f4e694fa8ca74b20addf9bc517
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-06-28 10:01:54 +0000

    Added Mouse and Keyboard stubs for the new input API.

---
+++ Source/OpenTK/Input/MouseState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Input
+{
+    /// <summary>
+    /// Encapsulates the state of a mouse device.
+    /// </summary>
+    public struct MouseState : IEquatable<MouseState>
+    {
+        #region Constructors
+
+        internal MouseState(MouseButton[] buttons)
+        {
+        }
+
+        #endregion
+
+        #region IEquatable<MouseState> Members
+
+        /// <summary>
+        /// Compares two MouseState instances for equality.
+        /// </summary>
+        /// <param name="other">The instance to compare to.</param>
+        /// <returns>True, if both instances are equal; false otherwise.</returns>
+        public bool Equals(MouseState other)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
