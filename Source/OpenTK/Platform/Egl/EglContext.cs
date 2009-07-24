4d5aafcff8e6bb1520ddb498c1ad369e57e10d83
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-07-24 21:13:46 +0000

    Initial implementation.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@2011 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Platform/Egl/EglContext.cs
+++ Source/OpenTK/Platform/Egl/EglContext.cs
@@ -28,13 +28,141 @@
 using System;
 
 using OpenTK.Graphics;
+using System.Diagnostics;
 
 namespace OpenTK.Platform.Egl
 {
-    public class EglContext /*: IGraphicsContext*/
+    class EglContext : IGraphicsContext
     {
-        public EglContext()
+        #region Fields
+
+        EglWindowInfo window;
+        EGLContext context;
+        GraphicsMode mode;
+        bool vsync = true;   // Default vsync value is defined as 1 (true) in EGL.
+        bool disposed = false;
+
+        #endregion
+
+        #region Constructors
+
+        public EglContext(GraphicsMode mode, EglWindowInfo window, IGraphicsContext sharedContext,
+            int major, int minor, GraphicsContextFlags flags)
         {
+            if (mode == null)
+                throw new ArgumentNullException("mode");
+            if (window == null)
+                throw new ArgumentNullException("window");
+
+            EglContext shared = (EglContext)sharedContext;
+
+            Egl.Initialize(window.Display, out major, out minor);
+
+            EGLConfig config = new EGLConfig(mode.Index);
+
+            if (window.Surface.Handle == EGLSurface.None.Handle)
+                window.CreateWindowSurface(config);
+
+            context = Egl.CreateContext(window.Display, config, shared.context, null);
+            MakeCurrent(window);
         }
+
+        #endregion
+
+        #region IGraphicsContext Members
+
+        public void SwapBuffers()
+        {
+            Egl.SwapBuffers(window.Display, window.Surface);
+        }
+
+        public void MakeCurrent(IWindowInfo window)
+        {
+            EglWindowInfo egl = (EglWindowInfo)window;
+            Egl.MakeCurrent(egl.Display, egl.Surface, egl.Surface, context);
+            this.window = egl;
+        }
+
+        public bool IsCurrent
+        {
+            get { return Egl.GetCurrentContext().Handle == context.Handle; }
+        }
+
+        public event DestroyEvent<IGraphicsContext> Destroy;
+
+        public bool VSync
+        {
+            get
+            {
+                // Egl.GetSwapInterval does not exist, so store and return the current interval.
+                // The default interval is defined as 1 (true).
+                return vsync;
+            }
+            set
+            {
+                if (Egl.SwapInterval(window.Display, value ? 1 : 0))
+                    vsync = value;
+                else
+                    Debug.Print("[Warning] Egl.SwapInterval({0}, {1}) failed.", window.Display, value);
+            }
+        }
+
+        public void Update(IWindowInfo window)
+        {
+            // Do nothing.
+        }
+
+        public GraphicsMode GraphicsMode
+        {
+            get { return mode; }
+        }
+
+        // Todo: implement this!
+        public bool ErrorChecking
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Todo: cross-reference the specs. What should happen if the context is destroyed from a different
+        // thread?
+        void Dispose(bool manual)
+        {
+            if (!disposed)
+            {
+                if (manual)
+                {
+                    Egl.MakeCurrent(window.Display, window.Surface, window.Surface, EGLContext.None);
+                    Egl.DestroyContext(window.Display, context);
+                }
+                else
+                {
+                    Debug.Print("[Warning] {0}:{1} was not disposed.", this.GetType().Name, context.Handle);
+                }
+                disposed = true;
+            }
+        }
+
+        ~EglContext()
+        {
+            Dispose(false);
+        }
+
+        #endregion
     }
 }
