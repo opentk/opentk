503d86267ac945465b61985d899e5939ea167e14
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-07-24 21:52:01 +0000

    Marked all IPlatformFactory interface implementations as virtual.

--- Source/OpenTK/Platform/X11/X11Factory.cs
+++ Source/OpenTK/Platform/X11/X11Factory.cs
@@ -10,27 +10,27 @@ namespace OpenTK.Platform.X11
     {
         #region IPlatformFactory Members
 
-        public INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device)
+        public virtual INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device)
         {
             return new X11GLNative(x, y, width, height, title, mode, options, device);
         }
 
-        public IGLControl CreateGLControl(GraphicsMode mode, GLControl owner)
+        public virtual IGLControl CreateGLControl(GraphicsMode mode, GLControl owner)
         {
             return new X11GLControl(mode, owner);
         }
 
-        public IDisplayDeviceDriver CreateDisplayDeviceDriver()
+        public virtual IDisplayDeviceDriver CreateDisplayDeviceDriver()
         {
             return new X11XrandrDisplayDevice();
         }
 
-        public IGraphicsContext CreateGLContext(GraphicsMode mode, IWindowInfo window, IGraphicsContext shareContext, bool DirectRendering, int major, int minor, GraphicsContextFlags flags)
+        public virtual IGraphicsContext CreateGLContext(GraphicsMode mode, IWindowInfo window, IGraphicsContext shareContext, bool directRendering, int major, int minor, GraphicsContextFlags flags)
         {
-            return new X11GLContext(mode, window, shareContext, DirectRendering, major, minor, flags);
+            return new X11GLContext(mode, window, shareContext, directRendering, major, minor, flags);
         }
 
-        public OpenTK.Graphics.GraphicsContext.GetCurrentContextDelegate CreateGetCurrentGraphicsContext()
+        public virtual OpenTK.Graphics.GraphicsContext.GetCurrentContextDelegate CreateGetCurrentGraphicsContext()
         {
             return (GraphicsContext.GetCurrentContextDelegate)delegate
             {
@@ -38,12 +38,12 @@ namespace OpenTK.Platform.X11
             };
         }
 
-        public IGraphicsMode CreateGraphicsMode()
+        public virtual IGraphicsMode CreateGraphicsMode()
         {
             return new X11GraphicsMode();
         }
-        
-        public OpenTK.Input.IKeyboardDriver CreateKeyboardDriver()
+
+        public virtual OpenTK.Input.IKeyboardDriver CreateKeyboardDriver()
         {
             throw new NotImplementedException();
         }
