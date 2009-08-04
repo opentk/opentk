0a8e6d055521e86f7974b3a7d03a18012e79a9d4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-08-04 10:01:33 +0000

    Updated version information.

--- Source/Bind/Properties/AssemblyInfo.cs
+++ Source/Bind/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.9.11")]
-[assembly: AssemblyFileVersion("0.9.9.11")]
+[assembly: AssemblyVersion("0.9.9.12")]
+[assembly: AssemblyFileVersion("0.9.9.12")]
