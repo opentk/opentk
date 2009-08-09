3f009dc723eb41022e6c7a70ca7299f7471e5c9d
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-08-09 17:55:11 +0000

    Moved AutoGeneratedAttribute to root namespace (it is now used by more than just OpenTK.Graphics).

--- Source/OpenTK/AutoGeneratedAttribute.cs
+++ Source/OpenTK/AutoGeneratedAttribute.cs
@@ -29,7 +29,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace OpenTK.Graphics
+namespace OpenTK
 {
     /// <summary>
     /// Indicates that this function is generated automatically by a tool.
