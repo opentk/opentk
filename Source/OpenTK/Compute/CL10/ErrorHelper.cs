57ba374c60c85fcba8507ee4ffa101dbf43334c3
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-08-11 14:21:35 +0000

    Added generated bindings for OpenCL 1.0.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@2073 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

---
+++ Source/OpenTK/Compute/CL10/ErrorHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Compute.CL10
+{
+    struct ErrorHelper : IDisposable
+    {
+        #region Constructors
+
+        public ErrorHelper(IntPtr context)
+        {
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
