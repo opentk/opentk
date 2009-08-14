df0d9d1a4e5ed1dc8cd7b6043cdaa51dcc6d4bdb
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-08-14 12:29:30 +0000

    Added OpenTK.Compatibility project to provide an upgrade path from 0.9.8.

---
+++ Source/Compatibility/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("The Open Toolkit Compatilibity library")]
+[assembly: AssemblyDescription("Provides compatibility with previous versions of OpenTK")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The Open Toolkit Library")]
+[assembly: AssemblyProduct("The Open Toolkit Library")]
+[assembly: AssemblyCopyright("Copyright ©  2006-2009 the Open Toolkit team")]
+[assembly: AssemblyTrademark("OpenTK")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c495044-4b1a-4bff-aee9-ff9dbf85433f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.9.9.2")]
+[assembly: AssemblyFileVersion("0.9.9.2")]
+
+[assembly: System.CLSCompliant(true)]
