f927fa899b1345e0888c80a8f6a8113c709d6c04
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-09-04 21:31:31 +0000

    Added missing XML documentation.
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@2226 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Compute/CL10/ImageFormat.cs
+++ Source/OpenTK/Compute/CL10/ImageFormat.cs
@@ -32,14 +32,23 @@ using System.Runtime.InteropServices;
 
 namespace OpenTK.Compute.CL10
 {
+    /// <summary>
+    /// Defines the format of an OpenCL image.
+    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     public struct ImageFormat
     {
         ChannelOrder image_channel_order;
         ChannelType image_channel_data_type;
 
+        /// <summary>
+        /// Gets or sets a <see cref="ChannelOrder"/> value, which indicates the order of the image channels.
+        /// </summary>
         public ChannelOrder ChannelOrder { get { return image_channel_order; } set { image_channel_order = value; } }
 
+        /// <summary>
+        /// Gets or sets a <see cref="ChannelType"/> value, which indicates the data type of each image channel.
+        /// </summary>
         public ChannelType ChannelType { get { return image_channel_data_type; } set { image_channel_data_type = value; } }
     }
 }
