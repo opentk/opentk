17d0b2f8fb8e9879f5ce7ac10c4ae156bc1e5dd4
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2009-09-04 10:34:18 +0000

    * Source/Build/Properties/AssemblyInfo.cs: * Source/OpenTK/Properties/AssemblyInfo.cs: * Source/Examples/Properties/AssemblyInfo.cs: * Source/GLControl/Properties/AssemblyInfo.cs: * Source/Compatibility/Properties/AssemblyInfo.cs: Added   AssemblyKeyFile and AllowPartialTrustCallers attributes.
    * Source/OpenTK/Configuration.cs: Added [assemly: AssemblyKeyFile]
      attribute.
    Added missing license information.
    Moved [assembly: CLSCompliant] to Properies/AssemblyInfo.cs.
    
    git-svn-id: https://opentk.svn.sourceforge.net/svnroot/opentk/trunk@2216 ebc5dd9b-fb1d-0410-b6f8-d24c324e9604

--- Source/OpenTK/Configuration.cs
+++ Source/OpenTK/Configuration.cs
@@ -1,9 +1,28 @@
-﻿#region --- License ---
-/* Licensed under the MIT/X11 license.
- * Copyright (c) 2006-2008 the OpenTK Team.
- * This notice may not be removed from any source distribution.
- * See license.txt for licensing detailed licensing details.
- */
+﻿#region License
+//
+// The Open Toolkit Library License
+//
+// Copyright (c) 2006 - 2009 the Open Toolkit library.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights to 
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
 #endregion
 
 using System;
@@ -12,9 +31,7 @@ using System.Text;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
-
-[assembly: CLSCompliant(true)]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("OpenTK.Utilities")]
+using System.Reflection;
 
 namespace OpenTK
 {
