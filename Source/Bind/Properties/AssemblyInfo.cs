9d562fb68344930a4135e1510cd5b8658af1b114
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2010-10-04 21:37:33 +0000

    All projects now read common AssemblyInfo items from GlobalAssemblyInfo.cs. The various AssemblyInfo.cs files now only contain project-specific information. Assembly versions are now controlled by Build.UpdateVersion.

--- Source/Bind/Properties/AssemblyInfo.cs
+++ Source/Bind/Properties/AssemblyInfo.cs
@@ -5,29 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("The Open Toolkit Library Binding Generator")]
+[assembly: AssemblyTitle("Generator.Bind")]
 [assembly: AssemblyDescription("Generates C# bindings for the the Open Toolkit Library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Open Toolkit Library")]
-[assembly: AssemblyProduct("The Open Toolkit Library")]
-[assembly: AssemblyCopyright("Copyright ©  2006-2009 the Open Toolkit Library")]
-[assembly: AssemblyTrademark("OpenTK")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0d681958-ca78-4a67-b71c-ff8755488e23")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("0.9.9.14")]
-[assembly: AssemblyFileVersion("0.9.9.14")]
--- Source/Examples/Properties/AssemblyInfo.cs
+++ Source/Examples/Properties/AssemblyInfo.cs
@@ -7,31 +7,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenTK.Examples")]
 [assembly: AssemblyDescription("Examples showcasing OpenTK and OpenGL")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Open Toolkit Library")]
-[assembly: AssemblyProduct("The Open Toolkit Library")]
-[assembly: AssemblyCopyright("Copyright ©  2006-2010 the Open Toolkit Library")]
-[assembly: AssemblyTrademark("OpenTK")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("f236c767-678f-4c20-9282-d051a3c39657")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.201")]
-[assembly: AssemblyFileVersion("1.0.0.201")]
-
-#if SIGN_ASSEMBLY
-[assembly: AssemblyKeyFile(@"../../../OpenTK.snk")]
-#endif
