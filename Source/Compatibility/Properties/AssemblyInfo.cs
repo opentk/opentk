9d273cee7e81e96eb9bfcde284365bb219442d36
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2010-10-04 21:37:33 +0000

    All projects now read common AssemblyInfo items from GlobalAssemblyInfo.cs. The various AssemblyInfo.cs files now only contain project-specific information. Assembly versions are now controlled by Build.UpdateVersion.

--- Source/Compatibility/Properties/AssemblyInfo.cs
+++ Source/Compatibility/Properties/AssemblyInfo.cs
@@ -8,37 +8,12 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenTK.Compatibility")]
 [assembly: AssemblyDescription("Provides compatibility with previous versions of OpenTK and the Tao framework.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Open Toolkit Library")]
-[assembly: AssemblyProduct("The Open Toolkit Library")]
-[assembly: AssemblyCopyright("Copyright ©  2006-2010 the Open Toolkit Library")]
-[assembly: AssemblyTrademark("OpenTK")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7c495044-4b1a-4bff-aee9-ff9dbf85433f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.201")]
-[assembly: AssemblyFileVersion("1.0.0.201")]
 
 [assembly: System.CLSCompliant(true)]
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #if NET40
 [assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)]
-#endif
-
-#if SIGN_ASSEMBLY
-[assembly: AssemblyKeyFile(@"../../../OpenTK.snk")]
 #endif
--- Source/OpenTK/Properties/AssemblyInfo.cs
+++ Source/OpenTK/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -8,39 +8,12 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenTK")]
 [assembly: AssemblyDescription("Open source game development toolkit for .Net/Mono.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Open Toolkit Library")]
-[assembly: AssemblyProduct("The Open Toolkit Library")]
-[assembly: AssemblyCopyright("Copyright ©  2006-2010 the Open Toolkit Library")]
-[assembly: AssemblyTrademark("OpenTK")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7652241e-158d-4eb1-85f4-ed40ee356791")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.201")]
-[assembly: AssemblyFileVersion("1.0.0.201")]
 
 [assembly: CLSCompliant(true)]
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #if NET40
 [assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)]
-#endif
-
-#if SIGN_ASSEMBLY
-[assembly: AssemblyKeyFile(@"../../../OpenTK.snk")]
 #endif
