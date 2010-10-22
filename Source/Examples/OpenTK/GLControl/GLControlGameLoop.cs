202bd2a96e2a9fa21804bf31e4b4f8e5d6387a53
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2010-10-22 15:04:04 +0000

    Avoid using deprecated GLControl.GrabScreenshot() method.

--- Source/Examples/OpenTK/GLControl/GLControlGameLoop.cs
+++ Source/Examples/OpenTK/GLControl/GLControlGameLoop.cs
@@ -66,7 +66,9 @@ namespace Examples.WinForms
         void glControl_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.F12)
-                glControl.GrabScreenshot().Save("screenshot.png");
+            {
+                GrabScreenshot().Save("screenshot.png");
+            }
         }
 
         #endregion
@@ -200,6 +202,23 @@ namespace Examples.WinForms
             GL.Vertex3(1.0f, -1.0f, 1.0f);
 
             GL.End();
+        }
+
+        #endregion
+
+        #region private void GrabScreenshot()
+
+        Bitmap GrabScreenshot()
+        {
+            Bitmap bmp = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
+            System.Drawing.Imaging.BitmapData data =
+            bmp.LockBits(this.ClientRectangle, System.Drawing.Imaging.ImageLockMode.WriteOnly,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            GL.ReadPixels(0, 0, this.ClientSize.Width, this.ClientSize.Height, PixelFormat.Bgr, PixelType.UnsignedByte,
+                data.Scan0);
+            bmp.UnlockBits(data);
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            return bmp;
         }
 
         #endregion
