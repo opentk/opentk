eba9fef91344d6b06b842d4ab25b6e97551603dc
the_fiddler <the_fiddler@ebc5dd9b-fb1d-0410-b6f8-d24c324e9604>
2010-10-29 11:46:09 +0000

    Added missing files to SVN.

---
+++ Source/OpenTK/Input/IMouseDriver2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenTK.Input
+{
+    interface IMouseDriver2
+    {
+        /// <summary>
+        /// Retrieves the MouseState for the default keyboard device.
+        /// </summary>
+        /// <returns>A <see cref="OpenTK.Input.MouseState"/> structure containing the state of the mouse device.</returns>
+        MouseState GetState();
+
+        /// <summary>
+        /// Retrieves the MouseState for the specified keyboard device.
+        /// </summary>
+        /// <param name="index">The index of the keyboard device.</param>
+        /// <returns>A <see cref="OpenTK.Input.MouseState"/> structure containing the state of the mouse device.</returns>
+        MouseState GetState(int index);
+    }
+}
