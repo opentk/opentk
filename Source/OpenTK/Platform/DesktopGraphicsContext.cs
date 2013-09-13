079340f9f2e500e38b1b2c7fa4c60c36af80425b
Radek Doulik <rodo@xamarin.com>
2013-09-13 16:31:07 +0200

    added relevant part of monodroid 80bfb07b10abe26763971b2fdb91d4b343c4379a commit

--- Source/OpenTK/Platform/DesktopGraphicsContext.cs
+++ Source/OpenTK/Platform/DesktopGraphicsContext.cs
@@ -3,6 +3,7 @@
 // The Open Toolkit Library License
 //
 // Copyright (c) 2006 - 2009 the Open Toolkit library.
+// Copyright 2013 Xamarin Inc
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -33,9 +34,11 @@ namespace OpenTK.Platform
     // Provides the foundation for all desktop IGraphicsContext implementations.
     abstract class DesktopGraphicsContext : GraphicsContextBase
     {
+#if !MOBILE
         public override void LoadAll()
         {
             new OpenTK.Graphics.OpenGL.GL().LoadEntryPoints();
         }
+#endif
     }
 }
