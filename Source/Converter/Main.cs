53a449687791978328a7a9c662f9ab8b923fa0af
Radek Doulik <rodo@xamarin.com>
2013-09-18 20:13:37 +0200

    avoid BOM at the end of xml output to make Bind.exe happy

--- Source/Converter/Main.cs
+++ Source/Converter/Main.cs
@@ -1,5 +1,6 @@
 ﻿//
 // Copyright (C) 2009 the Open Toolkit (http://www.opentk.com)
+// Copyright 2013 Xamarin Inc
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -129,7 +130,7 @@ namespace CHeaderToXML
                 if (path == null)
                 {
                     out_stream = Console.Out;
-                    Console.OutputEncoding = System.Text.Encoding.UTF8;
+                    Console.OutputEncoding = new System.Text.UTF8Encoding (false);
                 }
                 else
                 {
