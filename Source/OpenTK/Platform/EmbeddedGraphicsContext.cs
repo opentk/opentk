440e3fe24821615405a21a619ff338cac6c2c2f4
Stefanos A <stapostol@gmail.com>
2013-11-08 10:39:08 +0100

    Load ES30 and OpenGL4 entry points

--- Source/OpenTK/Platform/EmbeddedGraphicsContext.cs
+++ Source/OpenTK/Platform/EmbeddedGraphicsContext.cs
@@ -38,6 +38,7 @@ namespace OpenTK.Platform
             new OpenTK.Graphics.ES10.GL().LoadEntryPoints();
             new OpenTK.Graphics.ES11.GL().LoadEntryPoints();
             new OpenTK.Graphics.ES20.GL().LoadEntryPoints();
+            new OpenTK.Graphics.ES30.GL().LoadEntryPoints();
         }
     }
 }
