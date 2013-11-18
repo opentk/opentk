ac0fab7760b8cc72fa85bc0f13cd1bbef798fab6
Stefanos A <stapostol@gmail.com>
2013-11-18 18:29:44 +0100

    More compatibility overloads for OpenGL

--- Source/OpenTK/Graphics/OpenGL/GLEnums.cs
+++ Source/OpenTK/Graphics/OpenGL/GLEnums.cs
@@ -19835,6 +19835,41 @@ namespace OpenTK.Graphics.OpenGL
     /// <summary>
     /// Used in GL.Amd.TessellationMode
     /// </summary>
+    public enum AmdVertexShaderTesselator : int
+    {
+        /// <summary>
+        /// Original was GL_SAMPLER_BUFFER_AMD = 0x9001
+        /// </summary>
+        SamplerBufferAmd = ((int)0x9001)        ,
+        /// <summary>
+        /// Original was GL_INT_SAMPLER_BUFFER_AMD = 0x9002
+        /// </summary>
+        IntSamplerBufferAmd = ((int)0x9002)        ,
+        /// <summary>
+        /// Original was GL_UNSIGNED_INT_SAMPLER_BUFFER_AMD = 0x9003
+        /// </summary>
+        UnsignedIntSamplerBufferAmd = ((int)0x9003)        ,
+        /// <summary>
+        /// Original was GL_TESSELLATION_MODE_AMD = 0x9004
+        /// </summary>
+        TessellationModeAmd = ((int)0x9004)        ,
+        /// <summary>
+        /// Original was GL_TESSELLATION_FACTOR_AMD = 0x9005
+        /// </summary>
+        TessellationFactorAmd = ((int)0x9005)        ,
+        /// <summary>
+        /// Original was GL_DISCRETE_AMD = 0x9006
+        /// </summary>
+        DiscreteAmd = ((int)0x9006)        ,
+        /// <summary>
+        /// Original was GL_CONTINUOUS_AMD = 0x9007
+        /// </summary>
+        ContinuousAmd = ((int)0x9007)        ,
+    }
+
+    /// <summary>
+    /// Used in GL.Amd.TessellationMode
+    /// </summary>
     public enum AmdVertexShaderTessellator : int
     {
         /// <summary>
