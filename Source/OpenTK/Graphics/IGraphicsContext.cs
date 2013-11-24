7a57c9c3d220eb739363ceef15d62e7e194feb69
Stefanos A. <stapostol@gmail.com>
2013-11-24 13:57:23 +0100

    Clarified GetAddress documentation

--- Source/OpenTK/Graphics/IGraphicsContext.cs
+++ Source/OpenTK/Graphics/IGraphicsContext.cs
@@ -112,12 +112,13 @@ namespace OpenTK.Graphics
         ContextHandle Context { get; }
 
         /// <summary>
-        /// Gets the address of an OpenGL extension function.
+        /// Retrieves the implementation-defined address of an OpenGL function.
         /// </summary>
         /// <param name="function">The name of the OpenGL function (e.g. "glGetString")</param>
         /// <returns>
-        /// A pointer to the specified function or IntPtr.Zero if the function isn't
-        /// available in the current opengl context.
+        /// A pointer to the specified function or an invalid pointer if the function is not
+        /// available in the current OpenGL context. The return value and calling convention
+        /// depends on the underlying platform.
         /// </returns>
         IntPtr GetAddress(string function);
     }
