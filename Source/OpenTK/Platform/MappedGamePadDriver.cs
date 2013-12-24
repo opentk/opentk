b9242c006b2e55fa293135d88fc00aef0056f9b0
Stefanos A. <stapostol@gmail.com>
2013-12-24 14:22:03 +0100

    Added MappedGamePadDriver skeleton implementation

---
+++ Source/OpenTK/Platform/MappedGamePadDriver.cs
@@ -0,0 +1,83 @@
+﻿#region License
+//
+// MappedGamePadDriver.cs
+//
+// Author:
+//       Stefanos A. <stapostol@gmail.com>
+//
+// Copyright (c) 2006-2013 Stefanos Apostolopoulos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Input;
+
+namespace OpenTK.Platform
+{
+    /// \internal
+    /// <summary>
+    /// Implements IGamePadDriver using OpenTK.Input.Joystick
+    /// and a gamepad-specific axis/button mapping.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This class supports OpenTK and is not meant to be accessed by user code.
+    /// </para>
+    /// <para>
+    /// To support gamepads on platforms that do not offer a gamepad-optimized API,
+    /// we need to use the generic OpenTK.Input.Joystick and implement a custom
+    /// mapping scheme to provide a stable mapping to OpenTK.Input.GamePad. This
+    /// class implements this mapping scheme.
+    /// </para>
+    /// </remarks>
+    class MappedGamePadDriver : IGamePadDriver
+    {
+        public GamePadState GetState(int index)
+        {
+            JoystickState joy = Joystick.GetState(index);
+            GamePadState pad = new GamePadState();
+            if (joy.IsConnected)
+            {
+                GamePadMapping mapping = new GamePadMapping();//GamePadMapping.Lookup()
+                // Todo: implement mapping
+            }
+            return pad;
+        }
+
+        public GamePadCapabilities GetCapabilities(int index)
+        {
+            JoystickCapabilities joy = Joystick.GetCapabilities(index);
+            GamePadCapabilities pad = new GamePadCapabilities();
+            if (joy.IsConnected)
+            {
+                // Todo: implement mapping
+            }
+            return pad;
+        }
+
+        public string GetName(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
