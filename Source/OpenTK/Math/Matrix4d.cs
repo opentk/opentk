a702a8180d6bc13bc9f75899430d0a2faab85d5c
Jonas Boesch <jonas.boesch@phonak.com>
2014-01-30 18:02:14 +0100

    Updated the Matrix4d CreateFromQuaternion methods to actually create a Matrix4d.
    They now mirror the Matrix4 CreateFromQuaternion methods, just using doubles and the -d classes.
    
    Previously, Matrix4d.CreateFromQuaterion actually created a Matrix4 from a single-precision Quaterion.

--- Source/OpenTK/Math/Matrix4d.cs
+++ Source/OpenTK/Math/Matrix4d.cs
@@ -890,70 +890,24 @@ namespace OpenTK
         /// Build a rotation matrix from the specified quaternion.
         /// </summary>
         /// <param name="q">Quaternion to translate.</param>
-        /// <param name="m">Matrix result.</param>
-        public static void CreateFromQuaternion(ref Quaternion q,ref Matrix4 m)
+        /// <param name="result">Matrix result.</param>
+        public static void CreateFromQuaternion(ref Quaterniond q, out Matrix4d result)
         {
-            m = Matrix4.Identity;
-            
-            float X = q.X;
-            float Y = q.Y;
-            float Z = q.Z;
-            float W = q.W;
-            
-            float xx = X * X;
-            float xy = X * Y;
-            float xz = X * Z;
-            float xw = X * W;
-            float yy = Y * Y;
-            float yz = Y * Z;
-            float yw = Y * W;
-            float zz = Z * Z;
-            float zw = Z * W;
-            
-            m.M11 = 1 - 2 * (yy + zz);
-            m.M21 = 2 * (xy - zw);
-            m.M31 = 2 * (xz + yw);
-            m.M12 = 2 * (xy + zw);
-            m.M22 = 1 - 2 * (xx + zz);
-            m.M32 = 2 * (yz - xw);
-            m.M13 = 2 * (xz - yw);
-            m.M23 = 2 * (yz + xw);
-            m.M33 = 1 - 2 * (xx + yy);
+            Vector3d axis;
+            double angle;
+            q.ToAxisAngle(out axis, out angle);
+            CreateFromAxisAngle(axis, angle, out result);
         }
-        
+
         /// <summary>
-        /// Build a rotation matrix from the specified quaternion.
+        /// Builds a rotation matrix from a quaternion.
         /// </summary>
-        /// <param name="q">Quaternion to translate.</param>
+        /// <param name="q">The quaternion to rotate by.</param>
         /// <returns>A matrix instance.</returns>
-        public static Matrix4 CreateFromQuaternion(ref Quaternion q)
+        public static Matrix4d CreateFromQuaternion(Quaterniond q)
         {
-            Matrix4 result = Matrix4.Identity;
-            
-            float X = q.X;
-            float Y = q.Y;
-            float Z = q.Z;
-            float W = q.W;
-            
-            float xx = X * X;
-            float xy = X * Y;
-            float xz = X * Z;
-            float xw = X * W;
-            float yy = Y * Y;
-            float yz = Y * Z;
-            float yw = Y * W;
-            float zz = Z * Z;
-            float zw = Z * W;
-            
-            result.M11 = 1 - 2 * (yy + zz);
-            result.M21 = 2 * (xy - zw);
-            result.M31 = 2 * (xz + yw);
-            result.M12 = 2 * (xy + zw);
-            result.M22 = 1 - 2 * (xx + zz);
-            result.M32 = 2 * (yz - xw);
-            result.M13 = 2 * (xz - yw);
-            result.M23 = 2 * (yz + xw);
-            result.M33 = 1 - 2 * (xx + yy);
+            Matrix4d result;
+            CreateFromQuaternion(ref q, out result);
             return result;
         }
         
