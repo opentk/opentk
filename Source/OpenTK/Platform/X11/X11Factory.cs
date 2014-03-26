ad3fa9280c010d10dd3d77a625a5a9d22b23fc51
thefiddler <stapostol@gmail.com>
2014-03-26 01:33:56 +0100

    [SDL] Call SDL.GL.SetAttributes before window construction
    SDL2 requires OpenGL attributes to be set before the window is
    constructed. Without this, context construction on Android will fail.

--- Source/OpenTK/Platform/X11/X11Factory.cs
+++ Source/OpenTK/Platform/X11/X11Factory.cs
@@ -66,7 +66,7 @@ namespace OpenTK.Platform.X11
 
         #region IPlatformFactory Members
 
-        public override INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device)
+        public override INativeWindow CreateNativeWindow(int x, int y, int width, int height, string title, GraphicsMode mode, GameWindowFlags options, DisplayDevice device, int major, int minor, GraphicsContextFlags flags)
         {
             return new X11GLNative(x, y, width, height, title, mode, options, device);
         }
