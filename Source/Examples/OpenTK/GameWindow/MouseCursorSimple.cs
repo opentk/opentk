51567e610c8e49ee44410bd5893bfc452aca273e
thefiddler <stapostol@gmail.com>
2014-04-27 19:43:16 +0200

    [Examples] Set a predefined cursor on Key.Space
    Custom cursor are not enabled for OpenTK 1.1.2. They will be part of
    OpenTK 1.1.3.

--- Source/Examples/OpenTK/GameWindow/MouseCursorSimple.cs
+++ Source/Examples/OpenTK/GameWindow/MouseCursorSimple.cs
@@ -37,7 +37,8 @@ namespace Examples.Tutorial
                     offset, rgba, y * stride, stride);
             }
             
-            this.Cursor = new OpenTK.MouseCursor(rgba, bitmap.Width, bitmap.Height, 0, 0);
+            //this.Cursor = new OpenTK.MouseCursor(rgba, bitmap.Width, bitmap.Height, 0, 0);
+            this.Cursor = MouseCursor.Default;
         }
 
         #region Keyboard_KeyDown
@@ -50,13 +51,29 @@ namespace Examples.Tutorial
         void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
         {
             if (e.Key == Key.Escape)
+            {
                 this.Exit();
+            }
 
-            if (e.Key == Key.F11)
+            if (e.Key == Key.Enter && e.Alt)
+            {
                 if (this.WindowState == WindowState.Fullscreen)
                     this.WindowState = WindowState.Normal;
                 else
                     this.WindowState = WindowState.Fullscreen;
+            }
+
+            if (e.Key == Key.Space)
+            {
+                if (Cursor == MouseCursor.Default)
+                {
+                    Cursor = MouseCursor.Empty;
+                }
+                else
+                {
+                    Cursor = MouseCursor.Default;
+                }
+            }
         }
 
         #endregion
