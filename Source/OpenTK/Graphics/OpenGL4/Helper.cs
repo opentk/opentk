ffd7682eebc99fe5d157eb86c19a5fd86bc413f2
thefiddler <stapostol@gmail.com>
2014-07-21 16:53:24 +0200

    [ES][GL] Added missing documentation comments

--- Source/OpenTK/Graphics/OpenGL4/Helper.cs
+++ Source/OpenTK/Graphics/OpenGL4/Helper.cs
@@ -458,18 +458,51 @@ namespace OpenTK.Graphics.OpenGL4
         #endregion
     }
 
-    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
-    public delegate void DebugProc(
-        DebugSource source, DebugType type, int id,
-        DebugSeverity severity, int length, IntPtr message,
-        IntPtr userParam);
-
+    /// <summary>
+    /// Defines the signature of a debug callback for 
+    /// <see cref="GL.Arb.DebugMessageCallback"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="DebugSource"/> for this debug message.</param>
+    /// <param name="type">The <see cref="DebugType"/> for this debug message.</param>
+    /// <param name="id">The id of this debug message.</param>
+    /// <param name="severity">The <see cref="DebugSeverity"/> for this debug message.</param>
+    /// <param name="length">The length of this debug message.</param>
+    /// <param name="message">A pointer to a null-terminated ASCII C string, representing the content of this debug message.</param>
+    /// <param name="userParam">A pointer to a user-specified parameter.</param>
     [UnmanagedFunctionPointer(CallingConvention.Winapi)]
     public delegate void DebugProcArb(
         DebugSource source, DebugType type, int id,
         DebugSeverity severity, int length, IntPtr message,
         IntPtr userParam);
 
+    /// <summary>
+    /// Defines the signature of a debug callback for 
+    /// <see cref="GL.DebugMessageCallback"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="DebugSource"/> for this debug message.</param>
+    /// <param name="type">The <see cref="DebugType"/> for this debug message.</param>
+    /// <param name="id">The id of this debug message.</param>
+    /// <param name="severity">The <see cref="DebugSeverity"/> for this debug message.</param>
+    /// <param name="length">The length of this debug message.</param>
+    /// <param name="message">A pointer to a null-terminated ASCII C string, representing the content of this debug message.</param>
+    /// <param name="userParam">A pointer to a user-specified parameter.</param>
+    [UnmanagedFunctionPointer(CallingConvention.Winapi)]
+    public delegate void DebugProc(
+        DebugSource source, DebugType type, int id,
+        DebugSeverity severity, int length, IntPtr message,
+        IntPtr userParam);
+
+    /// <summary>
+    /// Defines the signature of a debug callback for 
+    /// <see cref="GL.Khr.DebugMessageCallback"/>.
+    /// </summary>
+    /// <param name="source">The <see cref="DebugSource"/> for this debug message.</param>
+    /// <param name="type">The <see cref="DebugType"/> for this debug message.</param>
+    /// <param name="id">The id of this debug message.</param>
+    /// <param name="severity">The <see cref="DebugSeverity"/> for this debug message.</param>
+    /// <param name="length">The length of this debug message.</param>
+    /// <param name="message">A pointer to a null-terminated ASCII C string, representing the content of this debug message.</param>
+    /// <param name="userParam">A pointer to a user-specified parameter.</param>
     [UnmanagedFunctionPointer(CallingConvention.Winapi)]
     public delegate void DebugProcKhr(
         DebugSource source, DebugType type, int id,
