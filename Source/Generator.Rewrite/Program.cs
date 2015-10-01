13f79437d51a7a696495d269fefae8671059263c
Fraser Waters <frassle@gmail.com>
2015-10-01 12:19:21 +0100

    [Bind] Declare key file is only read
    Partly fixes #284

--- Source/Generator.Rewrite/Program.cs
+++ Source/Generator.Rewrite/Program.cs
@@ -82,7 +82,7 @@ namespace OpenTK.Rewrite
             if (!String.IsNullOrEmpty(keyfile) && File.Exists(keyfile))
             {
                 keyfile = Path.GetFullPath(keyfile);
-                var fs = new FileStream(keyfile, FileMode.Open);
+                var fs = new FileStream(keyfile, FileMode.Open, FileAccess.Read);
                 var keypair = new System.Reflection.StrongNameKeyPair(fs);
                 fs.Close();
                 write_params.StrongNameKeyPair = keypair;
