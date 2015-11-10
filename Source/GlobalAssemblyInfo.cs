1ef8c0f2b48b5f3f191a351bacb19f67b131ae8f
smoogipooo <smoogipooo@gmail.com>
2015-11-10 16:51:36 +1030

    [p] Try committing GlobalAssemblyInfo.

---
+++ Source/GlobalAssemblyInfo.cs
@@ -0,0 +1,15 @@
+// This file is auto-generated through Source/Build.Tasks/GenerateAssemblyInfo.cs.
+// Do not edit by hand!
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("The Open Toolkit Library")]
+[assembly: AssemblyProduct("The Open Toolkit Library")]
+[assembly: AssemblyCopyright("Copyright © 2006 - 2014 the Open Toolkit Library")]
+[assembly: AssemblyTrademark("OpenTK")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.2139.0")]
