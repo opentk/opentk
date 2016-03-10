678e571574aff0d094b30b210f4c8e0463a67efb
Justin <justin@agnomendesign.com>
2016-03-10 10:55:30 +0200

    Fixed warning & compile error

--- Source/OpenTK/Math/BezierCurveQuadric.cs
+++ Source/OpenTK/Math/BezierCurveQuadric.cs
@@ -2,6 +2,8 @@
 // This file is subject to the terms and conditions defined in
 // file 'License.txt', which is part of this source code package.
 
+using System;
+
 namespace OpenTK
 {
     /// <summary>
