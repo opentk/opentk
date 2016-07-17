7ca257f4b74b637a68c4313e82b411dc4eea1cdd
varon <varon@users.noreply.github.com>
2016-07-17 23:12:35 +0200

    Added initial parts of build framework

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,4 @@
+### 2.0.0 - alpha
+	* Moved to new FAKE/Paket based build system
+	* Removed superfluous release configurations
+	* Numerous other fixes and enhancements
---
+++ build.cmd
@@ -0,0 +1,18 @@
+@echo off
+cls
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+IF NOT EXIST build.fsx (
+  .paket\paket.exe update
+  packages\FAKE\tools\FAKE.exe init.fsx
+)
+packages\FAKE\tools\FAKE.exe build.fsx %*
