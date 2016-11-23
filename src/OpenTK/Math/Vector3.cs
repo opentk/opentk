c78a6c78444214e590d4e6487ba331c9b26b0119
Christopher Lees <leezer3@gmail.com>
2016-11-23 14:42:29 +0000

    Fix wrong XML param name tag

--- src/OpenTK/Math/Vector3.cs
+++ src/OpenTK/Math/Vector3.cs
@@ -1317,7 +1317,7 @@ namespace OpenTK
         /// <param name="height">The height of the viewport.</param>
         /// <param name="minZ">The minimum depth of the viewport.</param>
         /// <param name="maxZ">The maximum depth of the viewport.</param>
-        /// <param name="worldViewProjection">The inverse of the world-view-projection matrix.</param>
+        /// <param name="inverseWorldViewProjection">The inverse of the world-view-projection matrix.</param>
         /// <returns>The vector in object space.</returns>
         /// <remarks>
         /// To project from normalized device coordinates (NDC) use the following parameters:
