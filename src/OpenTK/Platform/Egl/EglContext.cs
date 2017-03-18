30752ed36d4c8dd8cf85817834565071568075bf
Armin Sander <armin@replicator.org>
2017-03-18 14:06:27 +0100

    ANGLE: keep the backbuffer size updated when the window resizes

--- src/OpenTK/Platform/Egl/EglContext.cs
+++ src/OpenTK/Platform/Egl/EglContext.cs
@@ -171,6 +171,18 @@ namespace OpenTK.Platform.Egl
             }
         }
 
+        public override void Update(IWindowInfo window)
+        {
+            // ANGLE updates the width and height of the back buffer surfaces in the WaitClient function.
+            // So without this calling this function, the surface won't match the size of the window after it 
+            // was resized.
+            // https://bugs.chromium.org/p/angleproject/issues/detail?id=1438
+            if (!Egl.WaitClient())
+            {
+                Debug.Print("[Warning] Egl.WaitClient() failed. Error: {0}", Egl.GetError());
+            }
+        }
+
         #endregion
 
         #region IGraphicsContextInternal Members
