79ec4c89a314a00417ab441b143e5e8097d49e99
varon <varon@users.noreply.github.com>
2017-03-19 15:35:46 +0200

    Enhance testing; add more generators; add testing for approxEqual

--- tests/OpenTK.Tests/Vectors.fs
+++ tests/OpenTK.Tests/Vectors.fs
@@ -6,38 +6,8 @@ open FsCheck.Xunit
 open System
 open OpenTK
 
-[<RequireQualifiedAccess>]
-module internal Generators = 
-    let private isValidFloat f = not (Single.IsNaN f || Single.IsInfinity f)
-    
-    let Vec2 = 
-        Arb.generate<float32>
-        |> Gen.filter isValidFloat
-        |> Gen.two
-        |> Gen.map Vector2
-        |> Arb.fromGen
-    
-    let Vec3 = 
-        Arb.generate<float32>
-        |> Gen.filter isValidFloat
-        |> Gen.three
-        |> Gen.map Vector3
-        |> Arb.fromGen
-    
-    let Vec4 = 
-        Arb.generate<float32>
-        |> Gen.filter isValidFloat
-        |> Gen.four
-        |> Gen.map Vector4
-        |> Arb.fromGen
-
-type VectorGen = 
-    static member Vector2() = Generators.Vec2
-    static member Vector3() = Generators.Vec3
-    static member Vector4() = Generators.Vec4
-
 module Vector2 = 
-    [<Properties(Arbitrary = [| typeof<VectorGen> |])>]
+    [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module ``Simple Properties`` = 
         //
         [<Property>]
@@ -50,28 +20,28 @@ module Vector2 =
             //
             Assert.True(a.Length >= 0.0f)
     
-    [<Properties(Arbitrary = [| typeof<VectorGen> |])>]
+    [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module Addition = 
         //
         [<Property>]
         let ``Vector addition is the same as component addition`` (a : Vector2,b : Vector2) = 
             let c = a + b
-            Assert.Equal(a.X + b.X,c.X)
-            Assert.Equal(a.Y + b.Y,c.Y)
+            Assert.ApproximatelyEqual(a.X + b.X,c.X)
+            Assert.ApproximatelyEqual(a.Y + b.Y,c.Y)
         
         [<Property>]
         let ``Vector addition is commutative`` (a : Vector2,b : Vector2) = 
             let c = a + b
             let c2 = b + a
-            Assert.Equal(c,c2)
+            Assert.ApproximatelyEqual(c,c2)
         
         [<Property>]
         let ``Vector addition is associative`` (a : Vector2,b : Vector2,c : Vector2) = 
             let r1 = (a + b) + c
             let r2 = a + (b + c)
-            Assert.Equal(r1,r2)
+            Assert.ApproximatelyEqual(r1,r2)
     
-    [<Properties(Arbitrary = [| typeof<VectorGen> |])>]
+    [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module Multiplication = 
         //
         [<Property>]
@@ -92,7 +62,7 @@ module Vector2 =
             Assert.Equal(a.X * f,r.X)
             Assert.Equal(a.Y * f,r.Y)
     
-    [<Properties(Arbitrary = [| typeof<VectorGen> |])>]
+    [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module Subtraction = 
         //
         [<Property>]
@@ -101,12 +71,11 @@ module Vector2 =
             Assert.Equal(a.X - b.X,c.X)
             Assert.Equal(a.Y - b.Y,c.Y)
     
-    [<Properties(Arbitrary = [| typeof<VectorGen> |])>]
+    [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module Division = 
         //
         [<Property>]
         let ``Vector-float division is the same as component-float division`` (a : Vector2,f : float32) = 
-            if f <> 0.0f then 
-                let r = a / f
-                Assert.Equal(a.X / f,r.X)
-                Assert.Equal(a.Y / f,r.Y)
+            let r = a / f
+            Assert.ApproximatelyEqual(a.X / f,r.X)
+            Assert.ApproximatelyEqual(a.Y / f,r.Y)
