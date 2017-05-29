dd0de0a75bda6d6e8bae3a5c7f7dbab49865e6f9
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-05-29 19:47:00 +0200

    Added additional identifier constraints.

--- src/Generator.Rewrite/GeneratedVariableIdentifier.cs
+++ src/Generator.Rewrite/GeneratedVariableIdentifier.cs
@@ -1,4 +1,5 @@
 using System;
+using Mono.Cecil.Cil;
 
 namespace OpenTK.Rewrite
 {
@@ -9,35 +10,47 @@ namespace OpenTK.Rewrite
     public class GeneratedVariableIdentifier
     {
         /// <summary>
+        /// The <see cref="MethodBody"/> which the variable is in.
+        /// </summary>
+        public MethodBody Body { get; }
+        
+        /// <summary>
+        /// The <see cref="VariableDefinition"/> which the variable idetifier maps to.
+        /// </summary>
+        public VariableDefinition Definition { get; }
+
+        /// <summary>
         /// The name of the generated variable.
         /// </summary>
         public string Name { get; }
-        
-        /// <summary>
-        /// The index of the generated variable in its method.
-        /// </summary>
-        public int Index { get; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="GeneratedVariableIdentifier"/> class.
         /// </summary>
+        /// <param name="body">The method body which the variable is in.</param>
         /// <param name="name">The name of the generated variable.</param>
         /// <param name="index">The index of the generated variable in its method.</param>
         /// <exception cref="ArgumentException"></exception>
-        public GeneratedVariableIdentifier(string name, int index)
+        public GeneratedVariableIdentifier(MethodBody body, VariableDefinition definition, string name)
         {
+            if (body == null)
+            {
+                throw new ArgumentException("The body argument cannot be null.", nameof(body));
+            }
+
+            if (definition == null)
+            {
+                throw new ArgumentException("The definition argument cannot be null.", nameof(body));
+            }
+
             if (string.IsNullOrEmpty(name))
             {
                 throw new ArgumentException("The name argument cannot be null or empty", nameof(name));
             }
 
-            if (index < 0)
-            {
-                throw new ArgumentException("The index must be greater than zero.", nameof(index));
-            }
-            
+            this.Body = body;
+            this.Definition = definition;
             this.Name = name;
-            this.Index = index;
         }
     }
 }
