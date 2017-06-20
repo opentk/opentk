5f9bf7fea3eaa5339427414ed6b70c7b00a38980
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-06-20 15:48:04 +0200

    Removed obsolete compatibility properties.

--- src/OpenTK/Input/MouseDevice.cs
+++ src/OpenTK/Input/MouseDevice.cs
@@ -8,7 +8,7 @@
  //
  // Permission is hereby granted, free of charge, to any person obtaining a copy
  // of this software and associated documentation files (the "Software"), to deal
- // in the Software without restriction, including without limitation the rights to 
+ // in the Software without restriction, including without limitation the rights to
  // use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  // the Software, and to permit persons to whom the Software is furnished to do
  // so, subject to the following conditions:
@@ -75,7 +75,7 @@ namespace OpenTK.Input
         #region public InputDeviceType DeviceType
 
         /// <summary>
-        /// Gets a value indicating the InputDeviceType of this InputDevice. 
+        /// Gets a value indicating the InputDeviceType of this InputDevice.
         /// </summary>
         public InputDeviceType DeviceType
         {
@@ -301,68 +301,6 @@ namespace OpenTK.Input
         }
 
         #endregion
-
-        #endregion
-
-        #region COMPAT_REV1519
-
-#if COMPAT_REV1519
-
-        #region public int WheelDelta
-
-        /// <summary>
-        /// Gets an integer representing the relative wheel movement.
-        /// </summary>
-        [Obsolete("WheelDelta is only defined for a single WheelChanged event.  Use the OpenTK.Input.MouseWheelEventArgs::Delta property with the OpenTK.Input.MouseDevice::WheelChanged event.", false)]
-        public int WheelDelta
-        {
-            get
-            {
-                int result = (int)Math.Round(state.WheelPrecise - wheel_last_accessed, MidpointRounding.AwayFromZero);
-                wheel_last_accessed = state.Wheel;
-                return result;
-            }
-        }
-
-        #endregion
-
-        #region public int XDelta
-
-        /// <summary>
-        /// Gets an integer representing the relative x movement of the pointer, in pixel coordinates.
-        /// </summary>
-        [Obsolete("XDelta is only defined for a single Move event.  Use the OpenTK.Input.MouseMoveEventArgs::Delta property with the OpenTK.Input.MouseDevice::Move event.", false)]
-        public int XDelta
-        {
-            get
-            {
-                int result = state.X - pos_last_accessed.X;
-                pos_last_accessed.X = state.X;
-                return result;
-            }
-        }
-
-        #endregion
-
-        #region public int YDelta
-
-        /// <summary>
-        /// Gets an integer representing the relative y movement of the pointer, in pixel coordinates.
-        /// </summary>
-        [Obsolete("YDelta is only defined for a single Move event.  Use the OpenTK.Input.MouseMoveEventArgs::Delta property with the OpenTK.Input.MouseDevice::Move event.", false)]
-        public int YDelta
-        {
-            get
-            {
-                int result = state.Y - pos_last_accessed.Y;
-                pos_last_accessed.Y = state.Y;
-                return result;
-            }
-        }
-
-        #endregion
-
-#endif
 
         #endregion
     }
