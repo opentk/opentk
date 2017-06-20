7601a86f6dbd2d5303b807492923d8c65aa501a4
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-06-20 15:41:39 +0200

    Removed obsolete compatibiliy fields.

--- src/OpenTK/Graphics/GraphicsBindingsBase.cs
+++ src/OpenTK/Graphics/GraphicsBindingsBase.cs
@@ -6,7 +6,7 @@
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights to 
+// in the Software without restriction, including without limitation the rights to
 // use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 // the Software, and to permit persons to whom the Software is furnished to do
 // so, subject to the following conditions:
@@ -36,22 +36,6 @@ namespace OpenTK.Graphics
     /// </summary>
     public abstract class GraphicsBindingsBase : BindingsBase
     {
-        /// <summary>
-        /// Contains the list of API entry points (function pointers).
-        /// This field must be set by an inheriting class.
-        /// </summary>
-        [Obsolete("Not used - this field remains for 1.1 API compatibility")]
-        protected IntPtr[] EntryPointsInstance;
-
-        /// <summary>
-        /// with the 1.1 API.
-        /// Contains the list of API entry point names.
-        /// This field must be set by an inheriting class.
-        /// </summary>
-        [Obsolete("Not used - this field remains for 1.1 API compatibility")]
-        protected string[] EntryPointNamesInstance;
-
-
         internal IntPtr[] _EntryPointsInstance;
         internal byte[] _EntryPointNamesInstance;
         internal int[] _EntryPointNameOffsetsInstance;
