d3e831038dc8573869f750b129b602ec1ee7cfbb
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-06-20 15:54:57 +0200

    Removed obsolete mouse driver.

--- src/OpenTK/Input/IInputDriver.cs
+++ src/OpenTK/Input/IInputDriver.cs
@@ -14,7 +14,7 @@ namespace OpenTK.Input
     /// Defines the interface for an input driver.
     /// </summary>
     [Obsolete("This interface has been replaced by OpenTK.Input.Keyboard, Mouse, Joystick and GamePad.")]
-    public interface IInputDriver : IKeyboardDriver, IMouseDriver, IJoystickDriver, IDisposable
+    public interface IInputDriver : IKeyboardDriver, IJoystickDriver, IDisposable
     {
         /// <summary>
         /// Updates the state of the driver.
