9386ca769d0b665899fbe9c04a7ec070cdb9ade8
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-07-27 11:58:30 +0200

    Add check for MyGet as the build runner and strip trailing newlines.

--- build.cmd
+++ build.cmd
@@ -15,4 +15,9 @@ IF NOT EXIST build.fsx (
   .paket\paket.exe update
   packages\FAKE\tools\FAKE.exe init.fsx
 )
-packages\FAKE\tools\FAKE.exe build.fsx %*
+
+if "%BuildRunner%" == "MyGet" (
+  SET BuildTarget=NuGet
+)
+
+packages\FAKE\tools\FAKE.exe build.fsx %* %BuildTarget%
