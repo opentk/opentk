ead44aaf8054be057f392ce112daf8fa402c3b2f
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-08-04 13:58:39 +0200

    Add comment explaining the release notes overwrite.

--- build.cmd
+++ build.cmd
@@ -20,6 +20,7 @@ SET BuildTarget=
 if "%BuildRunner%" == "MyGet" (
   SET BuildTarget=NuGet
 
+  :: Replace the existing release notes file with one for this build only
   echo ### %PackageVersion% > RELEASE_NOTES.md
   echo 	* git build >> RELEASE_NOTES.md
 )
