ffa26fb20447fc246b877f274e42b552cc8c02b1
Fraser Waters <frassle@gmail.com>
2017-10-05 12:33:19 +0100

    Add feature to Bind to generate legacy array overloads

--- src/Generator.Bind/Utilities.cs
+++ src/Generator.Bind/Utilities.cs
@@ -94,6 +94,10 @@ namespace Bind
         /// Add an int32 overload for convenience.
         /// </summary>
         SizeParameter = 1 << 14,
+        /// <summary>
+        /// Function takes a ref but we emit a legacy array overload to maintain backwards compatability.
+        /// </summary>
+        LegacyArrayParameter = 1 << 15,
     }
 
     internal static class Utilities
