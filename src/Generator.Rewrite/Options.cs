24b09303814591dc5dacbba3547c76610b4e4cdb
VperuS <vperus@gmail.com>
2017-11-20 06:14:44 +0200

    Apply required changes

--- src/Generator.Rewrite/Options.cs
+++ src/Generator.Rewrite/Options.cs
@@ -9,28 +9,28 @@ namespace OpenTK.Rewrite
     public class Options
     {
         /// <summary>
-        /// Set the path to the target assembly that should be rewritten.
+        /// Gets or sets the path to the target assembly that should be rewritten.
         /// </summary>
         [Option('a', "assembly", Required = true,
             HelpText = "The path to the target assembly that should be rewritten.")]
         public string TargetAssembly { get; set; }
 
         /// <summary>
-        /// Set the path to the strong name key which should be used to sign or resign the assembly.
+        /// Get or sets the path to the strong name key which should be used to sign or resign the assembly.
         /// </summary>
         [Option('k', "signing-key",
             HelpText = "The path to the strong name key which should be used to sign or resign the assembly.")]
         public string StrongNameKey { get; set; }
 
         /// <summary>
-        /// Enable calls to GL.GetError(), wrapped around each native call.
+        /// Gets or sets a value indicating whether enable calls to GL.GetError(), wrapped around each native call.
         /// </summary>
         [Option('d', "debug", Default = false,
             HelpText = "Enable calls to GL.GetError(), wrapped around each native call.")]
         public bool EnableDebugCalls { get; set; }
 
         /// <summary>
-        /// Force native calls to use DllImport instead of GetProcAddress.
+        /// Gets or sets a value indicating whether force native calls to use DllImport instead of GetProcAddress.
         /// </summary>
         [Option("dllimport", Default = false,
             HelpText = "Force native calls to use DllImport instead of GetProcAddress.")]
