d93011937c49ceda0840ab8bab702f458151c564
Unknown <thomas.dorsch@gmx.org>
2017-12-11 21:08:05 +0100

    Add c# test project skeleton for unit testing

---
+++ tests/OpenTK.Tests.CSharp/Quaternion_Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+namespace OpenTK.Tests.CSharp
+{
+	[TestFixture()]
+	public class Quaternion_Tests
+	{
+		[Test()]
+		public void TestCase()
+		{
+			Assert.AreEqual(1, 1);
+		}
+	}
+}
+
