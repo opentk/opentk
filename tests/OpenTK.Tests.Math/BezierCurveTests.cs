ddd77a70b73d18c42ba0c7f3c46a5b41db8dc28f
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-01-04 14:54:08 +0100

    Add example usage to bezier curve tests.

--- tests/OpenTK.Tests.Math/BezierCurveTests.cs
+++ tests/OpenTK.Tests.Math/BezierCurveTests.cs
@@ -1,16 +1,38 @@
-﻿using Xunit;
+﻿using FsCheck.Xunit;
+using OpenTK.Tests.Generators;
+using Xunit;
+using Assert = Xunit.Assert;
 
-namespace OpenTK.Tests.Unit
+namespace OpenTK.Tests.Math
 {
     public class BezierCurveTests
     {
-        [Fact]
-        public void StraightLine()
+        [Properties(Arbitrary = new[] { typeof(OpenTKGen) })]
+        public class Constructor
         {
-            var curve = new BezierCurve(Vector2.Zero, Vector2.One);
-            Assert.Equal(Vector2.Zero, curve.CalculatePoint(0.0f));
-            Assert.Equal(Vector2.One, curve.CalculatePoint(1.0f));
-            Assert.Equal(new Vector2(0.5f, 0.5f), curve.CalculatePoint(0.5f));
+            [Property]
+            public void AcceptingVector2EnumerableCreatesCurveContainingThePointsInTheEnumerable
+                (Vector2 a, Vector2 b, Vector2 c, Vector2 d)
+            {
+                var expected = new[] { a, b, c, d };
+
+                var curve = new BezierCurve(expected);
+
+                var actual = curve.Points;
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        public class CalculatePoint
+        {
+            [Fact]
+            public void StraightLine()
+            {
+                var curve = new BezierCurve(Vector2.Zero, Vector2.One);
+                Assert.Equal(Vector2.Zero, curve.CalculatePoint(0.0f));
+                Assert.Equal(Vector2.One, curve.CalculatePoint(1.0f));
+                Assert.Equal(new Vector2(0.5f, 0.5f), curve.CalculatePoint(0.5f));
+            }
         }
     }
 }
