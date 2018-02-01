c701843ebf00ae63756027eff8770818dfa450e3
CallumDev <callum.mcging@gmail.com>
2018-02-01 22:48:13 +1030

    Move unsafe block

--- src/OpenTK/Math/Matrix4.cs
+++ src/OpenTK/Math/Matrix4.cs
@@ -1236,139 +1236,143 @@ namespace OpenTK
         /// <param name="mat">The matrix to invert</param>
         /// <param name="result">The inverse of the given matrix if it has one, or the input if it is singular</param>
         /// <exception cref="InvalidOperationException">Thrown if the Matrix4 is singular.</exception>
-        public static unsafe void Invert(ref Matrix4 mat, out Matrix4 result)
+        public static void Invert(ref Matrix4 mat, out Matrix4 result)
         {
             result = mat;
-            float* inv = stackalloc float [16];
+            unsafe
+            {
+                float* inv = stackalloc float[16];
 
-            fixed (Matrix4* m0 = &mat, m1 = &result) {
-                var m = (float*)m0;
-                var invOut = (float*)m1;
-                float det;
-                inv [0] = m [5] * m [10] * m [15] -
-                         m [5] * m [11] * m [14] -
-                         m [9] * m [6] * m [15] +
-                         m [9] * m [7] * m [14] +
-                         m [13] * m [6] * m [11] -
-                         m [13] * m [7] * m [10];
-
-                inv [4] = -m [4] * m [10] * m [15] +
-                          m [4] * m [11] * m [14] +
-                          m [8] * m [6] * m [15] -
-                          m [8] * m [7] * m [14] -
-                          m [12] * m [6] * m [11] +
-                          m [12] * m [7] * m [10];
-
-                inv [8] = m [4] * m [9] * m [15] -
-                         m [4] * m [11] * m [13] -
-                         m [8] * m [5] * m [15] +
-                         m [8] * m [7] * m [13] +
-                         m [12] * m [5] * m [11] -
-                         m [12] * m [7] * m [9];
-
-                inv [12] = -m [4] * m [9] * m [14] +
-                           m [4] * m [10] * m [13] +
-                           m [8] * m [5] * m [14] -
-                           m [8] * m [6] * m [13] -
-                           m [12] * m [5] * m [10] +
-                           m [12] * m [6] * m [9];
-
-                inv [1] = -m [1] * m [10] * m [15] +
-                          m [1] * m [11] * m [14] +
-                          m [9] * m [2] * m [15] -
-                          m [9] * m [3] * m [14] -
-                          m [13] * m [2] * m [11] +
-                          m [13] * m [3] * m [10];
-
-                inv [5] = m [0] * m [10] * m [15] -
-                         m [0] * m [11] * m [14] -
-                         m [8] * m [2] * m [15] +
-                         m [8] * m [3] * m [14] +
-                         m [12] * m [2] * m [11] -
-                         m [12] * m [3] * m [10];
-
-                inv [9] = -m [0] * m [9] * m [15] +
-                          m [0] * m [11] * m [13] +
-                          m [8] * m [1] * m [15] -
-                          m [8] * m [3] * m [13] -
-                          m [12] * m [1] * m [11] +
-                          m [12] * m [3] * m [9];
-
-                inv [13] = m [0] * m [9] * m [14] -
-                          m [0] * m [10] * m [13] -
-                          m [8] * m [1] * m [14] +
-                          m [8] * m [2] * m [13] +
-                          m [12] * m [1] * m [10] -
-                          m [12] * m [2] * m [9];
-
-                inv [2] = m [1] * m [6] * m [15] -
-                         m [1] * m [7] * m [14] -
-                         m [5] * m [2] * m [15] +
-                         m [5] * m [3] * m [14] +
-                         m [13] * m [2] * m [7] -
-                         m [13] * m [3] * m [6];
-
-                inv [6] = -m [0] * m [6] * m [15] +
-                          m [0] * m [7] * m [14] +
-                          m [4] * m [2] * m [15] -
-                          m [4] * m [3] * m [14] -
-                          m [12] * m [2] * m [7] +
-                          m [12] * m [3] * m [6];
-
-                inv [10] = m [0] * m [5] * m [15] -
-                          m [0] * m [7] * m [13] -
-                          m [4] * m [1] * m [15] +
-                          m [4] * m [3] * m [13] +
-                          m [12] * m [1] * m [7] -
-                          m [12] * m [3] * m [5];
-
-                inv [14] = -m [0] * m [5] * m [14] +
-                           m [0] * m [6] * m [13] +
-                           m [4] * m [1] * m [14] -
-                           m [4] * m [2] * m [13] -
-                           m [12] * m [1] * m [6] +
-                           m [12] * m [2] * m [5];
-
-                inv [3] = -m [1] * m [6] * m [11] +
-                          m [1] * m [7] * m [10] +
-                          m [5] * m [2] * m [11] -
-                          m [5] * m [3] * m [10] -
-                          m [9] * m [2] * m [7] +
-                          m [9] * m [3] * m [6];
-
-                inv [7] = m [0] * m [6] * m [11] -
-                         m [0] * m [7] * m [10] -
-                         m [4] * m [2] * m [11] +
-                         m [4] * m [3] * m [10] +
-                         m [8] * m [2] * m [7] -
-                         m [8] * m [3] * m [6];
-
-                inv [11] = -m [0] * m [5] * m [11] +
-                           m [0] * m [7] * m [9] +
-                           m [4] * m [1] * m [11] -
-                           m [4] * m [3] * m [9] -
-                           m [8] * m [1] * m [7] +
-                           m [8] * m [3] * m [5];
-
-                inv [15] = m [0] * m [5] * m [10] -
-                          m [0] * m [6] * m [9] -
-                          m [4] * m [1] * m [10] +
-                          m [4] * m [2] * m [9] +
-                          m [8] * m [1] * m [6] -
-                          m [8] * m [2] * m [5];
-
-                det = m [0] * inv [0] + m [1] * inv [4] + m [2] * inv [8] + m [3] * inv [12];
-
-                if (det == 0) 
+                fixed (Matrix4* m0 = &mat, m1 = &result)
                 {
-                    throw new InvalidOperationException ("Matrix is singular and cannot be inverted.");
-                } 
-                else 
-                {
-                    det = 1.0f / det;
+                    var m = (float*)m0;
+                    var invOut = (float*)m1;
+                    float det;
+                    inv[0] = m[5] * m[10] * m[15] -
+                             m[5] * m[11] * m[14] -
+                             m[9] * m[6] * m[15] +
+                             m[9] * m[7] * m[14] +
+                             m[13] * m[6] * m[11] -
+                             m[13] * m[7] * m[10];
 
-                    for (int i = 0; i < 16; i++)
-                        invOut [i] = inv [i] * det;
+                    inv[4] = -m[4] * m[10] * m[15] +
+                              m[4] * m[11] * m[14] +
+                              m[8] * m[6] * m[15] -
+                              m[8] * m[7] * m[14] -
+                              m[12] * m[6] * m[11] +
+                              m[12] * m[7] * m[10];
+
+                    inv[8] = m[4] * m[9] * m[15] -
+                             m[4] * m[11] * m[13] -
+                             m[8] * m[5] * m[15] +
+                             m[8] * m[7] * m[13] +
+                             m[12] * m[5] * m[11] -
+                             m[12] * m[7] * m[9];
+
+                    inv[12] = -m[4] * m[9] * m[14] +
+                               m[4] * m[10] * m[13] +
+                               m[8] * m[5] * m[14] -
+                               m[8] * m[6] * m[13] -
+                               m[12] * m[5] * m[10] +
+                               m[12] * m[6] * m[9];
+
+                    inv[1] = -m[1] * m[10] * m[15] +
+                              m[1] * m[11] * m[14] +
+                              m[9] * m[2] * m[15] -
+                              m[9] * m[3] * m[14] -
+                              m[13] * m[2] * m[11] +
+                              m[13] * m[3] * m[10];
+
+                    inv[5] = m[0] * m[10] * m[15] -
+                             m[0] * m[11] * m[14] -
+                             m[8] * m[2] * m[15] +
+                             m[8] * m[3] * m[14] +
+                             m[12] * m[2] * m[11] -
+                             m[12] * m[3] * m[10];
+
+                    inv[9] = -m[0] * m[9] * m[15] +
+                              m[0] * m[11] * m[13] +
+                              m[8] * m[1] * m[15] -
+                              m[8] * m[3] * m[13] -
+                              m[12] * m[1] * m[11] +
+                              m[12] * m[3] * m[9];
+
+                    inv[13] = m[0] * m[9] * m[14] -
+                              m[0] * m[10] * m[13] -
+                              m[8] * m[1] * m[14] +
+                              m[8] * m[2] * m[13] +
+                              m[12] * m[1] * m[10] -
+                              m[12] * m[2] * m[9];
+
+                    inv[2] = m[1] * m[6] * m[15] -
+                             m[1] * m[7] * m[14] -
+                             m[5] * m[2] * m[15] +
+                             m[5] * m[3] * m[14] +
+                             m[13] * m[2] * m[7] -
+                             m[13] * m[3] * m[6];
+
+                    inv[6] = -m[0] * m[6] * m[15] +
+                              m[0] * m[7] * m[14] +
+                              m[4] * m[2] * m[15] -
+                              m[4] * m[3] * m[14] -
+                              m[12] * m[2] * m[7] +
+                              m[12] * m[3] * m[6];
+
+                    inv[10] = m[0] * m[5] * m[15] -
+                              m[0] * m[7] * m[13] -
+                              m[4] * m[1] * m[15] +
+                              m[4] * m[3] * m[13] +
+                              m[12] * m[1] * m[7] -
+                              m[12] * m[3] * m[5];
+
+                    inv[14] = -m[0] * m[5] * m[14] +
+                               m[0] * m[6] * m[13] +
+                               m[4] * m[1] * m[14] -
+                               m[4] * m[2] * m[13] -
+                               m[12] * m[1] * m[6] +
+                               m[12] * m[2] * m[5];
+
+                    inv[3] = -m[1] * m[6] * m[11] +
+                              m[1] * m[7] * m[10] +
+                              m[5] * m[2] * m[11] -
+                              m[5] * m[3] * m[10] -
+                              m[9] * m[2] * m[7] +
+                              m[9] * m[3] * m[6];
+
+                    inv[7] = m[0] * m[6] * m[11] -
+                             m[0] * m[7] * m[10] -
+                             m[4] * m[2] * m[11] +
+                             m[4] * m[3] * m[10] +
+                             m[8] * m[2] * m[7] -
+                             m[8] * m[3] * m[6];
+
+                    inv[11] = -m[0] * m[5] * m[11] +
+                               m[0] * m[7] * m[9] +
+                               m[4] * m[1] * m[11] -
+                               m[4] * m[3] * m[9] -
+                               m[8] * m[1] * m[7] +
+                               m[8] * m[3] * m[5];
+
+                    inv[15] = m[0] * m[5] * m[10] -
+                              m[0] * m[6] * m[9] -
+                              m[4] * m[1] * m[10] +
+                              m[4] * m[2] * m[9] +
+                              m[8] * m[1] * m[6] -
+                              m[8] * m[2] * m[5];
+
+                    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+                    if (det == 0)
+                    {
+                        throw new InvalidOperationException("Matrix is singular and cannot be inverted.");
+                    }
+                    else
+                    {
+                        det = 1.0f / det;
+
+                        for (int i = 0; i < 16; i++)
+                            invOut[i] = inv[i] * det;
+                    }
                 }
             }
         }
