7985ae58630f79a44153cfcbc57b255a286f3851
FreezyLemon <freezestyleman@gmx.de>
2018-06-10 21:46:13 +0200

    delete unused code from API.cs

--- src/OpenTK/Platform/Windows/API.cs
+++ src/OpenTK/Platform/Windows/API.cs
@@ -27,12 +27,7 @@
 //
 
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Text;
-using OpenTK.Core;
-using OpenTK.Platform.Common;
 
 #if !MINIMAL
 using System.Drawing;
@@ -51,42 +46,7 @@ namespace OpenTK.Platform.Windows
     using HWND = IntPtr;
     using HINSTANCE = IntPtr;
     using HMENU = IntPtr;
-    using HICON = IntPtr;
-    using HBRUSH = IntPtr;
-    using HCURSOR = IntPtr;
-    using HKEY = IntPtr;
-    using PHKEY = IntPtr;
-    using HDROP = IntPtr;
-    using LRESULT = IntPtr;
     using LPVOID = IntPtr;
-    using LPCTSTR = String;
-    using WPARAM = IntPtr;
-    using LPARAM = IntPtr;
-    using HANDLE = IntPtr;
-    using HRAWINPUT = IntPtr;
-    using BYTE = Byte;
-    using SHORT = Int16;
-    using USHORT = UInt16;
-    using LONG = Int32;
-    using ULONG = UInt32;
-    using WORD = Int16;
-    using DWORD = Int32;
-    using BOOL = Boolean;
-    using INT = Int32;
-    using UINT = UInt32;
-    using LONG_PTR = IntPtr;
-    using ATOM = Int32;
-    using COLORREF = Int32;
-    using RECT = Win32Rectangle;
-    using WNDPROC = IntPtr;
-    using LPDEVMODE = DeviceMode;
-    using HDEVNOTIFY = IntPtr;
-    using HRESULT = IntPtr;
-    using HMONITOR = IntPtr;
-    using DWORD_PTR = IntPtr;
-    using UINT_PTR = UIntPtr;
-    using TIMERPROC = Functions.TimerProc;
-    using REGSAM = UInt32;
 
     /// \internal
     /// <summary>
@@ -179,14 +139,6 @@ namespace OpenTK.Platform.Windows
         // Pixel types (found in wingdi.h)
         internal const byte PFD_TYPE_RGBA = 0;
         internal const byte PFD_TYPE_COLORINDEX = 1;
-
-        public enum MouseActivate
-        {
-            ACTIVATE = 1,
-            ACTIVATEANDEAT = 2,
-            NOACTIVATE = 3,
-            NOACTIVATEANDEAT = 4
-        }
 
         // Layer types (found in wingdi.h)
         internal const byte PFD_MAIN_PLANE = 0;
@@ -337,16 +289,6 @@ namespace OpenTK.Platform.Windows
         public WindowStyle New;
     }
 
-    internal struct ExtendedStyleStruct
-    {
-        public ExtendedWindowStyle Old;
-        public ExtendedWindowStyle New;
-    }
-
-    /// \internal
-    
-
-
     /// \internal
     /// <summary>
     /// Describes the pixel format of a drawing surface.
@@ -378,140 +320,6 @@ namespace OpenTK.Platform.Windows
         internal byte LayerPlane;
         private readonly byte Reserved;
         internal int crTransparent;
-    }
-
-    /// \internal
-    /// <summary>
-    /// The <b>GlyphMetricsFloat</b> structure contains information about the placement and orientation of a glyph in a
-    /// character cell.
-    /// </summary>
-    /// <remarks>The values of <b>GlyphMetricsFloat</b> are specified as notional units.</remarks>
-    /// <seealso cref="PointFloat" />
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct GlyphMetricsFloat
-    {
-        /// <summary>
-        /// Specifies the width of the smallest rectangle (the glyph's black box) that completely encloses the glyph.
-        /// </summary>
-        internal float BlackBoxX;
-
-        /// <summary>
-        /// Specifies the height of the smallest rectangle (the glyph's black box) that completely encloses the glyph.
-        /// </summary>
-        internal float BlackBoxY;
-
-        /// <summary>
-        /// Specifies the x and y coordinates of the upper-left corner of the smallest rectangle that completely encloses the
-        /// glyph.
-        /// </summary>
-        internal PointFloat GlyphOrigin;
-
-        /// <summary>
-        /// Specifies the horizontal distance from the origin of the current character cell to the origin of the next character
-        /// cell.
-        /// </summary>
-        internal float CellIncX;
-
-        /// <summary>
-        /// Specifies the vertical distance from the origin of the current character cell to the origin of the next character
-        /// cell.
-        /// </summary>
-        internal float CellIncY;
-    }
-
-    /// \internal
-    /// <summary>
-    /// The <b>PointFloat</b> structure contains the x and y coordinates of a point.
-    /// </summary>
-    /// <seealso cref="GlyphMetricsFloat" />
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct PointFloat
-    {
-        /// <summary>
-        /// Specifies the horizontal (x) coordinate of a point.
-        /// </summary>
-        internal float X;
-
-        /// <summary>
-        /// Specifies the vertical (y) coordinate of a point.
-        /// </summary>
-        internal float Y;
-    }
-
-    /*
-    typedef struct _devicemode {
-      BCHAR  dmDeviceName[CCHDEVICENAME];
-      WORD   dmSpecVersion;
-      WORD   dmDriverVersion;
-      WORD   dmSize;
-      WORD   dmDriverExtra;
-      DWORD  dmFields;
-      union {
-        struct {
-          short dmOrientation;
-          short dmPaperSize;
-          short dmPaperLength;
-          short dmPaperWidth;
-          short dmScale;
-          short dmCopies;
-          short dmDefaultSource;
-          short dmPrintQuality;
-        };
-        POINTL dmPosition;
-        DWORD  dmDisplayOrientation;
-        DWORD  dmDisplayFixedOutput;
-      };
-
-      short  dmColor;
-      short  dmDuplex;
-      short  dmYResolution;
-      short  dmTTOption;
-      short  dmCollate;
-      BYTE  dmFormName[CCHFORMNAME];
-      WORD  dmLogPixels;
-      DWORD  dmBitsPerPel;
-      DWORD  dmPelsWidth;
-      DWORD  dmPelsHeight;
-      union {
-        DWORD  dmDisplayFlags;
-        DWORD  dmNup;
-      }
-      DWORD  dmDisplayFrequency;
-    #if(WINVER >= 0x0400)
-      DWORD  dmICMMethod;
-      DWORD  dmICMIntent;
-      DWORD  dmMediaType;
-      DWORD  dmDitherType;
-      DWORD  dmReserved1;
-      DWORD  dmReserved2;
-    #if (WINVER >= 0x0500) || (_WIN32_WINNT >= 0x0400)
-      DWORD  dmPanningWidth;
-      DWORD  dmPanningHeight;
-    #endif
-    #endif
-    } DEVMODE;
-    */
-    
-
-    /// \internal
-    
-
-    
-
-    
-
-    /// \internal
-    /// <summary>
-    /// Struct pointed to by WM_GETMINMAXINFO lParam
-    /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct MINMAXINFO
-    {
-        private readonly Point Reserved;
-        public Size MaxSize;
-        public Point MaxPosition;
-        public Size MinTrackSize;
-        public Size MaxTrackSize;
     }
 
     /// \internal
@@ -559,122 +367,6 @@ namespace OpenTK.Platform.Windows
         [MarshalAs(UnmanagedType.U4)] internal SetWindowPosFlags flags;
     }
 
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-#if false
-    [StructLayout(LayoutKind.Sequential)]
-    internal struct RawMouse
-    {
-        byte b00, b01, b02, b03, b04, b05, b06, b07,
-             b08, b09, b10, b11, b12, b13, b14, b15,
-             b16, b17, b18, b19, b20, b21, b22, b23;
-
-        unsafe private byte* this[int i] { get { fixed (byte* ptr = &b00) { return ptr + i; } } }
-
-        /// <summary>
-        /// Mouse state. This member can be any reasonable combination of the following.
-        /// MOUSE_ATTRIBUTES_CHANGED
-        /// Mouse attributes changed; application needs to query the mouse attributes.
-        /// MOUSE_MOVE_RELATIVE
-        /// Mouse movement data is relative to the last mouse position.
-        /// MOUSE_MOVE_ABSOLUTE
-        /// Mouse movement data is based on absolute position.
-        /// MOUSE_VIRTUAL_DESKTOP
-        /// Mouse coordinates are mapped to the virtual desktop (for a multiple monitor system).
-        /// </summary>
-        internal RawMouseFlags Flags { get { unsafe { return *(RawMouseFlags*)this[0]; } } }
-
-        /// <summary>
-        /// Transition state of the mouse buttons.
-        /// </summary>
-        internal RawInputMouseState ButtonFlags { get { unsafe { return *(RawInputMouseState*)this[4]; } } }
-
-        /// <summary>
-        /// If usButtonFlags is RI_MOUSE_WHEEL, this member is a signed value that specifies the wheel delta.
-        /// </summary>
-        internal USHORT ButtonData { get { unsafe { return *(USHORT*)this[6]; } } }
-
-        /// <summary>
-        /// Raw state of the mouse buttons.
-        /// </summary>
-        internal ULONG RawButtons { get { unsafe { return *(ULONG*)this[8]; } } }
-
-        /// <summary>
-        /// Motion in the X direction. This is signed relative motion or absolute motion, depending on the value of usFlags.
-        /// </summary>
-        internal LONG LastX { get { unsafe { return *(LONG*)this[12]; } } }
-
-        /// <summary>
-        /// Motion in the Y direction. This is signed relative motion or absolute motion, depending on the value of usFlags.
-        /// </summary>
-        internal LONG LastY { get { unsafe { return *(LONG*)this[16]; } } }
-
-        /// <summary>
-        /// Device-specific additional information for the event.
-        /// </summary>
-        internal ULONG ExtraInformation { get { unsafe { return *(ULONG*)this[20]; } } }
-    }
-#endif
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    /// \internal
-    
-
-    
-
-    /// \internal
-    
-
-    
-
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    internal struct NcCalculateSize
-    {
-        public Win32Rectangle NewBounds;
-        public Win32Rectangle OldBounds;
-        public Win32Rectangle OldClientRectangle;
-        public unsafe WindowPosition* Position;
-    }
-
-    
-
-    internal struct BroadcastHeader
-    {
-        public int Size;
-        public DeviceBroadcastType DeviceType;
-        private int dbch_reserved;
-    }
-
     internal struct BroadcastDeviceInterface
     {
         public int Size;
@@ -719,11 +411,6 @@ namespace OpenTK.Platform.Windows
 
 #endif
 
-    
-
-    /// \internal
-    
-
     /// <summary>
     /// Window field offsets for GetWindowLong() and GetWindowLongPtr().
     /// </summary>
@@ -746,161 +433,6 @@ namespace OpenTK.Platform.Windows
         MINIMIZED = 1,
         RESTORED = 0
     }
-
-    internal enum NcCalcSizeOptions
-    {
-        ALIGNTOP = 0x10,
-        ALIGNRIGHT = 0x80,
-        ALIGNLEFT = 0x20,
-        ALIGNBOTTOM = 0x40,
-        HREDRAW = 0x100,
-        VREDRAW = 0x200,
-        REDRAW = HREDRAW | VREDRAW,
-        VALIDRECTS = 0x400
-    }
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    /// <summary>
-    /// Enumerates available mouse keys (suitable for use in WM_MOUSEMOVE messages).
-    /// </summary>
-    internal enum MouseKeys
-    {
-        // Summary:
-        //     No mouse button was pressed.
-        None = 0,
-
-        //
-        // Summary:
-        //     The left mouse button was pressed.
-        Left = 0x0001,
-
-        //
-        // Summary:
-        //     The right mouse button was pressed.
-        Right = 0x0002,
-
-        //
-        // Summary:
-        //     The middle mouse button was pressed.
-        Middle = 0x0010,
-
-        //
-        // Summary:
-        //     The first XButton was pressed.
-        XButton1 = 0x0020,
-
-        //
-        // Summary:
-        //     The second XButton was pressed.
-        XButton2 = 0x0040
-    }
-
-    /// \internal
-    
-
-    
-
-    
-
-    
-
-    /// <summary>
-    /// Identifiers for the WM_SHOWWINDOW message
-    /// </summary>
-    internal enum ShowWindowMessageIdentifiers
-    {
-        PARENTCLOSING = 1,
-        OTHERZOOM = 2,
-        PARENTOPENING = 3,
-        OTHERUNZOOM = 4
-    }
-
-    /// <summary>
-    /// Enumerates the available character sets.
-    /// </summary>
-    internal enum GdiCharset
-    {
-        Ansi = 0,
-        Default = 1,
-        Symbol = 2,
-        ShiftJIS = 128,
-        Hangeul = 129,
-        Hangul = 129,
-        GB2312 = 134,
-        ChineseBig5 = 136,
-        OEM = 255,
-
-        //#if(WINVER >= 0x0400)
-        Johab = 130,
-        Hebrew = 177,
-        Arabic = 178,
-        Greek = 161,
-        Turkish = 162,
-        Vietnamese = 163,
-        Thai = 222,
-        EastEurope = 238,
-        Russian = 204,
-        Mac = 77,
-        Baltic = 186
-    }
-
-    
-
-    
-
-    internal enum DwmWindowAttribute
-    {
-        NCRENDERING_ENABLED = 1,
-        NCRENDERING_POLICY,
-        TRANSITIONS_FORCEDISABLED,
-        ALLOW_NCPAINT,
-        CAPTION_BUTTON_BOUNDS,
-        NONCLIENT_RTL_LAYOUT,
-        FORCE_ICONIC_REPRESENTATION,
-        FLIP3D_POLICY,
-        EXTENDED_FRAME_BOUNDS,
-        HAS_ICONIC_BITMAP,
-        DISALLOW_PEEK,
-        EXCLUDED_FROM_PEEK,
-        LAST
-    }    
 }
 
 #pragma warning restore 3019
