a7cb537143ca5f04acfa6ab04889994db83c4254
FreezyLemon <freezestyleman@gmx.de>
2018-07-04 15:09:48 +0200

    more review changes

--- src/OpenTK.NT/Native/User32/Enums/WindowMessageActivate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenTK.NT.Native
-{
-    public enum WindowMessageActivate // typedef in windows?
-    {
-        Active = 1,
-        ClickActive = 2,
-        Inactive = 0
-    }
-}
--- src/OpenTK.NT/Native/User32/Structs/Msg.cs
+++ src/OpenTK.NT/Native/User32/Structs/Msg.cs
@@ -1,4 +1,5 @@
-﻿using HWND = System.IntPtr;
+﻿using DWORD = System.UInt32;
+using HWND = System.IntPtr;
 using LPARAM = System.IntPtr;
 using WPARAM = System.IntPtr;
 
@@ -10,10 +11,9 @@ namespace OpenTK.NT.Native
         public WindowMessage Message;
         public WPARAM WParam;
         public LPARAM LParam;
-        public uint Time;
+        public DWORD Time;
 
         public Point Point;
-        //public object RefObject;
 
         public override string ToString()
             => $"msg=0x{(int)Message:x} ({Message.ToString()}) hwnd=0x{HWnd.ToInt32():x} wparam=0x{WParam.ToInt32():x} lparam=0x{LParam.ToInt32():x} pt=0x{Point:x}";
