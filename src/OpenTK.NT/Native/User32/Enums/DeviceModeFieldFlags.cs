749f6a4c4315bdf5537f75bdb95beb46bdaf4115
FreezyLemon <freezestyleman@gmx.de>
2018-07-06 21:08:13 +0200

    try to represent DeviceMode unions properly
    **This still needs testing!**

--- src/OpenTK.NT/Native/User32/Enums/DeviceModeFieldFlags.cs
+++ src/OpenTK.NT/Native/User32/Enums/DeviceModeFieldFlags.cs
@@ -2,13 +2,44 @@
 
 namespace OpenTK.NT.Native
 {
+    /// <summary>
+    /// Used in the <see cref="DeviceMode"/> structure to specify whether certain members of the structure have been initialized.<para/>
+    /// If a member is initialized, its corresponding bit is set, otherwise the bit is clear. A driver supports only those <see cref="DeviceMode"/> members that are appropriate for the printer or display technology.
+    /// </summary>
     public enum DeviceModeFieldFlags : DWORD
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        Orientation = 0x00000001,
+        PaperSize = 0x00000002,
+        PaperLength = 0x00000004,
+        PaperWidth = 0x00000008,
+        Scale = 0x00000010,
+        Position = 0x00000020,
+        Nup = 0x00000040,
+        DisplayOrientation = 0x00000080,
+        Copies = 0x00000100,
+        DefaultSource = 0x00000200,
+        PrintQuality = 0x00000400,
+        Color = 0x00000800,
+        Duplex = 0x00001000,
+        YResolution = 0x00002000,
+        TTOption = 0x00004000,
+        Collate = 0x00008000,
+        FormName = 0x00010000,
         LogPixels = 0x00020000,
         BitsPerPel = 0x00040000,
         PelsWidth = 0x00080000,
         PelsHeight = 0x00100000,
         DisplayFlags = 0x00200000,
-        DisplayFrequency = 0x00400000
+        DisplayFrequency = 0x00400000,
+        IcmMethod = 0x00800000,
+        IcmIntent = 0x01000000,
+        MediaType = 0x02000000,
+        DitherType = 0x04000000,
+        PanningWidth = 0x08000000,
+        PanningHeight = 0x10000000,
+        DisplayFixedOutput = 0x20000000,
     }
 }
--- src/OpenTK.NT/Native/User32/Structs/DeviceMode.cs
+++ src/OpenTK.NT/Native/User32/Structs/DeviceMode.cs
@@ -5,47 +5,126 @@ using WORD = System.UInt16;
 
 namespace OpenTK.NT.Native
 {
-    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-    public struct DeviceMode // unions aren't represented
+    /// <summary>
+    /// Specifies characteristics of display and print devices.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode)]
+    public struct DeviceMode
     {
+        [FieldOffset(0)]
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
         public string DeviceName;
+
+        [FieldOffset(64)]
         public WORD SpecVersion;
+
+        [FieldOffset(66)]
         public WORD DriverVersion;
+
+        [FieldOffset(68)]
         public WORD Size;
+
+        [FieldOffset(70)]
         public WORD DriverExtra;
+
+        [FieldOffset(72)]
         public DeviceModeFieldFlags Fields;
 
-        public Point Position;
-        public DWORD DisplayOrientation;
-        public DWORD DisplayFixedOutput;
+        [FieldOffset(80)]
+        public PrinterDeviceOptions PrinterOptions;
 
+        [FieldOffset(80)]
+        public Point Position;
+
+        [FieldOffset(80)]
+        public DisplayDeviceOptions DisplayOptions;
+
+        [FieldOffset(96)]
         public short Color;
+
+        [FieldOffset(98)]
         public short Duplex;
+
+        [FieldOffset(100)]
         public short YResolution;
+
+        [FieldOffset(102)]
         public short TTOption;
+
+        [FieldOffset(104)]
         public short Collate;
 
+        [FieldOffset(106)]
         [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
         public string FormName;
+
+        [FieldOffset(170)]
         public WORD LogPixels;
+
+        [FieldOffset(172)]
         public DWORD BitsPerPel;
+
+        [FieldOffset(176)]
         public DWORD PelsWidth;
+
+        [FieldOffset(180)]
         public DWORD PelsHeight;
 
+        [FieldOffset(184)]
         public DWORD DisplayFlags;
+
+        [FieldOffset(184)]
+        public DWORD Nup;
+
+        [FieldOffset(188)]
         public DWORD DisplayFrequency;
 
+        [FieldOffset(192)]
         public DWORD ICMMethod;
+
+        [FieldOffset(196)]
         public DWORD ICMIntent;
+
+        [FieldOffset(200)]
         public DWORD MediaType;
+
+        [FieldOffset(204)]
         public DWORD DitherType;
+
+        [FieldOffset(208)]
         public DWORD Reserved1;
+
+        [FieldOffset(212)]
         public DWORD Reserved2;
 
+        [FieldOffset(216)]
         public DWORD PanningWidth;
+
+        [FieldOffset(220)]
         public DWORD PanningHeight;
 
+        /// <summary>
+        /// The size of the structure in bytes.
+        /// </summary>
         public static readonly uint SizeInBytes = (uint)Marshal.SizeOf<DeviceMode>();
+
+        public struct PrinterDeviceOptions
+        {
+            public short Orientation;
+            public short PaperSize;
+            public short PaperLength;
+            public short PaperWidth;
+            public short Scale;
+            public short Copies;
+            public short DefaultSource;
+            public short PrintQuality;
+        }
+
+        public struct DisplayDeviceOptions
+        {
+            public Point Position;
+            public DWORD DisplayOrientation;
+            public DWORD DisplayFixedOutput;
+        }
     }
 }
