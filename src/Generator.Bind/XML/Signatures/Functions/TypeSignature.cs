2c5bb302514af1a8523b66cf4df2bfcf16a015b1
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2018-07-19 21:38:41 +0200

    Implement equality members for type signatures.

--- src/Generator.Bind/XML/Signatures/Functions/TypeSignature.cs
+++ src/Generator.Bind/XML/Signatures/Functions/TypeSignature.cs
@@ -6,7 +6,7 @@ namespace Bind.XML.Signatures.Functions
     /// <summary>
     /// Represents a type signature in an API.
     /// </summary>
-    public class TypeSignature
+    public class TypeSignature : IEquatable<TypeSignature>
     {
         /// <summary>
         /// Gets the name of the type.
@@ -45,6 +45,79 @@ namespace Bind.XML.Signatures.Functions
             Name = name ?? throw new ArgumentNullException(nameof(name));
             ArrayDimensions = arrayDimensions;
             IndirectionLevel = indirectionLevel;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(TypeSignature other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(Name, other.Name)
+                   && IndirectionLevel == other.IndirectionLevel
+                   && ArrayDimensions == other.ArrayDimensions;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return Equals((TypeSignature)obj);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Name.GetHashCode();
+                hashCode = (hashCode * 397) ^ IndirectionLevel;
+                hashCode = (hashCode * 397) ^ ArrayDimensions;
+                return hashCode;
+            }
+        }
+
+        /// <summary>
+        /// Determines if two <see cref="TypeSignature"/> instances are equal.
+        /// </summary>
+        /// <param name="left">The left instance.</param>
+        /// <param name="right">The right instance.</param>
+        /// <returns>true if the instances are equal; otherwise, false.</returns>
+        public static bool operator ==([CanBeNull] TypeSignature left, [CanBeNull] TypeSignature right)
+        {
+            return Equals(left, right);
+        }
+
+        /// <summary>
+        /// Determines if two <see cref="TypeSignature"/> instances are not equal.
+        /// </summary>
+        /// <param name="left">The left instance.</param>
+        /// <param name="right">The right instance.</param>
+        /// <returns>true if the instances are not equal; otherwise, false.</returns>
+        public static bool operator !=([CanBeNull] TypeSignature left, [CanBeNull] TypeSignature right)
+        {
+            return !Equals(left, right);
         }
 
         /// <inheritdoc/>
