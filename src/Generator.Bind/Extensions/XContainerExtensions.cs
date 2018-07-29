3a91291cdebe59c950d5526571885e9426d774e6
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2018-07-29 19:50:07 +0200

    Improve extensions.

--- src/Generator.Bind/Extensions/XContainerExtensions.cs
+++ src/Generator.Bind/Extensions/XContainerExtensions.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 using JetBrains.Annotations;
 
@@ -13,19 +15,56 @@ namespace Bind.Extensions
         /// Gets a required element from a container.
         /// </summary>
         /// <param name="container">The container to get the element from.</param>
-        /// <param name="attributeName">The name of the element.</param>
+        /// <param name="elementName">The name of the element.</param>
+        /// <param name="ignoreNamespace">Whether or not the namespace should be ignored.</param>
         /// <exception cref="InvalidDataException">Thrown if the element is not present.</exception>
         /// <returns>The element.</returns>
         [NotNull]
-        public static XElement GetRequiredElement([NotNull] this XContainer container, string attributeName)
+        public static XElement GetRequiredElement
+        (
+            [NotNull] this XContainer container,
+            [NotNull] string elementName,
+            bool ignoreNamespace = true
+        )
         {
-            var attribute = container.Element(attributeName);
-            if (attribute is null)
+            var element = ignoreNamespace
+                ? container.Elements().FirstOrDefault(a => a.Name.LocalName == elementName)
+                : container.Element(elementName);
+
+            if (element is null)
             {
-                throw new InvalidDataException($"Required element \"{attributeName}\" not found.");
+                throw new InvalidDataException($"Required element \"{elementName}\" not found.");
             }
 
-            return attribute;
+            return element;
+        }
+
+        /// <summary>
+        /// Gets a set of required elements from a container.
+        /// </summary>
+        /// <param name="container">The container to get the elements from.</param>
+        /// <param name="elementName">The name of the element.</param>
+        /// <param name="ignoreNamespace">Whether or not the namespace should be ignored.</param>
+        /// <returns>The elements.</returns>
+        [NotNull, ItemNotNull]
+        public static IEnumerable<XElement> GetRequiredElements
+        (
+            [NotNull] this XContainer container,
+            [NotNull] string elementName,
+            bool ignoreNamespace = true
+        )
+        {
+            var elements = ignoreNamespace
+                ? container.Elements().Where(a => a.Name.LocalName == elementName)
+                : container.Elements(elementName);
+
+            var requiredElements = elements.ToList();
+            if (!requiredElements.Any())
+            {
+                throw new InvalidDataException($"Required element \"{elements}\" not found.");
+            }
+
+            return requiredElements;
         }
     }
 }
--- src/Generator.Bind/Extensions/XElementExtensions.cs
+++ src/Generator.Bind/Extensions/XElementExtensions.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 using JetBrains.Annotations;
 
@@ -14,12 +16,21 @@ namespace Bind.Extensions
         /// </summary>
         /// <param name="element">The element to get the attribute from.</param>
         /// <param name="attributeName">The name of the attribute.</param>
+        /// <param name="ignoreNamespace">Whether or not the namespace should be ignored.</param>
         /// <exception cref="InvalidDataException">Thrown if the attribute is not present.</exception>
         /// <returns>The attribute.</returns>
         [NotNull]
-        public static XAttribute GetRequiredAttribute([NotNull] this XElement element, string attributeName)
+        public static XAttribute GetRequiredAttribute
+        (
+            [NotNull] this XElement element,
+            [NotNull] string attributeName,
+            bool ignoreNamespace = true
+        )
         {
-            var attribute = element.Attribute(attributeName);
+            var attribute = ignoreNamespace
+                ? element.Attributes().FirstOrDefault(a => a.Name.LocalName == attributeName)
+                : element.Attribute(attributeName);
+
             if (attribute is null)
             {
                 throw new InvalidDataException($"Required attribute \"{attributeName}\" not found.");
