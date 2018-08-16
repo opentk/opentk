bc78243e8e70a5e4194bf2dfd06cc795186d2542
Jarl Gullberg <Jarl.Gullberg@gmail.com>
2018-08-16 20:39:35 +0200

    Implement source functions.

--- src/OpenTK.OpenAL/Native/AL/Interfaces/IALSources.cs
+++ src/OpenTK.OpenAL/Native/AL/Interfaces/IALSources.cs
@@ -1,4 +1,6 @@
 using AdvancedDLSupport;
+using OpenTK.Mathematics;
+// ReSharper disable ExplicitCallerInfoArgument
 
 namespace OpenTK.OpenAL.Native.Interfaces
 {
@@ -8,14 +10,185 @@ namespace OpenTK.OpenAL.Native.Interfaces
     [NativeSymbols(Prefix = "al")]
     public interface IALSources
     {
-        void GenSources(int count, out uint[] sources);
+        /// <summary>
+        /// Creates one or more source objects.
+        /// </summary>
+        /// <param name="count">The number of objects to generate.</param>
+        /// <param name="sources">A pointer to the first element of the array where the handles will be stored.</param>
+        /// <seealso cref="DeleteSources"/>
+        /// <seealso cref="IsSource"/>
+        unsafe void GenSources(int count, uint* sources);
 
-        void DeleteSources(int count, uint[] sources);
+        /// <summary>
+        /// Deletes one or more source objects, freeing their resources.
+        /// </summary>
+        /// <param name="count">The number of objects to delete.</param>
+        /// <param name="sources">A pointer to the first element of the array where the handles are stored.</param>
+        /// <seealso cref="GenSources"/>
+        /// <seealso cref="IsSource"/>
+        unsafe void DeleteSources(int count, uint* sources);
 
+        /// <summary>
+        /// Determines whether or not a given handle is an source handle.
+        /// </summary>
+        /// <param name="source">The handle.</param>
+        /// <returns>true if the handle is an source handle; otherwise, false.</returns>
+        /// <seealso cref="GenSources"/>
+        /// <seealso cref="DeleteSources"/>
         bool IsSource(uint source);
 
-        void Source(uint source, ALSourcef param, float value);
+        /// <summary>
+        /// Sets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("Sourcef")]
+        void SetSourceProperty(uint source, ALSourcef param, float value);
 
-        void Source(uint source, ALSource3f param, float x, float y, float z);
+        /// <summary>
+        /// Sets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("Sourcefv")]
+        void SetSourceProperty(uint source, ALSource3f param, in Vector3 value);
+
+        /// <summary>
+        /// Sets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("Sourcei")]
+        void SetSourceProperty(uint source, ALSourcef param, int value);
+
+        /// <summary>
+        /// Gets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("GetSourcef")]
+        void GetSourceProperty(uint source, ALSourcef param, out float value);
+
+        /// <summary>
+        /// Gets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("GetSourcefv")]
+        void GetSourceProperty(uint source, ALSource3f param, out Vector3 value);
+
+        /// <summary>
+        /// Gets the value of a named property on the given source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="param">The named property.</param>
+        /// <param name="value">The value to set the property to.</param>
+        [NativeSymbol("GetSourcei")]
+        void GetSourceProperty(uint source, ALSourcef param, out int value);
+
+        /// <summary>
+        /// Plays a source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        void SourcePlay(uint source);
+
+        /// <summary>
+        /// Plays a set of sources.
+        /// </summary>
+        /// <param name="count">The number of sources to play.</param>
+        /// <param name="sources">A pointer to the first element in an array of handles.</param>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        [NativeSymbol("SourcePlayv")]
+        unsafe void SourcePlay(int count, uint* sources);
+
+        /// <summary>
+        /// Pauses a source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        void SourcePause(uint source);
+
+        /// <summary>
+        /// Pauses a set of sources.
+        /// </summary>
+        /// <param name="count">The number of sources to pause.</param>
+        /// <param name="sources">A pointer to the first element in an array of handles.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        [NativeSymbol("SourcePausev")]
+        unsafe void SourcePause(int count, uint* sources);
+
+        /// <summary>
+        /// Stops a source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        void SourceStop(uint source);
+
+        /// <summary>
+        /// Stops a set of sources.
+        /// </summary>
+        /// <param name="count">The number of sources to stop.</param>
+        /// <param name="sources">A pointer to the first element in an array of handles.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceRewind(uint)"/>
+        [NativeSymbol("SourceStopv")]
+        unsafe void SourceStop(int count, uint* sources);
+
+        /// <summary>
+        /// Rewinds a source.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        void SourceRewind(uint source);
+
+        /// <summary>
+        /// Rewinds a set of sources.
+        /// </summary>
+        /// <param name="count">The number of sources to rewind.</param>
+        /// <param name="sources">A pointer to the first element in an array of handles.</param>
+        /// <seealso cref="SourcePlay(uint)"/>
+        /// <seealso cref="SourcePause(uint)"/>
+        /// <seealso cref="SourceStop(uint)"/>
+        [NativeSymbol("SourceRewindv")]
+        unsafe void SourceRewind(int count, uint* sources);
+
+        /// <summary>
+        /// Queues a set of buffers on the source. All buffers attached to a source will be played in sequence, and the
+        /// number of processed buffers can be detected using a call to
+        /// <see cref="GetSourceProperty(uint,OpenTK.OpenAL.Native.ALSourcei,out int)"/>.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="count">The number of buffers to queue.</param>
+        /// <param name="buffers">A pointer to the first element in an array of handles.</param>
+        unsafe void SourceQueueBuffers(uint source, int count, uint* buffers);
+
+        /// <summary>
+        /// Unqueues a set of buffers on the source. The number of processed buffers can be detected using a call to
+        /// <see cref="GetSourceProperty(uint,OpenTK.OpenAL.Native.ALSourcei,out int)"/>, which is the maximum number of
+        /// buffers that can be unqueued during this call.
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="count">The number of buffers to queue.</param>
+        /// <param name="buffers">A pointer to the first element in an array of handles.</param>
+        unsafe void SourceUnqueueBuffers(uint source, int count, uint* buffers);
     }
 }
