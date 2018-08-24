efb631d810199fd631284f8e66806dbe15fcc24a
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-08-24 14:44:55 +0200

    Correct the structure of the capture device enumeration extension.

---
+++ src/OpenAL/Extensions/OpenTK.OpenAL.Extensions.EXT.Capture.Enumeration/CaptureEnumerationEnumeration.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using AdvancedDLSupport;
+using OpenTK.OpenAL.Native;
+using OpenTK.OpenAL.Native.Extensions;
+
+namespace OpenTK.OpenAL.Extensions.Enumeration
+{
+    /// <summary>
+    /// Exposes the API in the CaptureEnumeration extension.
+    /// </summary>
+    public abstract class CaptureEnumeration : ExtensionBase, ICaptureEnumerationContextState
+    {
+        /// <inheritdoc cref="ExtensionBase"/>
+        protected CaptureEnumeration(string path, ImplementationOptions options) : base(path, options)
+        {
+        }
+
+        /// <inheritdoc />
+        public abstract unsafe char* GetString(void* device, GetCaptureEnumerationContextString param);
+
+        /// <inheritdoc cref="GetString(void*, GetCaptureEnumerationContextString)"/>
+        public string GetString(IntPtr device, GetCaptureEnumerationContextString param)
+        {
+            unsafe
+            {
+                var result = GetString(device.ToPointer(), param);
+                if (result != (char*)0)
+                {
+                    return Marshal.PtrToStringAnsi(new IntPtr(result));
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <inheritdoc />
+        public abstract unsafe char* GetStringList(void* device, GetEnumerationContextStringList param);
+
+        /// <inheritdoc cref="GetString(void*, GetEnumerationContextStringList)"/>
+        public IEnumerable<string> GetStringList(GetEnumerationContextStringList param)
+        {
+            unsafe
+            {
+                var result = GetStringList(null, param);
+                if (result == (char*)0)
+                {
+                    return new List<string>();
+                }
+
+                var strings = new List<string>();
+
+                var currentPos = result;
+                while (true)
+                {
+                    var currentString = Marshal.PtrToStringAnsi(new IntPtr(currentPos));
+                    if (currentString is null)
+                    {
+                        break;
+                    }
+
+                    strings.Add(currentString);
+                    currentPos += currentString.Length + 1;
+                }
+
+                return strings;
+            }
+        }
+    }
+}
