5758d3076fd62a1c840a7dee209ff1d4266e402a
Yannick Seibert <yseibert@live.de>
2018-10-30 17:06:48 +0100

    Code relayout & added XML documentation

--- src/Generator.Bind/LicenseResource.cs
+++ src/Generator.Bind/LicenseResource.cs
@@ -3,17 +3,25 @@ using System.Reflection;
 
 namespace Bind
 {
+    /// <summary>
+    /// Class for handling embedded license text.
+    /// </summary>
     internal class LicenseResource
     {
         private static string _license;
 
+        /// <summary>
+        /// Gets and returns the license text from the embedded resource.
+        /// </summary>
         public static string LicenseText
         {
             get
             {
                 if (_license != null)
+                {
                     return _license;
-                
+                }
+
                 var streamReader = new StreamReader(Assembly.GetAssembly(typeof(Program)).GetManifestResourceStream("Specifications/License.txt"));
                 _license = streamReader.ReadToEnd();
                 streamReader.Dispose();
