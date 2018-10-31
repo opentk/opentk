590de0b7c479ad9b64c18781e7db7b9373c275b1
Lyrc <ngalianos@hotmail.com>
2018-10-31 21:16:22 +0200

    Revert "Revert "Added directory safety""
    This reverts commit f44edd1d30477d9426ac797229723bb8bc9530de.

--- src/Generator.Bind/Generators/Bases/OpenGLGeneratorSettingsBase.cs
+++ src/Generator.Bind/Generators/Bases/OpenGLGeneratorSettingsBase.cs
@@ -53,7 +53,6 @@ namespace Bind.Generators.Bases
             var fullPath = Path.GetFullPath(overrideFileDirectoryPath);
 
 <<<<<<< HEAD
-<<<<<<< HEAD
 =======
             if (!Directory.Exists(fullPath))
             {
@@ -72,8 +71,6 @@ namespace Bind.Generators.Bases
             var extraOverrides = Directory.GetFiles(overrideFileDirectoryPath, "*.xml", SearchOption.AllDirectories);
 
 >>>>>>> 8ee326fdcb04412916581eda1afe167b84015141
-=======
->>>>>>> parent of 91811a3b... Added directory safety
             using (var fs = File.OpenText(Program.Arguments.LicenseFile))
             {
                 var licenseContents = fs.ReadToEnd().TrimEnd();
