dc810e55213a95e5e3ecee438cec35304c6ddc6d
Lyrc <32474602+Lyrcaxis@users.noreply.github.com>
2018-11-02 03:44:38 +0200

    Decoupled everything to accept partial arguments.

--- src/Generator.Bind/PathResolver.cs
+++ src/Generator.Bind/PathResolver.cs
@@ -3,6 +3,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -13,123 +14,141 @@ namespace Bind
     /// </summary>
     public static class PathResolver
     {
-        // Default path values in case of missing arguments.
-        private static string DefaultInputPath = Path.Combine("src", "Generator.Bind", "Specifications");
-        private static string DefaultOutputPath = Path.Combine("src", "OpenTK", "Graphics");
-        private static string DefaultDocumentationPath = Path.Combine("src", "Generator.Bind", "Specifications", "Docs", "docs.gl");
-        private static string DefaultLicenseFile = Path.Combine("src", "Generator.Bind", "Specifications", "License.txt");
-
         /// <summary>
-        /// Ensures that the paths are configured properly.
+        /// Helper enum that holds Argument IDs.
         /// </summary>
-        public static void EnsureCorrectArguments()
+        private enum ArgumentID
         {
-            System.Collections.Generic.List<string> argValues = new System.Collections.Generic.List<string>()
-            {
-                "Input Path",
-                "Output Path",
-                "Documentation Path",
-                "License file"
-            };
-
-            bool AnyEmpty(params string[] args)
-            {
-                bool markedWrong = false;
-                for (int i = 0; i < args.Length; i++)
-                {
-                    if (string.IsNullOrEmpty(args[i]))
-                    {
-                        Console.WriteLine($"Argument {argValues[i]} wasn't specified.");
-                        markedWrong = true;
-                    }
-                }
-                return markedWrong;
-            }
-
-            if (AnyEmpty(Program.Arguments.InputPath, Program.Arguments.OutputPath, Program.Arguments.DocumentationPath, Program.Arguments.LicenseFile))
-            {
-                Console.WriteLine("Reseting arguments to default");
-                SetDefaultArguments();
-                ResolvePath();
-                return;
-            }
-
-            // Continue checking for invalid paths, just in case.
-            bool AnyWrong(params string[] args)
-            {
-                bool markedWrong = false;
-                foreach (var path in args)
-                {
-                    var dummyFullPath = Path.GetFullPath(path);
-                    if (!Directory.Exists(path))
-                    {
-                        Console.WriteLine($"Path {path} doesn't exist.");
-                        markedWrong = true;
-                    }
-                }
-                return markedWrong;
-            }
-
-            if (AnyWrong(Program.Arguments.InputPath, Program.Arguments.OutputPath, Program.Arguments.DocumentationPath, Program.Arguments.LicenseFile))
-            {
-                ResolvePath();
-            }
+            Input,
+            Output,
+            Documentation,
+            License
         }
 
         /// <summary>
-        /// Resolves the path in case of empty arguments.
+        /// Default path values in case of missing arguments.
+        /// </summary>
+        private static Dictionary<ArgumentID, string> _defaultPaths = new Dictionary<ArgumentID, string>()
+        {
+            { ArgumentID.Input, Path.Combine("src", "Generator.Bind", "Specifications") },
+            { ArgumentID.Output, Path.Combine("src", "OpenTK", "Graphics") },
+            { ArgumentID.Documentation, Path.Combine("src", "Generator.Bind", "Specifications", "Docs", "docs.gl") },
+            { ArgumentID.License, Path.Combine("src", "Generator.Bind", "Specifications", "License.txt") }
+        };
+
+        /// <summary>
+        /// How to represent values in program. Cosmetic usage only.
+        /// Note: Index order is hardcoded.
+        /// </summary>
+        private static Dictionary<ArgumentID, string> _argValues = new Dictionary<ArgumentID, string>()
+        {
+            { ArgumentID.Input, "Input Path" },
+            { ArgumentID.Output, "Output Path" },
+            { ArgumentID.Documentation, "Documentation Path" },
+            { ArgumentID.License, "License Path" }
+        };
+
+        /// <summary>
+        /// Resolves the path in case of empty arguments and Ensures that the paths are configured properly.
         /// Should allow the user to manually build and execute the Binder from within the bin folder.
         /// </summary>
-        private static void ResolvePath()
+        public static void ValidateArguments()
         {
-            var dummyFullPath = Path.GetFullPath(Program.Arguments.InputPath);
-            if (Directory.Exists(dummyFullPath))
-            {
-                Console.WriteLine("No path arguments passed but directory is valid..");
-                Console.WriteLine("Press F to fix directory, or any other key to continue");
+            CheckForEmptyArguments(Program.Arguments.InputPath, Program.Arguments.OutputPath, Program.Arguments.DocumentationPath, Program.Arguments.LicenseFile);
 
-                string userInput = Console.ReadKey(true).Key.ToString().ToLower();
-                if (userInput != "f")
+            // Check if paths are valid.
+            ValidatePaths(false, Program.Arguments.InputPath, Program.Arguments.OutputPath, Program.Arguments.DocumentationPath, Program.Arguments.LicenseFile);
+
+            // Check again. Initiate program exit if a path is not valid.
+            ValidatePaths(true, Program.Arguments.InputPath, Program.Arguments.OutputPath, Program.Arguments.DocumentationPath, Program.Arguments.LicenseFile);
+        }
+
+        /// <summary>
+        /// Scan the arguments and replace any missing ones with the default argument value.
+        /// </summary>
+        /// <param name="args">Arguments to check.</param>
+        private static void CheckForEmptyArguments(params string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (string.IsNullOrEmpty(args[i]))
                 {
-                    return;
+                    Console.WriteLine($"Argument {_argValues.ElementAt(i).Value} wasn't specified. Setting default value.");
+                    SetDefaultArgument(_argValues.ElementAt(i).Key);
                 }
-            }
-
-            System.Console.WriteLine("Adding solution path to directory");
-            UpdateArguments(dummyFullPath);
-
-            dummyFullPath = Path.GetFullPath(Program.Arguments.InputPath);
-
-            if (!Directory.Exists(dummyFullPath))
-            {
-                Console.WriteLine("Failed to resolve directory.");
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey(true);
-                Environment.Exit(-1);
             }
         }
 
         /// <summary>
-        /// Inserts the solution path to the arguments paths.
+        /// Path validation.
         /// </summary>
-        /// <param name="fullPath">Dummy full path.</param>
-        private static void UpdateArguments(string fullPath)
+        /// <param name="quitIfNotFound">Specify if program exit be initiated on fail.</param>
+        /// <param name="args">Paths to check for integrity.</param>
+        private static void ValidatePaths(bool quitIfNotFound, params string[] args)
         {
+            for (int i = 0; i < args.Length; i++)
+            {
+                var fullPath = Path.GetFullPath(args[i]);
+                bool dirExists = Directory.Exists(fullPath) || File.Exists(fullPath);
+
+                if (!dirExists && !quitIfNotFound)
+                {
+                    Console.WriteLine($"Path {fullPath} doesn't exist.");
+                    Console.WriteLine("...Replacing with solution directory.");
+                    UpdateArgument(_argValues.ElementAt(i).Key);
+                }
+                else if (!dirExists && quitIfNotFound)
+                {
+                    Console.WriteLine($"Failed to resolve directory for {_argValues.ElementAt(i).Value}.");
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey(true);
+                    Environment.Exit(-1);
+                }
+            }
+        }
+
+        private static void SetDefaultArgument(ArgumentID argID)
+        {
+            if (argID == ArgumentID.Input)
+            {
+                Program.Arguments.InputPath = _defaultPaths[argID];
+            }
+            else if (argID == ArgumentID.Output)
+            {
+                Program.Arguments.OutputPath = _defaultPaths[argID];
+            }
+            else if (argID == ArgumentID.Documentation)
+            {
+                Program.Arguments.DocumentationPath = _defaultPaths[argID];
+            }
+            else if (argID == ArgumentID.License)
+            {
+                Program.Arguments.LicenseFile = _defaultPaths[argID];
+            }
+        }
+
+        private static void UpdateArgument(ArgumentID argID)
+        {
+            var fullPath = Path.GetFullPath(Program.Arguments.InputPath);
             string solutionPath = fullPath.Substring(0, fullPath.IndexOf(@"src"));
             string InsertSolutionDir(string str) => str.Insert(0, solutionPath);
 
-            Program.Arguments.InputPath = InsertSolutionDir(Program.Arguments.InputPath);
-            Program.Arguments.OutputPath = InsertSolutionDir(Program.Arguments.OutputPath);
-            Program.Arguments.DocumentationPath = InsertSolutionDir(Program.Arguments.DocumentationPath);
-            Program.Arguments.LicenseFile = InsertSolutionDir(Program.Arguments.LicenseFile);
-        }
-
-        private static void SetDefaultArguments()
-        {
-            Program.Arguments.InputPath = DefaultInputPath;
-            Program.Arguments.OutputPath = DefaultOutputPath;
-            Program.Arguments.DocumentationPath = DefaultDocumentationPath;
-            Program.Arguments.LicenseFile = DefaultLicenseFile;
+            if (argID == ArgumentID.Input)
+            {
+                Program.Arguments.InputPath = InsertSolutionDir(Program.Arguments.InputPath);
+            }
+            else if (argID == ArgumentID.Output)
+            {
+                Program.Arguments.OutputPath = InsertSolutionDir(Program.Arguments.OutputPath);
+            }
+            else if (argID == ArgumentID.Documentation)
+            {
+                Program.Arguments.DocumentationPath = InsertSolutionDir(Program.Arguments.DocumentationPath);
+            }
+            else if (argID == ArgumentID.License)
+            {
+                Program.Arguments.LicenseFile = InsertSolutionDir(Program.Arguments.LicenseFile);
+            }
         }
     }
 }
