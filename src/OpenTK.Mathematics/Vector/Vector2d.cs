4dae426386d761154ad674464ce3bd195b44889f
void <theprismaticvoid@gmail.com>
2018-11-04 21:04:07 -0600

    Add * operator to all double-precision vectors

--- src/OpenTK.Mathematics/Vector/Vector2d.cs
+++ src/OpenTK.Mathematics/Vector/Vector2d.cs
@@ -814,6 +814,18 @@ namespace OpenToolkit.Mathematics
         }
 
         /// <summary>
+        /// Transforms a vector by a quaternion rotation.
+        /// </summary>
+        /// <param name="quat">The quaternion to rotate the vector by.</param>
+        /// <param name="vec">The vector to transform.</param>
+        /// <returns>The transformed vector.</returns>
+        public static Vector2d operator *(Quaterniond quat, Vector2d vec)
+        {
+            Transform(ref vec, ref quat, out Vector2d result);
+            return result;
+        }
+
+        /// <summary>
         /// Divides an instance by a scalar.
         /// </summary>
         /// <param name="vec">The instance.</param>
