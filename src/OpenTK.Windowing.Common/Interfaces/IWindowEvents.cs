5b25bcbc225be815289a6da87ceb3510df02f0e1
void <theprismaticvoid@gmail.com>
2019-02-09 16:34:40 -0600

    Remove event caller functions from interface

--- src/OpenTK.Windowing.Common/Interfaces/IWindowEvents.cs
+++ src/OpenTK.Windowing.Common/Interfaces/IWindowEvents.cs
@@ -22,121 +22,101 @@ namespace OpenToolkit.Windowing.Common
         /// Occurs whenever the window is moved.
         /// </summary>
         event EventHandler<EventArgs> Move;
-        void OnMove(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever the window is resized.
         /// </summary>
         event EventHandler<EventArgs> Resize;
-        void OnResize(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the window is about to close.
         /// </summary>
         event EventHandler<CancelEventArgs> Closing;
-        void OnClosing(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs after the window has closed.
         /// </summary>
         event EventHandler<EventArgs> Closed;
-        void OnClosed(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the window is disposed.
         /// </summary>
         event EventHandler<EventArgs> Disposed;
-        void OnDisposed(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="Icon" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> IconChanged;
-        void OnIconChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="INativeWindow.Title" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> TitleChanged;
-        void OnTitleChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="INativeWindow.Visible" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> VisibleChanged;
-        void OnVisibleChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="INativeWindow.Focused" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> FocusedChanged;
-        void OnFocusChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="WindowBorder" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> WindowBorderChanged;
-        void OnWindowBorderChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs when the <see cref="WindowState" /> property of the window changes.
         /// </summary>
         event EventHandler<EventArgs> WindowStateChanged;
-        void OnWindowStateChanged(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a keyboard key is pressed.
         /// </summary>
         event EventHandler<KeyboardKeyEventArgs> KeyDown;
-        void OnKeyDown(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a character is typed.
         /// </summary>
         event EventHandler<KeyPressEventArgs> KeyPress;
-        void OnKeyPress(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a keyboard key is released.
         /// </summary>
         event EventHandler<KeyboardKeyEventArgs> KeyUp;
-        void OnKeyUp(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever the mouse cursor leaves the window <see cref="Bounds" />.
         /// </summary>
         event EventHandler<EventArgs> MouseLeave;
-        void OnMouseLeave(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever the mouse cursor enters the window <see cref="Bounds" />.
         /// </summary>
         event EventHandler<EventArgs> MouseEnter;
-        void OnMouseEnter(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a <see cref="Input.MouseButton" /> is clicked.
         /// </summary>
         event EventHandler<MouseButtonEventArgs> MouseDown;
-        void OnMouseDown(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a <see cref="Input.MouseButton" /> is released.
         /// </summary>
         event EventHandler<MouseButtonEventArgs> MouseUp;
-        void OnMouseUp(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever the mouse cursor is moved;
         /// </summary>
         event EventHandler<MouseMoveEventArgs> MouseMove;
-        void OnMouseMove(object sender, EventArgs e);
 
         /// <summary>
         /// Occurs whenever a mouse wheel is moved;
         /// </summary>
         event EventHandler<MouseWheelEventArgs> MouseWheel;
-        void OnMouseWheel(object sender, EventArgs e);
 
         //event EventHandler<MouseEventArgs> MouseClick;
         //event EventHandler<MouseEventArgs> MouseDoubleClick;
@@ -145,6 +125,5 @@ namespace OpenToolkit.Windowing.Common
         /// Occurs whenever file dropped on window.
         /// </summary>
         event EventHandler<FileDropEventArgs> FileDrop;
-        void OnFileDrop(object sender, EventArgs e);
     }
 }
