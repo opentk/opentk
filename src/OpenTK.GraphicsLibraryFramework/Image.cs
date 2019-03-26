e09e15524f0f829dbc7f0ee8505710ee3ec54f0a
void <theprismaticvoid@gmail.com>
2019-03-25 23:30:53 -0500

    Remove dependency on ImageSharp

--- src/OpenTK.GraphicsLibraryFramework/Image.cs
+++ src/OpenTK.GraphicsLibraryFramework/Image.cs
@@ -8,7 +8,6 @@
 //
 
 using System;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace OpenToolkit.GraphicsLibraryFramework
@@ -20,16 +19,7 @@ namespace OpenToolkit.GraphicsLibraryFramework
     {
         public int Width { get; set; }
         public int Height { get; set; }
-        public unsafe IntPtr Pixels { get; }
-
-
-        public Image(Stream imageStream)
-        {
-            var image = SixLabors.ImageSharp.Image.Load(imageStream); // HIGH: Need to create boundary utility class to prevent tight coupling with ImageSharp dependency
-            Width = image.Width;
-            Height = image.Height;
-            Pixels = Marshal.AllocHGlobal((int)imageStream.Length); // HIGH: This is not the raw image data size and could be smaller/larger due to header data and compression
-        }
+        public IntPtr Pixels { get; set; }
 
         public void Dispose()
         {
