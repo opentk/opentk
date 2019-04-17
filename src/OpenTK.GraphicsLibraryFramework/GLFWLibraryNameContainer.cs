682eef595d0a04edc35af8440b558846c9fba811
void <theprismaticvoid@gmail.com>
2019-04-17 15:26:32 -0500

    Update GLFW redist

--- src/OpenTK.GraphicsLibraryFramework/GLFWLibraryNameContainer.cs
+++ src/OpenTK.GraphicsLibraryFramework/GLFWLibraryNameContainer.cs
@@ -17,7 +17,7 @@ namespace OpenToolkit.GraphicsLibraryFramework
     internal class GLFWLibraryNameContainer : PlatformLibraryNameContainerBase
     {
         /// <inheritdoc />
-        public override string Linux => "libglfw.so.3";
+        public override string Linux => "libglfw.so.3.3";
 
         /// <inheritdoc />
         public override string MacOS => "libglfw.dylib";
