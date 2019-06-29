03509e74895f8bfd84c0e12d024a6402da8cf0ef
Julius Häger <julius_hager@hotmail.com>
2019-06-30 01:58:57 +0200

    Change to compare absolute error instead of the 'fits all' equality check

--- tests/OpenToolkit.Tests/Assertions.fs
+++ tests/OpenToolkit.Tests/Assertions.fs
@@ -16,6 +16,7 @@ module private AssertHelpers =
     let private EquivalenceTolerance = 0.00005f
 
     let approxEq a b = MathHelper.ApproximatelyEquivalent(a, b, EquivalenceTolerance)
+    let approxEqEpsilon a b eps = MathHelper.ApproximatelyEquivalent(a, b, float32(eps))
 
     let approxEqDelta a b = MathHelper.ApproximatelyEqual(a, b, BitAccuracy)
 
@@ -49,6 +50,9 @@ type internal Assert =
     static member ApproximatelyEquivalent(a : float32,b : float32) =
         if not <| approxEq a b then raise <| new Xunit.Sdk.EqualException(a,b)
 
+    static member ApproximatelyEquivalent(a : Color4, b : Color4, epsilon:float32) =
+        if not <| approxEqEpsilon a.R b.R epsilon && approxEqEpsilon a.G b.G epsilon && approxEqEpsilon a.B b.B epsilon && approxEqEpsilon a.A b.A epsilon then
+            raise <| new Xunit.Sdk.EqualException(a,b)
 
     static member ApproximatelyEqualEpsilon(a : Vector2, b : Vector2, epsilon:float32) =
         if neqEpsilon a.X b.X epsilon || neqEpsilon a.Y b.Y epsilon then
@@ -60,10 +64,6 @@ type internal Assert =
 
     static member ApproximatelyEqualEpsilon(a : Vector4, b : Vector4, epsilon:float32) =
         if neqEpsilon a.X b.X epsilon || neqEpsilon a.Y b.Y epsilon || neqEpsilon a.Z b.Z epsilon || neqEpsilon a.W b.W epsilon then
-            raise <| new Xunit.Sdk.EqualException(a,b)
-
-    static member ApproximatelyEqualEpsilon(a : Color4, b : Color4, epsilon:float32) =
-        if neqEpsilon a.R b.R epsilon || neqEpsilon a.G b.G epsilon || neqEpsilon a.B b.B epsilon || neqEpsilon a.A b.A epsilon then
             raise <| new Xunit.Sdk.EqualException(a,b)
 
     static member ApproximatelyEqualEpsilon(a : float32, b : float32) =
--- tests/OpenToolkit.Tests/Color4Tests.fs
+++ tests/OpenToolkit.Tests/Color4Tests.fs
@@ -10,45 +10,46 @@ open OpenToolkit.Mathematics
 
 module Color4 =
     [<Literal>]
-    let private epsilon:float32 = 1e-4f
+    let private epsilon:float32 = 1e-6f
+    let private epsilonXYZ:float32 = 1e-4f
 
     [<Properties(Arbitrary = [| typeof<OpenTKGen> |])>]
     module Covertions =
-        [<Property(MaxTest = 1000)>]
+        [<Property>]
         let ``RGB to sRGB to RGB roundtrip`` (c : Color4) =
             let srgb = Color4.ToSrgb c
             let rgb = Color4.FromSrgb srgb
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilon)
 
-        [<Property(MaxTest = 1000)>]
+        [<Property>]
         let ``RGB to HSL to RGB roundtrip`` (Color4LDR c) =
             let hsl = Color4.ToHsl c
             let rgb = Color4.FromHsl hsl
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilon)
 
-        [<Property(MaxTest = 1000)>]
+        [<Property>]
         let ``RGB to HSV to RGB roundtrip`` (c : Color4) =
             let hsv = Color4.ToHsv c
             let rgb = Color4.FromHsv hsv
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilon)
 
-        [<Property(MaxTest = 1000)>]
+        [<Property>]
         let ``RGB to XYZ to RGB roundtrip`` (Color4LDR c) =
             let xyz = Color4.ToXyz c
             let rgb = Color4.FromXyz xyz
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilonXYZ)
         
-        [<Property(MaxTest = 1000)>]
-        let ``RGB to YCbCr to RGB roundtrip`` (c : Color4) =
+        [<Property>]
+        let ``RGB to YCbCr to RGB roundtrip`` (Color4LDR c) =
             let ycbcr = Color4.ToYcbcr c
             let rgb = Color4.FromYcbcr ycbcr
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilon)
 
-        [<Property(MaxTest = 1000)>]
+        [<Property>]
         let ``RGB to HCY to RGB roundtrip`` (Color4LDR c) =
             let hcy = Color4.ToHcy c
             let rgb = Color4.FromHcy hcy
-            Assert.ApproximatelyEqualEpsilon(c, rgb, epsilon)
+            Assert.ApproximatelyEquivalent(c, rgb, epsilon)
 
             
 
