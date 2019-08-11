cac93c63d7cf55eef4ed90527fa715ff56d8a0b4
realvictorprm <mueller.vpr@gmail.com>
2019-08-11 21:39:16 +0200

    initial implementation of code generation

--- src/Generator/Program.fs
+++ src/Generator/Program.fs
@@ -1,4 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿// Learn more about F// at http://fsharp.org
 
 open System
 open Types
@@ -146,11 +146,342 @@ let looslyTypedFunctionsToTypedFunctions enumMap functions =
         }
     )
 
+let csharpTypeToSpecType =
+    [|
+        "BooleanPointer","GLboolean*"
+        "Boolean","GLboolean"
+        "Char","GLchar"
+        "CharPointer","GLchar*"
+        "ConstCharPointer","GLchar* const"
+        "CheckedFloat32","GLfloat"
+        "CheckedInt32","GLint"
+        "ClampedFixed","GLfixed"
+        "ClampedFloat32","GLclampf"
+        "ClampedFloat64","GLclampd"
+        "ClampedStencilValue","GLint"
+        "ColorB","GLbyte"
+        "ColorD","GLdouble"
+        "ColorF","GLfloat"
+        "ColorI","GLint"
+        "ColorIndexValueD","GLdouble"
+        "ColorIndexValueF","GLfloat"
+        "ColorIndexValueI","GLint"
+        "ColorIndexValueS","GLshort"
+        "ColorIndexValueUB","GLubyte"
+        "ColorS","GLshort"
+        "ColorUB","GLubyte"
+        "ColorUI","GLuint"
+        "ColorUS","GLushort"
+        "CompressedTextureARB","GLvoid*"
+        "CoordD","GLdouble"
+        "CoordF","GLfloat"
+        "CoordI","GLint"
+        "CoordS","GLshort"
+        "DrawBufferName","GLint"
+        "FeedbackElement","GLfloat"
+        "FenceNV","GLuint"
+        "Float32","GLfloat"
+        "Float32Pointer","GLfloat*"
+        "Float64","GLdouble"
+        "Float64Pointer","GLdouble*"
+        "Fixed","GLfixed"
+        "ConstFixed","GLfixed"
+        "FunctionPointer","_GLfuncptr"
+        "IglooParameterSGIX","GLvoid*"
+        "Int16","GLshort"
+        "Int32","GLint"
+        "Int8","GLbyte"
+        "LineStipple","GLushort"
+        "List","GLuint"
+        "MaskedColorIndexValueF","GLfloat"
+        "MaskedColorIndexValueI","GLuint"
+        "MaskedStencilValue","GLuint"
+        "ProgramCharacterNV","GLubyte"
+        "ReplacementCodeSUN","GLuint"
+        "SelectName","GLuint"
+        "SizeI","GLsizei"
+        "StencilValue","GLint"
+        "Texture","GLuint"
+        "UInt16","GLushort"
+        "UInt32","GLuint"
+        "UInt8","GLubyte"
+        "Void","GLvoid"
+        "VoidPointer","GLvoid*"
+        "ConstVoidPointer","GLvoid* const"
+        "WinCoord","GLint"
+        "ConstByte","GLbyte"
+        "ConstUByte","GLubyte"
+        "ConstFloat32","GLfloat"
+        "ConstInt32","GLint"
+        "ConstUInt32","GLuint"
+        "ConstVoid","GLvoid"
+        // ARB_vertex_buffer_object types and core equivalents for new types
+        "BufferOffset","GLintptr"
+        "BufferSize","GLsizeiptr"
+        "BufferOffsetARB","GLintptrARB"
+        "BufferSizeARB","GLsizeiptrARB"
+        // NV_half
+        "Half16NV","GLhalfNV"
+        // Generic types for as-yet-unspecified enums
+        "TypeEnum","GLenum"
+        "Int64","GLint64"
+        "UInt64","GLuint64"
+        // Object handle & data pointers
+        "handleARB","GLhandleARB"
+        "charARB","GLcharARB"
+        "charPointerARB","GLcharARB*"
+        "sync","GLsync"
+        // EXT_timer_query
+        "Int64EXT","GLint64EXT"
+        "UInt64EXT","GLuint64EXT"
+        // EXT_direct_state_access
+        "Framebuffer","GLuint"
+        "Intptr","GLintptr"
+        "Renderbuffer","GLuint"
+        "Sizeiptr","GLsizeiptr"
+        // NV_vdpau_interop
+        "vdpauSurfaceNV","GLvdpauSurfaceNV"
+        // External API types
+        "Path","GLuint"
+        "PathCommand","GLubyte"
+        "PathElement","GLvoid*"
+    |] |> Map.ofArray
+
+let specTypeToCSharpType =
+    [|
+        // Normal types.
+        "GLsizei","uint"
+        "GLsizeiptr","UIntPtr"
+        "GLintptr","IntPtr"
+        "GLboolean","bool" // Boolean // Int32
+        "GLbitfield","uint"
+        "GLvoid","void" //Object
+        "GLchar","char"
+        "GLbyte","sbyte"
+        "GLubyte","byte"
+        "GLshort","short"
+        "GLushort","ushort"
+        "GLint","int"
+        "GLuint","uint"
+        "GLfloat","float"
+        "GLclampf","float"
+        "GLdouble","double"
+        "GLclampd","double"
+        "GLstring","string"
+        "String","string"
+
+        // Pointer types
+        "GLvoid*","void*"
+        "GLchar*","string"
+        "GLcharARB*","string"
+        "GLfloat*","float*"
+        "GLdouble*","double*"
+        "GLboolean*","bool*"
+
+        // Note: ADL cannot marshal arrays as return types.
+        // There's no function in the spec that does but if one's added we need to look into this.
+        "GLchar**","string[]"
+
+        // ARB and NV types.
+        "GLsizeiptrARB","UIntPtr"
+        "GLintptrARB","IntPtr"
+        "GLhandleARB","uint"
+        "GLhalfARB","OpenToolkit.Mathematics.Half"
+        "GLhalfNV","OpenToolkit.Mathematics.Half"
+        "GLcharARB","char"
+
+        // 64 bit types (introduced in 2.1)
+        "GLint64EXT","long"
+        "GLuint64EXT","ulong"
+        "GLint64","long"
+        "GLuint64","ulong"
+
+        // ARB_sync (introduced in 3.2)
+        "sync","IntPtr"
+        "GLsync","		        IntPtr"
+
+        // Debug callbacks
+        "GLDEBUGPROC","DebugProc"
+        "GLDEBUGPROCAMD","DebugProcAmd"
+        "GLDEBUGPROCARB","DebugProcArb"
+        "GLDEBUGPROCKHR","DebugProcKhr"
+
+        // NV_vdpau
+        "GLvdpauSurfaceNV","IntPtr"
+
+        // Wgl types.
+        "PROC","IntPtr"
+        "LPCSTR","string"
+        "COLORREF","int"
+        "BOOL","bool"
+        "DWORD","int"
+        "FLOAT","float"
+        "HANDLE","IntPtr"
+        "HDC","IntPtr"
+        "HGLRC","IntPtr"
+        "HPBUFFERARB","IntPtr" //HPBUFFERARB
+        "HPBUFFEREXT","IntPtr" //HPBUFFEREXT
+        "INT32","int"
+        "INT64","long"
+        "LPVOID","void*"
+        "UINT","uint"
+        "USHORT","ushort"
+        "VOID","void"
+        "VoidPointer","void*"
+
+        // Glu types.
+        "Float64","double"
+        "Float64Pointer","double*"
+        "Float32","float"
+        "Float32Pointer","float*"
+
+        // Glx types.
+        "Void","void"
+        "Bool","bool"
+        "int64_t","long"
+        "int32_t","int"
+
+        "Display","IntPtr"
+        "Window","IntPtr"
+        "Pixmap","IntPtr"
+        "Colormap","IntPtr"
+
+        "GLXWindow","IntPtr"
+        "GLXContext","IntPtr"
+        "GLXDrawable","IntPtr"
+        "GLXPixmap","IntPtr"
+        "__GLXextFuncPtr","IntPtr"
+
+        "VLServer","IntPtr"
+        "VLPath","IntPtr"
+        "VLNode","IntPtr"
+
+        // OpenGL|ES types.
+        "GLclampx","int"
+        "GLfixed","int"
+        "GLeglImageOES","IntPtr"
+
+        // External egl buffer type added in OpenGL 4.6
+        "GLeglClientBufferEXT","IntPtr"
+
+        // nVidia vulkan entry point added by NV_draw_vulkan_image extension
+        "GLVULKANPROCNV","IntPtr"
+
+        // OpenCL types.
+        "_cl_context","IntPtr"
+        "_cl_event","IntPtr"
+        "cl_command_queue","IntPtr"
+        "cl_context","IntPtr"
+        "cl_device_id","IntPtr"
+        "cl_event","IntPtr"
+        "cl_kernel","IntPtr"
+        "cl_mem","IntPtr"
+        "cl_platform_id","IntPtr"
+        "cl_program","IntPtr"
+        "cl_sampler","IntPtr"
+        "size_t","IntPtr"
+
+        "cl_bool","bool"
+        "cl_int","int"
+        "cl_uint","uint"
+        "uchar","byte"
+
+        "cl_addressing_mode","AddressingMode"
+        "cl_command_queue_info","CommandQueueInfo"
+        "cl_command_queue_properties","CommandQueueProperties"
+        "cl_context_info","ContextInfo"
+        "cl_context_properties","IntPtr" // ContextProperties
+        "cl_device_info","DeviceInfo"
+        "cl_device_type","DeviceType"
+        "cl_event_info","EventInfo"
+        "cl_filter_mode","FilterMode"
+        "cl_image_format","ImageFormat"
+        "cl_image_info","ImageInfo"
+        "cl_kernel_group_info","KernelGroupInfo"
+        "cl_kernel_info","KernelInfo"
+        "cl_kernel_work_group_info","KernelWorkGroupInfo"
+        "cl_map_flags","MapFlags"
+        "cl_mem_info","MemInfo"
+        "cl_mem_flags","MemFlags"
+        "cl_mem_object_type","MemObjectType"
+        "cl_platform_info","PlatformInfo"
+        "cl_profiling_info","ProfilingInfo"
+        "cl_program_build_info","ProgramBuildInfo"
+        "cl_program_info","ProgramInfo"
+        "cl_sampler_info","SamplerInfo"
+        "cl_work_group_info","WorkGroupInfo"
+    |] |> Map.ofArray
+
 open System.IO
+
+let rec typeToString (ty: GLType) =
+    match ty with
+    | GLType.Void -> "void"
+    | GLType.GLenum inner -> inner.groupName
+    | GLType.Pointer inner ->
+        typeToString inner + "*"
+    | other ->
+        let fallback = other.ToString()
+        specTypeToCSharpType
+        |> Map.tryFind fallback
+        |> Option.defaultValue fallback
+
+let generateInterface enums (functions: TypedFunctionDeclaration[]) =
+    use backing = new StringWriter()
+    use writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
+    let writeLine (str: string) = writer.WriteLine str
+    let indent() = writer.Indent <- writer.Indent + 1
+    let unindent() = writer.Indent <- writer.Indent - 1
+    writeLine "namespace FooBar"
+    writeLine "{"
+    indent()
+    
+    for enum in enums do
+        if enum.cases.Length > 0 then
+            sprintf "enum %s" enum.groupName
+            |> writeLine
+            writeLine "{"
+            indent()
+            let formattedCases =
+                enum.cases
+                |> Array.Parallel.map(fun case ->
+                    sprintf "%s = %s" case.name case.value
+                )
+            if formattedCases.Length > 2 then
+                for case in formattedCases.[..formattedCases.Length - 2] do
+                    writeLine (case + ",")
+            formattedCases.[formattedCases.Length - 1]
+            |> writeLine
+            unindent()
+            writeLine "}"
+        
+
+    writeLine "public interface IGL"
+    writeLine "{"
+    indent()
+
+    functions
+    |> Seq.iter(fun func ->
+        let retTypeAsString = func.retType |> typeToString
+        
+        let formattedParams =
+            func.parameters
+            |> Array.Parallel.map(fun p -> sprintf "%s %s" (p.typ |> typeToString) p.name)
+            |> String.concat ", "
+        sprintf "%s %s(%s);" retTypeAsString func.name formattedParams
+        |> writeLine
+        writeLine ""
+    )
+    unindent()
+    writeLine "}"
+    unindent()
+    writeLine "}"
+    backing.Flush()
+    File.WriteAllText("./functions.cs", backing.ToString())
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Hello World from F//!"
     let path = @"D:\repos\opentk\src\Generator\gl.xml"
     let test = Types.OpenGL_Specification.Load path
     let enums = getEnumsFromSpecification test
@@ -172,20 +503,6 @@ let main argv =
     //)
     let functions = getFunctions test
     printfn "Function count: %d" functions.Length
-    use writer = new StringWriter()
-    functions
-    |> Seq.iter(fun func ->
-        sprintf "-- func %A --" func.funcName
-        |> writer.WriteLine
-        func.parameters
-        |> Seq.iteri(fun i p ->
-            sprintf "parameter %d, name: %A, type: %A" i p.paramName p.paramType
-            |> writer.WriteLine
-        )
-        writer.WriteLine "-----"
-    )
-    writer.Flush()
-    File.WriteAllText("./functions.txt", writer.ToString())
     let possibleTypes =
         functions
         |> Array.Parallel.collect(fun func ->
@@ -202,6 +519,7 @@ let main argv =
     //    printfn "%A" possibleType
     let typecheckedFunctions = looslyTypedFunctionsToTypedFunctions enumMap functions
     printfn "overall correct function specifications: %d" typecheckedFunctions.Length
+    generateInterface enums typecheckedFunctions
     //for func in typecheckedFunctions |> Array.take 100 do
     //    printfn "%A" func
 
