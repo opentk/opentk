3164b879718cefcb160d015b9a8b79c2d08024ca
realvictorprm <mueller.vpr@gmail.com>
2019-08-16 20:19:03 +0200

    Refactored printing slightly

--- src/Generator/Formatting.fs
+++ src/Generator/Formatting.fs
@@ -2,81 +2,33 @@
 open Types
 open Constants
 open TypeMapping
-open System.IO
 
-type StringWriterBuilder() =
-    let backing = new StringWriter()
-    let writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
-    [<CustomOperation("writeLine")>]
-    member __.WriteLine (_, str: string) =
-        writer.WriteLine str
+type WriteStatement =
+    | WriteLine of string
+    | Write of string
+    | Indent
+    | Unindent
 
-    [<CustomOperation("writeLines")>]
-    member __.WriteLines(_, strs: string[]) =
-        strs
-        |> Seq.iter(fun line ->
-            writer.WriteLine line
-        )
-        
-    [<CustomOperation("returnStringWriter")>]
-    member __.GetStringWriter (_) = backing
-        
-    [<CustomOperation("write")>]
-    member __.Write (_, str: string) =
-        writer.Write str
-    
-    [<CustomOperation("indent")>]
-    member __.Indent(()) =
-        writer.Indent <- writer.Indent + 1
-    
-    [<CustomOperation("unindent")>]
-    member __.Unindent(()) =
-        writer.Indent <- max (writer.Indent - 1) 0
-    
-    member __.Bind _ = ()
+module WriteStatement =
+    let writeLine s = WriteLine s
+    let write s = Write s
+    let indent = Indent
+    let unindent = Unindent
 
-    [<CustomOperation("toString")>]
-    member __.toString(()) =
+    let execute statements =
+        use backing = new System.IO.StringWriter()
+        use writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
+        let executeStatement statement =
+            match statement with
+            | WriteLine str -> writer.WriteLine str
+            | Write str -> writer.Write str
+            | Indent -> writer.Indent <- writer.Indent + 1
+            | Unindent -> writer.Indent <- max 0 (writer.Indent - 1)
+        statements
+        |> Seq.iter executeStatement 
         writer.Flush()
         backing.Flush()
         backing.ToString()
-
-    member __.Return _ = ()
-
-    [<CustomOperation("fecker", MaintainsVariableSpaceUsingBind = true)>]
-    member __.Fecker(_, [<ProjectionParameter>] a) =
-        ()
-
-    member __.Yield _ = ()
-
-    member __.For(_, _) = " asdiajsodj"
-
-    member __.Zero _ = backing.ToString()
-
-let writer() = StringWriterBuilder()
-
-
-
-//let test =
-//    writer() {
-//        //writeLine "foobar"
-//        //indent
-//        //writeLine "indented"
-//        //unindent
-//        writeLine ""
-//        let test = 
-//            writer() {
-//                for i in 0..10 do
-//                    writeLine "a"
-//                fecker ()
-//            }
-//        writeLines
-//            [| for i in 0 .. 10 ->
-//                    string i |]
-//            //writeLine ""
-//        //writeLine "continue writing"
-//        //toString
-//    }
 
 let rec typeToString (ty: GLType) =
     match ty with
@@ -102,6 +54,12 @@ let formatParam (p: TypedParameterInfo) =
 
 let outputPath = "../binding/Binding"
 
+open WriteStatement
+
+let writeLeftBracket = WriteLine "{"
+let writeRightBracket = WriteLine "}"
+let writeEmptyLine = WriteLine ""
+
 let generateDummyTypes () =
     let usings = 
         [
@@ -110,168 +68,146 @@ let generateDummyTypes () =
         
     let formatDummyType ty =
         sprintf "public struct %s {}" ty.name
-    writer() {
-        for using in usings do
-            sprintf "using %s;" using
-            |> writeLine
 
-        writeLine ""
-        writeLine "namespace FooBar"
-        writeLine "{"
-        indent()
 
+    seq {
+        for using in usings ->
+            writeLine (sprintf "using %s;" using)
+            
+        yield writeEmptyLine
+        yield writeLine "namespace FooBar"
+        yield writeLeftBracket
+        yield indent
         for ty in additionalTypesToGenerate do
             match ty._namespace with
             | Some n ->
-                sprintf "namespace %s" n
-                |> writeLine
-                writeLine "{"
-                indent()
-                formatDummyType ty
-                |> writeLine
-                unindent()
-                writeLine "}"
+                yield writeLine (sprintf "namespace %s" n)
+                yield writeLeftBracket
+                yield indent
+                yield formatDummyType ty |> writeLine
+                yield unindent
+                yield writeRightBracket
             | None ->
-                formatDummyType ty
-                |> writeLine
+                yield formatDummyType ty |> writeLine
 
-        unindent()
-        writeLine "}"
-        toString
-    }
-    backing.Flush()
-    backing.ToString()
+        yield unindent
+        yield writeRightBracket
+    } |> execute
 
-let generateEnums enums openGl=
-    use backing = new StringWriter()
-    use writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
-    let writeLine (str: string) = writer.WriteLine str
-    let indent() = writer.Indent <- writer.Indent + 1
-    let unindent() = writer.Indent <- writer.Indent - 1
+let generateEnums enums openGl =
     let usings = 
         [
             "System"
         ]
-    for using in usings do
-        sprintf "using %s;" using
-        |> writeLine
-
-    writeLine ""
-    writeLine "namespace FooBar"
-    writeLine "{"
-    indent()
+    seq {
+        for using in usings ->
+            sprintf "using %s;" using
+            |> writeLine
     
-    for enum in enums do
-        if enum.cases.Length > 0 then
-            sprintf "public enum %s" enum.groupName
-            |> writeLine
-            writeLine "{"
-            indent()
-            let formattedCases =
-                let valueToString value =
-                    match value with
-                    | "0xFFFFFFFF" -> "-1"
-                    | "0x80000000" ->
-                        let value = 0x80000000
-                        value |> string
-                    | _ -> value
-                enum.cases
-                |> Array.Parallel.map(fun case ->
-                    sprintf "%s = %s" case.name (valueToString case.value)
-                )
-            if formattedCases.Length > 2 then
-                for case in formattedCases.[..formattedCases.Length - 2] do
-                    writeLine (case + ",")
-            formattedCases.[formattedCases.Length - 1]
-            |> writeLine
-            unindent()
-            writeLine "}"
+        yield writeEmptyLine
+        yield writeLine "namespace FooBar"
+        yield writeLeftBracket
+        yield indent
+    
+        for enum in enums do
+            if enum.cases.Length > 0 then
+                yield
+                    sprintf "public enum %s" enum.groupName
+                    |> writeLine
+                yield writeLeftBracket
+                yield indent
+                let formattedCases =
+                    let valueToString value =
+                        match value with
+                        | "0xFFFFFFFF" -> "-1"
+                        | "0x80000000" ->
+                            let value = 0x80000000
+                            value |> string
+                        | _ -> value
+                    enum.cases
+                    |> Array.Parallel.map(fun case ->
+                        sprintf "%s = %s" case.name (valueToString case.value)
+                    )
+                if formattedCases.Length > 2 then
+                    for case in formattedCases.[..formattedCases.Length - 2] ->
+                        writeLine (case + ",")
+                yield
+                    formattedCases.[formattedCases.Length - 1]
+                    |> writeLine
+                yield unindent
+                yield writeRightBracket
 
-    unindent()
-    writeLine "}"
-    backing.Flush()
-    backing.ToString()
+        yield unindent
+        yield writeRightBracket
+    } |> execute
 
 let generateInterface (functions: TypedFunctionDeclaration[]) =
-    use backing = new StringWriter()
-    use writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
-    let writeLine (str: string) = writer.WriteLine str
-    let indent() = writer.Indent <- writer.Indent + 1
-    let unindent() = writer.Indent <- writer.Indent - 1
     let usings = 
         [
             "System"
         ]
-    for using in usings do
-        sprintf "using %s;" using
-        |> writeLine
+    seq {
+        for using in usings ->
+            sprintf "using %s;" using
+            |> writeLine
 
-    writeLine ""
-    writeLine "namespace FooBar"
-    writeLine "{"
-    indent()
+        yield writeEmptyLine
+        yield writeLine "namespace FooBar"
+        yield writeLeftBracket
+        yield indent
 
-    writeLine "public interface IGL"
-    writeLine "{"
-    indent()
+        yield writeLine "public interface IGL"
+        yield writeLeftBracket
+        yield indent
 
-    functions
-    |> Seq.iter(fun func ->
-        let retTypeAsString = func.retType |> typeToString
-        
-        let formattedParams =
-            func.parameters
-            |> Array.Parallel.map formatParam
-            |> String.concat ", "
-        sprintf "unsafe %s %s(%s);" retTypeAsString func.name formattedParams
-        |> writeLine
-        writeLine ""
-    )
-    unindent()
-    writeLine "}"
-    unindent()
-    writeLine "}"
-    backing.Flush()
-    backing.ToString()
+        for func in functions do
+            let retTypeAsString = func.retType |> typeToString
+            
+            let formattedParams =
+                func.parameters
+                |> Array.Parallel.map formatParam
+                |> String.concat ", "
+            yield sprintf "unsafe %s %s(%s);" retTypeAsString func.name formattedParams
+            |> writeLine
+            yield writeEmptyLine
+        yield unindent
+        yield writeRightBracket
+        yield unindent
+        yield writeRightBracket
+    } |> execute
 
 let generateStaticClass (functions: TypedFunctionDeclaration[]) =
-   use backing = new StringWriter()
-   use writer = new System.CodeDom.Compiler.IndentedTextWriter(backing)
-   let writeLine (str: string) = writer.WriteLine str
-   let indent() = writer.Indent <- writer.Indent + 1
-   let unindent() = writer.Indent <- writer.Indent - 1
-   let usings = 
+    let usings = 
         [
             "System"
         ]
-   for using in usings do
-        sprintf "using %s;" using
-        |> writeLine
-   writeLine "namespace FooBar"
-   writeLine "{"
-   indent()
+    seq {
+        for using in usings ->
+            sprintf "using %s;" using
+            |> writeLine
+        yield writeLine "namespace FooBar"
+        yield writeLeftBracket
+        yield indent
 
-   writeLine "public static partial class GL"
-   writeLine "{"
-   indent()
+        yield writeLine "public static partial class GL"
+        yield writeLeftBracket
+        yield indent
 
-   functions
-   |> Seq.iter(fun func ->
-       let retTypeAsString = func.retType |> typeToString
+        for func in functions do
+            let retTypeAsString = func.retType |> typeToString
        
-       let formattedParams =
-           func.parameters
-           |> Array.Parallel.map formatParam
-           |> String.concat ", "
-       let funcName = func.name
-       let formattedParamNames = func.parameters |> Array.Parallel.map(fun p -> p.name |> formatName) |> String.concat ", "
-       sprintf "public static unsafe %s %s(%s) => instance.%s(%s);" retTypeAsString funcName formattedParams funcName formattedParamNames
-       |> writeLine
-       writeLine ""
-   )
-   unindent()
-   writeLine "}"
-   unindent()
-   writeLine "}"
-   backing.Flush()
-   backing.ToString()
+            let formattedParams =
+                func.parameters
+                |> Array.Parallel.map formatParam
+                |> String.concat ", "
+            let funcName = func.name
+            let formattedParamNames = func.parameters |> Array.Parallel.map(fun p -> p.name |> formatName) |> String.concat ", "
+            yield
+                sprintf "public static unsafe %s %s(%s) => instance.%s(%s);" retTypeAsString funcName formattedParams funcName formattedParamNames
+                |> writeLine
+            yield writeEmptyLine
+        yield unindent
+        yield writeRightBracket
+        yield unindent
+        yield writeRightBracket
+    } |> execute
--- src/Generator/Script1.fsx
+++ src/Generator/Script1.fsx
@@ -4,10 +4,10 @@
     | Indent
     | Unindent
 
-type LazyWriter() =
+and LazyWriter() =
     let statements = ResizeArray()
     
-    member __.Add statement = statements.Add statement
+    member __.Add(statement: WriteStatements) = statements.Add statement
 
     member __.Statements = statements
 
@@ -23,6 +23,7 @@ module LazyWriter =
             | Indent -> writer.Indent <- writer.Indent + 1
             | Unindent -> writer.Indent <- max 0 (writer.Indent - 1)
 
+
         lazyWriter.Statements
         |> Seq.iter executeStatement 
         writer.Flush()
@@ -33,59 +34,34 @@ type WriterV2() =
     member __.Yield(a: int) =
         printfn "yield %d" a
         LazyWriter()
-    member __.Yield(()) = LazyWriter()
 
-    member __.For(a: seq<'a>, b : 'a -> LazyWriter) =
-        printfn "a is %A" a
-        printfn "b is %A" b
-        a
-        |> Seq.map b
-        |> Seq.reduce (fun writer1 writer2 ->
-            printfn "writer1 %A" writer1.Statements
-            printfn "writer2 %A" writer2.Statements
-            for statement in writer1.Statements do
-                writer2.Add statement
-            writer2
-        )
+    member __.Yield(()) = LazyWriter()
 
     member __.For(lazyWriter: LazyWriter, b : unit -> LazyWriter) =
         let res = b()
         //for statement in res.st
         printfn "res %A" res
         for statement in res.Statements do
-            printfn "statement: %A" statement
             lazyWriter.Add statement
         lazyWriter
 
-    member __.Combine(f,g) = ()
+    member __.Combine(f,g) =
+        printfn "combine"
+        ()
 
-    member __.YieldFrom f  =
-        printfn "yield from: %A" f
-        f
+    member __.Bind(a: seq<LazyWriter>, fn) =
+        a
+        |> Seq.reduce(fun writer1 writer2 ->
+            for statement in writer2.Statements do
+                writer1.Add statement
+            writer1
+        )
 
-    //member __.For(a: LazyWriter, b: unit -> WriteStatements list) =
-    //    b() |> List.iter (a.Add)
-    //    a
-
-    //member __.For(a: string) =
-    //    LazyWriter()
-    
-    //member __.For(a: LazyWriter, b: string) =
-    //    a
-
-    //member __.For(a: seq<'a>, fn: ('a -> LazyWriter)) =
-    //    a
-    //    //|> Seq.map fn
-    //    //|> Seq.reduce (fun writer1 writer2 ->
-    //    //    for statement in writer1.Statements do
-    //    //        writer2.Add statement
-    //    //    writer2
-    //    //)
-
+    member __.Delay(f) = f()
 
     [<CustomOperation("write")>]
     member __.Write(writer: LazyWriter, str) =
-        printfn "%A" writer.Statements
+        printfn "write %A" str
         Write str
         |> writer.Add
         writer
@@ -114,12 +90,18 @@ let writeV2 = WriterV2()
 let test2 =
     writeV2 {
         write "a"
-        for i in 0..10 do
-            printfn "%A" i
-            write "in loop"
-            write "in loop"
-            indent
-            write "in loop"
-            unindent
-            write "in loop"
+        do!
+            Seq.init 10 <| fun _ ->
+                writeV2 {
+                    indent
+                    writeLine "a b c"
+                    unindent
+                    writeLine "d e f"
+                }
+        write "in loop"
+        write "in loop"
+        indent
+        write "in loop"
+        unindent
+        write "in loop"
     } |> LazyWriter.execute
