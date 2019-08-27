2cd54956743bf3bddc43f5662626dfdb7f049f4d
jvbsl <jvsbl@gmx.de>
2019-08-28 00:16:49 +0200

    a little refactor

--- src/Generator/Constants.fs
+++ src/Generator/Constants.fs
@@ -45,27 +45,27 @@ let rec getBaseGLType t =
     | ArrayType(s) -> getBaseGLType s
     | _ -> t 
 let sufixToRemove =
-    [| ("1", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("2", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("3", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("4", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("fv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("ubv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("u", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("uiv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("ui", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("usv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("us", fun (d : PrintReadyTypedFunctionDeclaration) -> (Array.exists (fun (p : PrintReadyTypedParameterInfo) -> (getBaseGLType p.typ.typ) = GLType.GLushort) d.parameters ));
-       ("i", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("iv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("f", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("b", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("d", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("dv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("s", fun (d : PrintReadyTypedFunctionDeclaration) -> (Array.exists (fun (p : PrintReadyTypedParameterInfo) -> (getBaseGLType p.typ.typ) = GLType.GLshort) d.parameters ));
-       ("sv", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("ub", fun (d : PrintReadyTypedFunctionDeclaration) -> true);
-       ("v", fun (d : PrintReadyTypedFunctionDeclaration) -> true)
+    [| ("1", fun (_ : PrintReadyTypedFunctionDeclaration) -> true);
+       ("2", fun _ -> true);
+       ("3", fun _ -> true);
+       ("4", fun _ -> true);
+       ("fv", fun _ -> true);
+       ("ubv", fun _ -> true);
+       ("u", fun _ -> true);
+       ("uiv", fun _ -> true);
+       ("ui", fun _ -> true);
+       ("usv", fun _ -> true);
+       ("us", fun d -> (Array.exists (fun (p : PrintReadyTypedParameterInfo) -> (getBaseGLType p.typ.typ) = GLType.GLushort) d.parameters ));
+       ("i", fun _ -> true);
+       ("iv", fun _ -> true);
+       ("f", fun _ -> true);
+       ("b", fun _ -> true);
+       ("d", fun _ -> true);
+       ("dv", fun _ -> true);
+       ("s", fun d -> (Array.exists (fun (p : PrintReadyTypedParameterInfo) -> (getBaseGLType p.typ.typ) = GLType.GLshort) d.parameters ));
+       ("sv", fun _ -> true);
+       ("ub", fun _ -> true);
+       ("v", fun _ -> true)
        |]
     |> Array.sortByDescending (fun (s,f) -> s.Length)
 
