26852b5eff377ef56a94d11d8b5f8297d155d3e3
H. Utku Maden <utkumaden@hotmail.com>
2019-10-20 16:26:27 +0300

    Made DpiCalculator use DpiInfo instead.

--- src/Windowing/OpenToolkit.Windowing.Desktop/DpiCalculator.cs
+++ src/Windowing/OpenToolkit.Windowing.Desktop/DpiCalculator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.InteropServices;
 using OpenToolkit.GraphicsLibraryFramework;
 
@@ -9,46 +10,14 @@ namespace OpenToolkit.Windowing.Desktop
     /// <summary>
     /// This class contains methods used for calculating dpi.
     /// </summary>
-    internal static class DpiCalculator
+    internal static partial class DpiCalculator
     {
-        private static Rectangle[] monitorClientAreas = null;
-        private static unsafe Monitor** monitors = null;
-        private static int monitorCount;
+        private static List<DpiInfo> _dpiInfos = new List<DpiInfo>();
 
-        private static unsafe Rectangle[] MonitorClientAreas
-        {
-            get
-            {
-                Cache();
+        // I am not sure how well of an idea this is.
+        private static Dictionary<IntPtr, int> _dpiIndexLookup = new Dictionary<IntPtr, int>();
 
-                return monitorClientAreas;
-            }
-        }
-
-        private static bool isCacheValid = false; // To detect redundant calls.
-
-        private static unsafe void Cache()
-        {
-            // Don't handle redundant calls.
-            if (isCacheValid)
-            {
-                return;
-            }
-
-            monitors = GLFWProvider.GLFW.Value.GetMonitors(out monitorCount);
-            monitorClientAreas = GetMonitorClientAreas(monitors, monitorCount);
-
-            isCacheValid = true;
-        }
-
-        /// <summary>
-        /// Invalidates local monitor cache.
-        /// </summary>
-        /// <remarks>
-        /// This function should be called when the monitor configuration is changed to ensure
-        /// some such as <see cref="GetWindowMonitorByArea(Window*)"/> work correctly.
-        /// </remarks>
-        public static void InvalidateCache() => isCacheValid = false;
+        private static bool _isCacheBuilt = false;
 
         /// <summary>
         /// Gets the current monitor scale.
@@ -63,14 +32,10 @@ namespace OpenToolkit.Windowing.Desktop
             out float verticalScale
         )
         {
-            if (monitor != null)
+            if (TryGetFromCache((IntPtr)monitor, out DpiInfo info))
             {
-                GLFWProvider.GLFW.Value.GetMonitorContentScale(
-                    monitor,
-                    out horizontalScale,
-                    out verticalScale
-                );
-
+                horizontalScale = info.HorizontalScale;
+                verticalScale = info.VerticalScale;
                 return true;
             }
             else
@@ -95,12 +60,17 @@ namespace OpenToolkit.Windowing.Desktop
         /// </remarks>
         public static unsafe bool TryGetMonitorDpi(Monitor* monitor, out float horizontalDpi, out float verticalDpi)
         {
-            float defaultDpi = DpiCalculator.GetPlatformDefaultDpi();
-            bool success = TryGetMonitorScale(monitor, out float horizontalScale, out float verticalScale);
-
-            horizontalDpi = defaultDpi * horizontalScale;
-            verticalDpi = defaultDpi * verticalScale;
-            return success;
+            if (TryGetFromCache((IntPtr)monitor, out DpiInfo info))
+            {
+                horizontalDpi = info.HorizontalDpi;
+                verticalDpi = info.VerticalDpi;
+                return true;
+            }
+            else
+            {
+                horizontalDpi = verticalDpi = GetPlatformDefaultDpi();
+                return false;
+            }
         }
 
         /// <summary>
@@ -117,48 +87,17 @@ namespace OpenToolkit.Windowing.Desktop
         /// </remarks>
         public static unsafe bool TryGetMonitorDpiRaw(Monitor* monitor, out float horizontalDpi, out float verticalDpi)
         {
-            horizontalDpi = verticalDpi = GetPlatformDefaultDpi();
-
-            if (monitor == null)
+            if (TryGetFromCache((IntPtr)monitor, out DpiInfo info))
             {
+                horizontalDpi = info.HorizontalRawDpi;
+                verticalDpi = info.VerticalRawDpi;
+                return true;
+            }
+            else
+            {
+                horizontalDpi = verticalDpi = GetPlatformDefaultDpi();
                 return false;
             }
-
-            int widthInMm, heightInMm;
-            int widthInPixels, heightInPixels;
-
-            // According to GLFW documentation, physical dimensions are reported in millimeters.
-            GLFWProvider.GLFW.Value.GetMonitorPhysicalSize(monitor, out widthInMm, out heightInMm);
-
-            var videoMode = GLFWProvider.GLFW.Value.GetVideoMode(monitor);
-
-            if (videoMode == null)
-            {
-                return false;
-            }
-
-            widthInPixels = videoMode->Width;
-            heightInPixels = videoMode->Height;
-
-            if (widthInMm <= 0 || heightInMm <= 0 || widthInPixels <= 0 || heightInPixels <= 0)
-            {
-                return false;
-            }
-
-            /*
-             * How does this formula calculate dpi?
-             *
-             * 1. 1" = 25.4mm thus dInMm / 25.4 = dInInches
-             * 2. dpi = pixelCount/dInInches
-             *
-             * Ergo:
-             *     pixelCount / (dInMm / 25.4)
-             *   = (pixelCount / dInMm) * 25.4
-             */
-
-            horizontalDpi = ((float)widthInPixels / (float)widthInMm) * 25.4f;
-            verticalDpi = ((float)heightInPixels / (float)heightInMm) * 25.4f;
-            return true;
         }
 
         /// <summary>
@@ -210,11 +149,9 @@ namespace OpenToolkit.Windowing.Desktop
         /// <returns>The monitor which the window intersects with the most.</returns>
         public static unsafe Monitor* GetWindowMonitorByArea(Window* window)
         {
-            var monitorAreas = MonitorClientAreas;
-
-            if (monitorCount == 0)
+            if (!CheckCache())
             {
-                return null;
+                throw new Exception("This method can only be called from the main GLFW thread.");
             }
 
             Rectangle windowArea;
@@ -226,18 +163,95 @@ namespace OpenToolkit.Windowing.Desktop
             }
 
             int selectedIndex = 0;
-            for (int i = 0; i < monitorCount; i++)
+            for (int i = 0; i < _dpiInfos.Count; i++)
             {
                 if (
-                    GetRectangleIntersectionArea(monitorAreas[i], windowArea) >
-                    GetRectangleIntersectionArea(monitorAreas[selectedIndex], windowArea)
+                    GetRectangleIntersectionArea(_dpiInfos[i].ClientArea, windowArea) >
+                    GetRectangleIntersectionArea(_dpiInfos[selectedIndex].ClientArea, windowArea)
                 )
                 {
                     selectedIndex = i;
                 }
             }
 
-            return monitors[selectedIndex];
+            return _dpiInfos[selectedIndex].Handle;
+        }
+
+        /// <summary>
+        /// Checks wheter the cache has been built or builds it if it can.
+        /// </summary>
+        /// <returns>Wether the current cache is valid or not.</returns>
+        public static bool CheckCache()
+        {
+            if (!_isCacheBuilt && GLFWProvider.IsOnMainThread)
+            {
+                BuildMonitorChache();
+            }
+
+            return _isCacheBuilt;
+        }
+
+        /// <summary>
+        /// Tries to get a <see cref="DpiInfo"/> object from the prebuilt cache.
+        /// </summary>
+        /// <param name="index">The monitor index of the object.</param>
+        /// <param name="info">The cached object.</param>
+        /// <returns>True when the object was retrieved from cache successfully.</returns>
+        public static bool TryGetFromCache(int index, out DpiInfo info)
+        {
+            info = _dpiInfos.ElementAtOrDefault(index);
+            return info != null;
+        }
+
+        /// <summary>
+        /// Tries to get a <see cref="DpiInfo"/> object from the prebuilt cache.
+        /// </summary>
+        /// <param name="monitor">An opaque handle to the monitor.</param>
+        /// <param name="info">The cached object.</param>
+        /// <returns>True when the object was retrieved from cache successfully.</returns>
+        public static bool TryGetFromCache(IntPtr monitor, out DpiInfo info)
+        {
+            if (_dpiIndexLookup.TryGetValue(monitor, out int index))
+            {
+                return TryGetFromCache(index, out info);
+            }
+
+            info = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Builds the monitor cache (again if called before).
+        /// </summary>
+        public static unsafe void BuildMonitorChache()
+        {
+            if (!GLFWProvider.IsOnMainThread)
+            {
+                throw new InvalidOperationException("Only GLFW main thread can build the monitor chache.");
+            }
+
+            // The function needs exclusive rights to the list (and the index lookup), no other thread should read
+            // from or write to the list.
+            // A separate object is not used because it would not inhibit other threads from reading the list
+            // without the performance cost of locking the separate object to read.
+            lock (_dpiInfos)
+            lock (_dpiIndexLookup)
+            {
+                _dpiInfos.Clear(); // Just in case it was built before.
+                _dpiIndexLookup.Clear();
+
+                var monitors = GLFWProvider.GLFW.Value.GetMonitors(out int monitorCount);
+
+                for (int i = 0; i < monitorCount; i++)
+                {
+                    var monitor = *(monitors + i);
+
+                    _dpiInfos.Add(new DpiInfo(monitor));
+                    _dpiIndexLookup.Add((IntPtr)monitor, i);
+                }
+            }
+
+            _isCacheBuilt = true;
         }
     }
 }
