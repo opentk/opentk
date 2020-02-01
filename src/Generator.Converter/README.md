aec3ed26372e785f0cead9d721e0044f6ad1de59
glopesdev <glopesdev@users.noreply.github.com>
2020-02-01 17:57:28 +0000

    Bring back 3.x binding generators

---
+++ src/Generator.Converter/README.md
@@ -0,0 +1,24 @@
+﻿### Introduction
+
+This is a simple tool to convert Khronos XML to OpenTK XML files.
+
+### Usage
+```
+Converting local files:
+  Convert.exe --input-files gl.xml --output-file signatures.xml --prefix gl
+Converting latest remote specification:
+  Convert.exe --input-files https://raw.githubusercontent.com/KhronosGroup/OpenGL-Registry/master/xml/gl.xml --output-file signatures.xml --prefix gl
+
+  -p, --prefix         Required. (Default: gl) The prefix to remove from parsed functions and constants.
+
+  -o, --output-file    The path to the output file. Defaults to stdout if no path is provided.
+
+  -i, --input-files    Required. A list of the Khronos XML files to parse into OpenTK XML. Remote resources in the form of URLs are supported.
+
+  --help               Display this help screen.
+
+  --version            Display version information.
+```
+### Support
+
+If you encounter a bug, please file an issue report at the OpenTK [issue tracker](http://github.com/opentk/opentk/issues).
---
+++ src/Generator.Rewrite/Options.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace OpenTK.Rewrite
+{
+    /// <summary>
+    /// A container class used by <see cref="CommandLine.Parser"/> to parse command line arguments.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets the path to the target assembly that should be rewritten.
+        /// </summary>
+        [Option('a', "assembly", Required = true,
+            HelpText = "The path to the target assembly that should be rewritten.")]
+        public string TargetAssembly { get; set; }
+
+        /// <summary>
+        /// Get or sets the path to the strong name key which should be used to sign or resign the assembly.
+        /// </summary>
+        [Option('k', "signing-key",
+            HelpText = "The path to the strong name key which should be used to sign or resign the assembly.")]
+        public string StrongNameKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether calls to GL.GetError() are wrapped around each native call.
+        /// </summary>
+        [Option('d', "debug", Default = false,
+            HelpText = "Enable calls to GL.GetError(), wrapped around each native call.")]
+        public bool EnableDebugCalls { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether native calls are forced to use DllImport instead of GetProcAddress.
+        /// </summary>
+        [Option("dllimport", Default = false,
+            HelpText = "Force native calls to use DllImport instead of GetProcAddress.")]
+        public bool UseDLLImport { get; set; }
+    }
+}
