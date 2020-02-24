79bb385e61f68887f7de774f817d2cd57b9f53a2
Jarl Erik Schmidt <github@jarlerik.com>
2020-02-24 22:53:28 +0100

    Vector3 projection + angle

---
+++ bench/OpenToolkit.Benchmarks.Mathematics/Vector/Vector3.Angle.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using OpenToolkit.Mathematics;
+
+namespace OpenToolkit.Benchmarks.Mathematics.Vector
+{
+    [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter]
+    [BenchmarkCategory("Vector", "Vector3", "Angle")]
+    public class Vector3Angle
+    {
+        private Vector3 _first = Vector3.One;
+        private Vector3 _second = Vector3.One / 2;
+
+        private float _result;
+
+        [Benchmark(Description = "Vector3.CalculateAngle(Vector3 first, Vector3 second)")]
+        public void Vector3_CalculateAngleByValue()
+        {
+            _result = Vector3.CalculateAngle(_first, _second);
+        }
+
+        [Benchmark(Description = "Vector3.CalculateAngle(ref Vector3 first, ref Vector3 second, out float result)")]
+        public void Vector3_CalculateAngleByReference()
+        {
+            Vector3.CalculateAngle(ref _first, ref _second, out _result);
+        }
+    }
+}
---
+++ bench/OpenToolkit.Benchmarks.Mathematics/Vector/Vector3.Projection.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using OpenToolkit.Mathematics;
+
+namespace OpenToolkit.Benchmarks.Mathematics.Vector
+{
+    [SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter]
+    [BenchmarkCategory("Vector", "Vector3", "Projection")]
+    public class Vector3Projection
+    {
+        private readonly Vector3 _vector = new Vector3(1, 2, 3);
+        private readonly Matrix4 _matrix = Matrix4.Identity;
+
+        private Matrix4 _inverse;
+        private Vector3 _result;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _inverse = _matrix;
+            _inverse.Invert();
+        }
+
+        [Benchmark(Description = "Vector3.Project(Vector3 vector, float x, float y, float width, float height, float minZ, float maxZ, Matrix4 worldViewProjection)")]
+        public void Vector3_ProjectByValue()
+        {
+            _result = Vector3.Project(_vector, -1, -1, 2, 2, -1, 1, _matrix);
+        }
+
+        [Benchmark(Description = "Vector3.Unproject(Vector3 vector, float x, float y, float width, float height, float minZ, float maxZ, Matrix4 inverseWorldViewProjection)")]
+        public void Vector3_UnprojectByValue()
+        {
+            _result = Vector3.Unproject(_vector, -1, -1, 2, 2, -1, 1, _inverse);
+        }
+    }
+}
