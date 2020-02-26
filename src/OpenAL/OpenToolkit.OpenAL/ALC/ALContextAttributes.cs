4a36ac36dce5fea238de27ff3b3485830c8905f7
Julius Häger <julius_hager@hotmail.com>
2020-02-26 01:07:09 +0100

    Added documentation to ALContextAttributes.cs

--- src/OpenAL/OpenToolkit.OpenAL/ALC/ALContextAttributes.cs
+++ src/OpenAL/OpenToolkit.OpenAL/ALC/ALContextAttributes.cs
@@ -13,24 +13,65 @@ using System.Text;
 
 namespace OpenToolkit.Audio.OpenAL
 {
+    /// <summary>
+    /// Convenience class for handling ALContext attributes.
+    /// </summary>
     public class ALContextAttributes
     {
+        /// <summary>
+        /// Gets or sets the output buffer frequency in Hz.
+        /// This does not actually change any AL state. To apply these attributes see <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/>.
+        /// </summary>
         public int? Frequency { get; set; }
 
+        /// <summary>
+        /// Gets or sets the number of mono sources.
+        /// This does not actually change any AL state. To apply these attributes see <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/>.
+        /// Not guaranteed to get exact number of mono sources when creating a context.
+        /// </summary>
         public int? MonoSources { get; set; }
 
+        /// <summary>
+        /// Gets or sets the number of stereo sources.
+        /// This does not actually change any AL state. To apply these attributes see <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/>.
+        /// Not guaranteed to get exact number of mono sources when creating a context.
+        /// </summary>
         public int? StereoSources { get; set; }
 
+        /// <summary>
+        /// Gets or sets the refrash interval in Hz.
+        /// This does not actually change any AL state. To apply these attributes see <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/>.
+        /// </summary>
         public int? Refresh { get; set; }
 
+        /// <summary>
+        /// Gets or sets if the context is synchronous.
+        /// This does not actually change any AL state. To apply these attributes see <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/>.
+        /// </summary>
         public bool? Sync { get; set; }
 
+        /// <summary>
+        /// Gets or sets additional attributes.
+        /// Will usually be the major and minor version numbers of the context. // FIXME: This needs verification. Docs say nothing about this.
+        /// </summary>
         public int[] AdditionalAttributes { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ALContextAttributes"/> class.
+        /// Leaving all attributes to the driver implementation default values.
+        /// </summary>
         public ALContextAttributes()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ALContextAttributes"/> class.
+        /// </summary>
+        /// <param name="frequency">The mixing output buffer frequency in Hz.</param>
+        /// <param name="monoSources">The number of mono sources available. Not guaranteed.</param>
+        /// <param name="stereoSources">The number of stereo sources available. Not guaranteed.</param>
+        /// <param name="refresh">The refresh interval in Hz.</param>
+        /// <param name="sync">If the context is synchronous.</param>
         public ALContextAttributes(int? frequency, int? monoSources, int? stereoSources, int? refresh, bool? sync)
         {
             Frequency = frequency;
@@ -41,7 +82,8 @@ namespace OpenToolkit.Audio.OpenAL
         }
 
         /// <summary>
-        /// Converts these context attributes to a <see cref="ALC.CreateContext(ALDevice, Span{int})"/> compatible list.
+        /// Converts these context attributes to a <see cref="ALC.CreateContext(ALDevice, int[])"/> compatible list.
+        /// Alternativly, consider using the more convenient <see cref="ALC.CreateContext(ALDevice, ALContextAttributes)"/> overload.
         /// </summary>
         /// <returns>The attibute list in the form of a span.</returns>
         public int[] CreateAttributeArray()
@@ -80,6 +122,11 @@ namespace OpenToolkit.Audio.OpenAL
             return attributeList;
         }
 
+        /// <summary>
+        /// Parses a AL attribute list.
+        /// </summary>
+        /// <param name="attributeArray">The AL context attribute list.</param>
+        /// <returns>The parsed <see cref="AlcContextAttributes"/> object.</returns>
         internal static ALContextAttributes FromArray(int[] attributeArray)
         {
             List<int> extra = new List<int>();
@@ -120,6 +167,7 @@ namespace OpenToolkit.Audio.OpenAL
             return attributes;
         }
 
+        // Used for ToString.
         private string GetOptionalString<T>(string title, T? value)
             where T : unmanaged
         {
