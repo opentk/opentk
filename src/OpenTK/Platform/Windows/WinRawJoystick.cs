c40eabd87385fbbbf7ebe5b8029c2f0a5b88a222
Tyron Madlener <tyronx@gmail.com>
2020-03-03 19:34:38 +0200

    Fix OpenTK crashing on startup when an exception is thrown during joystick initialization (which happens when there is a joystick with more than 64 buttons)

--- src/OpenTK/Platform/Windows/WinRawJoystick.cs
+++ src/OpenTK/Platform/Windows/WinRawJoystick.cs
@@ -254,36 +254,43 @@ namespace OpenTK.Platform.Windows
                 }
                 else
                 {
-                    device = new Device(handle, guid, is_xinput,
-                        is_xinput ? xinput_device_count++ : 0);
-
-                    // This is a new device, query its capabilities and add it
-                    // to the device list
-                    if (!QueryDeviceCaps(device) && !is_xinput)
+                    try
                     {
-                        continue;
-                    }
-                    device.SetConnected(true);
+                        device = new Device(handle, guid, is_xinput,
+                            is_xinput ? xinput_device_count++ : 0);
 
-                    // Check if a disconnected device with identical GUID already exists.
-                    // If so, replace that device with this instance.
-                    Device match = null;
-                    foreach (Device candidate in Devices)
-                    {
-                        if (candidate.GetGuid() == guid && !candidate.GetCapabilities().IsConnected)
+                        // This is a new device, query its capabilities and add it
+                        // to the device list
+                        if (!QueryDeviceCaps(device) && !is_xinput)
                         {
-                            match = candidate;
+                            continue;
                         }
-                    }
-                    if (match != null)
+                        device.SetConnected(true);
+
+                        // Check if a disconnected device with identical GUID already exists.
+                        // If so, replace that device with this instance.
+                        Device match = null;
+                        foreach (Device candidate in Devices)
+                        {
+                            if (candidate.GetGuid() == guid && !candidate.GetCapabilities().IsConnected)
+                            {
+                                match = candidate;
+                            }
+                        }
+                        if (match != null)
+                        {
+                            Devices.Remove(match.Handle.ToInt64());
+                        }
+
+                        Devices.Add(hardware_id, device);
+
+                        Debug.Print("[{0}] Connected joystick {1} ({2})",
+                            GetType().Name, device.GetGuid(), device.GetCapabilities());
+                    } catch (Exception e)
                     {
-                        Devices.Remove(match.Handle.ToInt64());
+                        Debug.Print("Exception thrown while trying to connect a joystick: {0}", e);
+                        device.SetConnected(false);
                     }
-
-                    Devices.Add(hardware_id, device);
-
-                    Debug.Print("[{0}] Connected joystick {1} ({2})",
-                        GetType().Name, device.GetGuid(), device.GetCapabilities());
                 }
             }
         }
