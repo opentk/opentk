e519a6360024a93d8daeb4ad7e740f7778950c07
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-05-01 09:12:39 +0200

    writer written

--- OpenToolkit.Generator/Parser.cs
+++ OpenToolkit.Generator/Parser.cs
@@ -27,26 +27,67 @@ namespace OpenToolkit.Generator
         public static Method ParseCommand(XmlNode command)
         {
             var proto = command.FirstChild;
-            var strType = proto.InnerText.Remove(proto.InnerText.IndexOf(' '));
-            var name = proto.LastChild.InnerText.Substring(2);
+            var strType = proto.SelectSingleNode("ptype")?.InnerText.Trim() ?? proto.FirstChild.InnerText.Trim();
+            var name = proto.SelectSingleNode("name").InnerText.Substring(2);
 
             var parameterNodes = command.SelectNodes("param");
             var parameters = new Parameter[parameterNodes.Count];
             for (int i = 0; i < parameterNodes.Count; i++)
             {
-                var pName = parameterNodes[i].FirstChild.InnerText;
-                var pType = parameterNodes[i].LastChild.InnerText;
-                parameters[i] = new Parameter("", pType, pName);
+                var pName = parameterNodes[i].SelectSingleNode("name").InnerText;
+
+                var pType = parameterNodes[i].SelectSingleNode("ptype")?.InnerText ?? parameterNodes[i].FirstChild.InnerText;
+                parameters[i] = new Parameter("", StringToType(pType), pName);
             }
 
-            return new Method();
+            return new Method("", StringToType(strType), name, parameters);
         }
 
         public static Type StringToType(string type)
         {
+            type = type.StartsWith("const") ? type.Substring(6) : type;
             var t = type switch
             {
-                "" => typeof(void),
+                "void" => typeof(void),
+                "void *" => typeof(void *),
+                "void **" => typeof(void **),
+                "GLenum" => typeof(uint),
+                "GLboolean" => typeof(byte),
+                "GLbitfield" => typeof(uint),
+                "GLvoid" => typeof(void),
+                "GLbyte" => typeof(sbyte),
+                "GLubyte" => typeof(byte),
+                "GLshort" => typeof(short),
+                "GLushort" => typeof(ushort),
+                "GLint" => typeof(int),
+                "GLuint" => typeof(uint),
+                "GLclampx" => typeof(int),
+                "GLsizei" => typeof(int),
+                "GLfloat" => typeof(float),
+                "GLclampf" => typeof(float),
+                "GLdouble" => typeof(double),
+                "GLclampd" => typeof(double),
+                "GLeglClientBufferEXT" => typeof(void *),
+                "GLeglImageOES" => typeof(void *),
+                "GLchar" => typeof(char),
+                "GLcharARB" => typeof(char),
+                "*GLhandleARB" => typeof(void),
+                "GLhandleARB" => typeof(uint),
+                "GLhalf" => typeof(short),
+                "GLhalfARB" => typeof(short),
+                "GLfixed" => typeof(int),
+                "GLintptr" => typeof(IntPtr),
+                "GLintptrARB" => typeof(IntPtr),
+                "GLsizeiptr" => typeof(uint), //What type?
+                "GLsizeiptrARB" => typeof(uint), //What type?
+                "GLint64" => typeof(int),
+                "GLint64EXT" => typeof(int),
+                "GLuint64" => typeof(uint),
+                "GLuint64EXT" => typeof(uint),
+                "GLsync" => typeof(uint), //What type
+                "GLhalfNV" => typeof(ushort),
+                "GLvdpauSurfaceNV" => typeof(IntPtr),
+                "GLVULKANPROCNV" => typeof(void),
                 _ => null
             };
 
