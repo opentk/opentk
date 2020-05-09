79825fb47f306a6481a23b6a2ebd3229d4e9be46
Frederik Juhl Andreasen <fjaplv64@gmail.com>
2020-05-09 16:54:56 +0200

    moved stuff aroundd

---
+++ src/OpenToolkit.Graphics@Deprecated@/DummyTypes.cs
@@ -0,0 +1,25 @@
+using System;
+namespace OpenTK.Graphics.GL
+{
+    
+    public struct CombinerPortionNV {}
+    public struct DebugProcAmd {}
+    public struct DebugProcArb {}
+    public struct DebugProcKhr {}
+    public struct ElementPointerTypeATI {}
+    public struct FragmentLightParameterSGIX {}
+    public struct IglooFunctionSelectSGIX {}
+    public struct IndexFunctionEXT {}
+    public struct MapTypeNV {}
+    public struct MatrixIndexPointerTypeARB {}
+    public struct ProgramFormat {}
+    public struct ProgramStringProperty {}
+    public struct ProgramTarget {}
+    public struct ReplacementCodeTypeSUN {}
+    public struct SecondaryColorPointerTypeIBM {}
+    public struct Struct_cl_context {}
+    public struct Struct_cl_event {}
+    public struct VertexShaderWriteMaskEXT {}
+    public struct VertexWeightPointerTypeEXT {}
+    public struct WeightPointerTypeARB {}
+}
