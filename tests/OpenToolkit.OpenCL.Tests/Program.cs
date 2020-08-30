39d1aa1f3ad9e0f7fa59eb553b9c9243c6f66236
nepnet <catalinbarbuta14@gmail.com>
2020-08-30 18:42:21 +0300

    Some minor changes on the example for opencl

--- tests/OpenToolkit.OpenCL.Tests/Program.cs
+++ tests/OpenToolkit.OpenCL.Tests/Program.cs
@@ -18,98 +18,98 @@ namespace OpenToolkit.OpenCL.Tests
 				CL.GetDeviceIds(platformId, DeviceType.All, out IntPtr[] deviceIds);
 
 				IntPtr context = CL.CreateContext(IntPtr.Zero, (uint)deviceIds.Length, deviceIds, IntPtr.Zero,
-					IntPtr.Zero,
-					out CLResultCode result);
+					IntPtr.Zero, out CLResultCode result);
 				if (result != CLResultCode.Success)
 				{
 					throw new Exception("The context couldn't be created.");
 				}
-				else
-				{
-					string code = @"
+
+				string code = @"
                 __kernel void add(__global float* A, __global float* B,__global float* result)
                 {
                     int i = get_global_id(0);
                     result[i] = A[i] + B[i];
                 }";
 
-					IntPtr program = CL.CreateProgramWithSource(context, code, out result);
-					CL.BuildProgram(program, 0, null, null, IntPtr.Zero, IntPtr.Zero);
+				IntPtr program = CL.CreateProgramWithSource(context, code, out result);
+				CL.BuildProgram(program, (uint)deviceIds.Length, deviceIds, null, IntPtr.Zero, IntPtr.Zero);
 
-					IntPtr kernel = CL.CreateKernel(program, "add", out result);
+				IntPtr kernel = CL.CreateKernel(program, "add", out result);
 
-					int arraySize = 2000;
-					float[] A = new float[arraySize];
-					float[] B = new float[arraySize];
+				int arraySize = 2000;
+				float[] A = new float[arraySize];
+				float[] B = new float[arraySize];
 
-					for (int i = 0; i < arraySize; i++)
+				for (int i = 0; i < arraySize; i++)
+				{
+					A[i] = i + 1;
+					B[i] = i;
+				}
+
+				IntPtr bufferA = CL.CreateBuffer(context, MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, A,
+					out result);
+				IntPtr bufferB = CL.CreateBuffer(context, MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, B,
+					out result);
+				IntPtr resultBuffer = CL.CreateBuffer(context, MemoryFlags.WriteOnly,
+					new UIntPtr((uint)(arraySize * sizeof(float))), IntPtr.Zero, out result);
+				UIntPtr bufferSize = (UIntPtr)UIntPtr.Size;
+
+				try
+				{
+					GCHandle inputA = GCHandle.Alloc(bufferA, GCHandleType.Pinned);
+					GCHandle inputB = GCHandle.Alloc(bufferB, GCHandleType.Pinned);
+					GCHandle resultGC = GCHandle.Alloc(resultBuffer, GCHandleType.Pinned);
+					CL.SetKernelArg(kernel, 0, bufferSize, inputA.AddrOfPinnedObject());
+					CL.SetKernelArg(kernel, 1, bufferSize, inputB.AddrOfPinnedObject());
+					CL.SetKernelArg(kernel, 2, bufferSize, resultGC.AddrOfPinnedObject());
+
+					// IntPtr commandQueue = CL.CreateCommandQueue(context, deviceIds[0], 0, out result);
+					IntPtr commandQueue =
+						CL.CreateCommandQueueWithProperties(context, deviceIds[0], IntPtr.Zero, out result);
+
+					CL.EnqueueNDRangeKernel(commandQueue, kernel, 1, null, new IntPtr[] {new IntPtr(A.Length)},
+						null, 0, null,  out IntPtr eventHandle);
+
+					CL.Finish(commandQueue);
+
+					CL.SetEventCallback(eventHandle, CommandExecutionStatus.Complete, (waitEvent, data) =>
 					{
-						A[i] = i;
-						B[i] = i;
-					}
-
-					IntPtr bufferA = CL.CreateBuffer(context, MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, A,
-						out result);
-					IntPtr bufferB = CL.CreateBuffer(context, MemoryFlags.ReadOnly | MemoryFlags.CopyHostPtr, B,
-						out result);
-					IntPtr resultBuffer = CL.CreateBuffer(context, MemoryFlags.WriteOnly,
-						new UIntPtr((uint)(arraySize * sizeof(float))), IntPtr.Zero, out result);
-					UIntPtr bufferSize = (UIntPtr)UIntPtr.Size;
-
-					try
-					{
-						GCHandle inputA = GCHandle.Alloc(bufferA, GCHandleType.Pinned);
-						GCHandle inputB = GCHandle.Alloc(bufferB, GCHandleType.Pinned);
-						GCHandle resultGC = GCHandle.Alloc(resultBuffer, GCHandleType.Pinned);
-						CL.SetKernelArg(kernel, 0, bufferSize, inputA.AddrOfPinnedObject());
-						CL.SetKernelArg(kernel, 1, bufferSize, inputB.AddrOfPinnedObject());
-						CL.SetKernelArg(kernel, 2, bufferSize, resultGC.AddrOfPinnedObject());
-
-						// IntPtr commandQueue = CL.CreateCommandQueue(context, deviceIds[0], 0, out result);
-						IntPtr commandQueue =
-							CL.CreateCommandQueueWithProperties(context, deviceIds[0], IntPtr.Zero, out result);
-
-						CL.EnqueueNDRangeKernel(commandQueue, kernel, 1, null, new IntPtr[] {new IntPtr(A.Length)},
-							null, 0, null, out _);
-
-
 						CL.EnqueueReadBuffer(commandQueue, resultBuffer, false, UIntPtr.Zero, arraySize,
-							out float[] resultValues, 0, null, out IntPtr eventHandle);
+							out float[] resultValues, 0, null, out _);
 
-						CL.SetEventCallback(eventHandle, CommandExecutionStatus.Complete, (waitEvent, data) =>
+						StringBuilder line = new StringBuilder();
+						foreach (float res in resultValues)
 						{
-							StringBuilder line = new StringBuilder();
-							foreach (float res in resultValues)
-							{
-								line.Append(res);
-								line.Append(", ");
-							}
+							line.Append(res);
+							line.Append(", ");
+						}
 
-							Console.WriteLine(line.ToString());
-						});
+						Console.WriteLine(line.ToString());
+					});
 
-						CL.WaitForEvents(1, new IntPtr[] {eventHandle});
-						CL.Finish(commandQueue);
+					//get rid of the buffers because we no longer need them
+					CL.ReleaseMemObject(bufferA);
+					CL.ReleaseMemObject(bufferB);
+					CL.ReleaseMemObject(resultBuffer);
+
+					//Release the program kernels and queues
+					CL.ReleaseProgram(program);
+					CL.ReleaseKernel(kernel);
+					CL.ReleaseCommandQueue(commandQueue);
+					CL.ReleaseContext(context);
+					CL.ReleaseEvent(eventHandle);
 
 
-						//get rid of the buffers because we no longer need them
-						CL.ReleaseMemObject(bufferA);
-						CL.ReleaseMemObject(bufferB);
-						CL.ReleaseMemObject(resultBuffer);
-						//release the context
-						CL.ReleaseContext(context);
+					//Get rid of the GC handles too
+					inputA.Free();
+					inputB.Free();
+					resultGC.Free();
 
-						//Get rid of the GC handles too
-						inputA.Free();
-						inputB.Free();
-						resultGC.Free();
-
-					}
-					catch (Exception e)
-					{
-						Console.WriteLine(e.ToString());
-						throw;
-					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.ToString());
+					throw;
 				}
 
 			}
