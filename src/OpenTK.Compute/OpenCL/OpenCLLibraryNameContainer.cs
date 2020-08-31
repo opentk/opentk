605c04fa9873bd6f65bcb43f423b2c5e9aa75ecc
nepnet <catalinbarbuta14@gmail.com>
2020-08-31 21:18:35 +0300

    Moved Opencl to OpenTK namespace

--- src/OpenTK.Compute/OpenCL/OpenCLLibraryNameContainer.cs
+++ src/OpenTK.Compute/OpenCL/OpenCLLibraryNameContainer.cs
@@ -1,22 +1,48 @@
-using OpenToolkit.Core.Loader;
+using System;
+using System.Runtime.InteropServices;
 
 namespace OpenTK.Compute.OpenCL
 {
-    public class OpenCLLibraryNameContainer : PlatformLibraryNameContainerBase
+    public class OpenCLLibraryNameContainer
     {
-        /// <inheritdoc />
-        public override string Linux => "libOpenCL.so.1";
+	    public string Linux => "libOpenCL.so.1";
+	    public string MacOS => "/System/Library/Frameworks/OpenCL.framework/OpenCL";
+        public string Android => Linux;
+        public string IOS => MacOS;
+        public string Windows => "OpenCL.dll";
 
-        /// <inheritdoc />
-        public override string MacOS => "/System/Library/Frameworks/OpenCL.framework/OpenCL";
-
-        /// <inheritdoc />
-        public override string Android => Linux;
-
-        /// <inheritdoc />
-        public override string IOS => MacOS;
-
-        /// <inheritdoc />
-        public override string Windows => "OpenCL.dll";
+        public string GetLibraryName()
+        {
+	        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+	        {
+		        if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("ANDROID")))
+		        {
+			        return Android;
+		        }
+		        else
+		        {
+			        return Linux;
+		        }
+	        }
+	        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+	        {
+		        return Windows;
+	        }
+	        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+	        {
+		        if (RuntimeInformation.IsOSPlatform(OSPlatform.Create("IOS")))
+		        {
+			        return IOS;
+		        }
+		        else
+		        {
+			        return MacOS;
+		        }
+	        }
+	        else
+	        {
+		        throw new NotSupportedException($"The library name couldn't be resolved for the given platform ('{RuntimeInformation.OSDescription}').");
+	        }
+        }
     }
 }
