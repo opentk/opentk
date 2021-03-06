d17607524f7e062672a8b7c74a4938153bfa6676
Julius Häger <julius_hager@hotmail.com>
2021-03-06 17:12:56 +0100

    Add temporary fix for weird spec issue

--- src/OpenTK.Graphics/OpenGL/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/GL.Manual.cs
@@ -3,6 +3,14 @@ using System.Runtime.InteropServices;
 
 namespace OpenTK.Graphics.OpenGL
 {
+    // FIXME: Remove this when it's fixed
+    // This is here because there in the gl.xml
+    // one of the paramteres for "glSampleMaskIndexedNV"
+    // is marked with a group named this, but this group is never referenced
+    // anywhere else in the file.
+    public enum SampleMaskNV
+    { }
+
     public static unsafe partial class GL
     {
         public static void ShaderSource(uint shader, string str)
