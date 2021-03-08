ad831970c082f827deb6008a2f6284f43c550945
FrederikJA <fjaplv64@gmail.com>
2021-03-08 18:59:04 +0100

    manual overloads

--- src/OpenTK.Graphics/OpenGL/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/GL.Manual.cs
@@ -17,7 +17,7 @@ namespace OpenTK.Graphics.OpenGL
         {
             IntPtr str_iptr = Marshal.StringToCoTaskMemAnsi(str);
             int length = str.Length;
-            GL.ShaderSource(shader, 1, (byte**)&str_iptr, ref length);
+            OpenGLES3.GL.ShaderSource(shader, 1, (byte**)&str_iptr, ref length);
             Marshal.FreeCoTaskMem(str_iptr);
         }
     }
