9c64e11800473fcd1adf67a468fb9cc092c93b17
FrederikJA <fjaplv64@gmail.com>
2021-03-08 18:59:04 +0100

    manual overloads

--- src/OpenTK.Graphics/OpenGL/Compatibility/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/Compatibility/GL.Manual.cs
@@ -5,6 +5,13 @@ namespace OpenTK.Graphics.OpenGL.Compatibility
 {
     public static unsafe partial class GL
     {
+        // FIXME: Remove this when it's fixed
+        // This is here because there in the gl.xml
+        // one of the paramteres for "glSampleMaskIndexedNV"
+        // is marked with a group named this, but this group is never referenced
+        // anywhere else in the file.
+        public enum SampleMaskNV
+        { }
         public static void ShaderSource(uint shader, string str)
         {
             IntPtr str_iptr = Marshal.StringToCoTaskMemAnsi(str);
