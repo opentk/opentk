00ee08606a2108f8fb8e4d7e841171d597d6503d
deccer <decccer@outlook.com>
2021-05-03 20:17:39 +0400

    Revert "Use proper marshaller when dealing with string arrays"
    This reverts commit 61fbc848db93c03127b188a304a4eeac961fab59.

--- src/OpenTK.Graphics/OpenGL/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/GL.Manual.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Runtime.InteropServices;
-using OpenTK.Core.Native;
 
 namespace OpenTK.Graphics.OpenGL
 {
@@ -44,10 +43,10 @@ namespace OpenTK.Graphics.OpenGL
         /// <exception cref="ArgumentNullException"></exception>
         public static void CreateShaderProgram(ShaderType shaderType, string shaderText)
         {
-            var shaderTexts = new[] {shaderText};
-            var shaderTextsPtr = MarshalTk.MarshalStringArrayToPtr(shaderTexts);
-            GL.CreateShaderProgramv_(shaderType, shaderTexts.Length, (byte**)shaderTextsPtr);
-            MarshalTk.FreeStringArrayPtr(shaderTextsPtr, shaderTexts.Length);
+            var shaderTextPtr = Marshal.StringToCoTaskMemAnsi(shaderText);
+            // ReSharper disable once ArrangeStaticMemberQualifier
+            GL.CreateShaderProgramv_(shaderType, 1, (byte**)shaderTextPtr);
+            Marshal.FreeCoTaskMem(shaderTextPtr);
         }
     }
 }
