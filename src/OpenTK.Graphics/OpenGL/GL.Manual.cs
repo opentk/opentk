ec008822aba2bfdbb8a2a3b8ffbdb1541a186d9f
deccer <decccer@outlook.com>
2021-05-03 20:21:39 +0400

    Remove Resharper annotation
    I did forget to remove it after experimenting

--- src/OpenTK.Graphics/OpenGL/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/GL.Manual.cs
@@ -44,7 +44,6 @@ namespace OpenTK.Graphics.OpenGL
         public static void CreateShaderProgram(ShaderType shaderType, string shaderText)
         {
             var shaderTextPtr = Marshal.StringToCoTaskMemAnsi(shaderText);
-            // ReSharper disable once ArrangeStaticMemberQualifier
             GL.CreateShaderProgramv_(shaderType, 1, (byte**)shaderTextPtr);
             Marshal.FreeCoTaskMem(shaderTextPtr);
         }
