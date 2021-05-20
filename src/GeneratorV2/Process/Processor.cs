786f599ca111c986ff74df9a5ebc90c31a4d8b3c
FrederikJA <fjaplv64@gmail.com>
2021-05-20 14:01:12 +0200

    move overloads into overloads.cs

--- src/GeneratorV2/Process/Processor.cs
+++ src/GeneratorV2/Process/Processor.cs
@@ -9,7 +9,6 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using GeneratorV2.Process.Overloaders;
 
 namespace GeneratorV2.Process
 {
