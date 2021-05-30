c81849610c8fd0976b6c7477ce4553d6948079c6
FrederikJA <fjaplv64@gmail.com>
2021-05-30 10:58:46 +0200

    remove fixme

--- src/OpenTK.Graphics/OpenGLES1/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGLES1/GL.Manual.cs
@@ -6,7 +6,7 @@ namespace OpenTK.Graphics.OpenGLES1
 {
     public static unsafe partial class GL
     {
-        // FIXME: Right now this is the only method that actually takes a color besides a few FFP methods.
+        // Right now this is the only method that actually takes a color besides a few FFP methods.
         // So currently its not worth it creating an overloader for these.
         // I also doubt there will ever be created new methods that take in a color.
         // 30-05-2021 FrederikJA
