078e5f807bc49b31115d39a2dea92f41e9277d0b
FrederikJA <fjaplv64@gmail.com>
2021-05-30 10:54:16 +0200

    clearcolor overloads

--- src/OpenTK.Graphics/OpenGLES1/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGLES1/GL.Manual.cs
@@ -1,9 +1,18 @@
 using System;
 using System.Runtime.InteropServices;
+using OpenTK.Mathematics;
 
 namespace OpenTK.Graphics.OpenGLES1
 {
     public static unsafe partial class GL
     {
+        // FIXME: Right now this is the only method that actually takes a color besides a few FFP methods.
+        // So currently its not worth it creating an overloader for these.
+        // I also doubt there will ever be created new methods that take in a color.
+        // 30-05-2021 FrederikJA
+        public static void ClearColor(Color4<Rgba> clearColor)
+        {
+            GL.ClearColor(clearColor.X, clearColor.Y, clearColor.Z, clearColor.W);
+        }
     }
 }
