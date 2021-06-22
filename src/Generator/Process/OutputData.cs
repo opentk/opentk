f0338112d5ba88744490197342cfdcc3fa0b1a95
FrederikJA <fjaplv64@gmail.com>
2021-06-22 22:28:38 +0200

    Rework the indented text writer to use System.CodeDom.Compiler version with extensions.

--- src/Generator/Process/OutputData.cs
+++ src/Generator/Process/OutputData.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics;
-using Generator.Utility;
 using Generator.Parsing;
+using System.CodeDom.Compiler;
 
 namespace Generator.Writing
 {
