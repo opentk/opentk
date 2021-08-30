6e3580a3526b7165760362457df80ab32f87ffdd
H. Utku Maden <utkumaden@hotmail.com>
2021-08-30 12:45:38 +0300

    Add icon component interface.

---
+++ src/OpenTK.Core/Platform/IIconComponent.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+
+namespace OpenTK.Core.Platform
+{
+    /// <summary>
+    /// Interface for PAL drivers which provide the icon component.
+    /// </summary>
+    public interface IIconComponent : IPalComponent
+    {
+        /// <summary>
+        /// True if icon objects can be loaded from files.
+        /// </summary>
+        bool CanLoadFile { get; }
+
+        /// <summary>
+        /// True if icon objects can be populated from common system icons.
+        /// </summary>
+        bool CanLoadSystemIcon { get; }
+
+        /// <summary>
+        /// True if icon objects can have mip maps.
+        /// </summary>
+        bool HasMipmaps { get; }
+
+        /// <summary>
+        /// Create an icon object.
+        /// </summary>
+        /// <returns>Handle to an icon object.</returns>
+        IconHandle Create();
+
+        /// <summary>
+        /// Destroy an icon object.
+        /// </summary>
+        /// <param name="handle">Handle to the icon object to destroy.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> is null.</exception>
+        void Destroy(IconHandle handle);
+
+        /// <summary>
+        /// Create the mipmap levels of an icon object if not already done.
+        /// </summary>
+        /// <param name="handle">Handle to an icon object.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> is null.</exception>
+        /// <exception cref="PalNotImplementedException">Driver does not provide mipmaps. See <see cref="HasMipmaps"/>.</exception>
+        void GenerateMipmaps(IconHandle handle);
+
+        /// <summary>
+        /// Load an icon object with image data.
+        /// </summary>
+        /// <param name="handle">Handle to an icon object.</param>
+        /// <param name="data">Image data to load into icon.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> is null.</exception>
+        void Load(IconHandle handle, ReadOnlySpan<byte> data);
+
+        /// <summary>
+        /// Load an icon object with image data.
+        /// </summary>
+        /// <param name="handle">Handle to an icon object.</param>
+        /// <param name="data">Image data to load into icon.</param>
+        /// <param name="level">Mip level to load data onto.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> is null.</exception>
+        /// <exception cref="PalNotImplementedException">Driver does not provide mipmaps. See <see cref="HasMipmaps"/>.</exception>
+        void Load(IconHandle handle, ReadOnlySpan<byte> data, int level);
+
+        /// <summary>
+        /// Load an icon object from a file.
+        /// </summary>
+        /// <param name="handle">Handle to an icon object.</param>
+        /// <param name="file">Path to the icon file.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> or <paramref name="file"/> is null.</exception>
+        /// <exception cref="FileNotFoundException">Icon file not found.</exception>
+        /// <exception cref="IOException">Generic IO error.</exception>
+        /// <exception cref="PalNotImplementedException">Driver does not implement loading from files. See <see cref="CanLoadFile"/>.</exception>
+        void Load(IconHandle handle, string file);
+
+        /// <summary>
+        /// Load an icon object from a stream.
+        /// </summary>
+        /// <param name="handle">Handle to icon object.</param>
+        /// <param name="stream">Stream containing icon file.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> or <paramref name="stream"/> is null.</exception>
+        /// <exception cref="FileNotFoundException">Icon file not found.</exception>
+        /// <exception cref="IOException">Generic IO error.</exception>
+        /// <exception cref="PalNotImplementedException">Driver does not implement loading from files. See <see cref="CanLoadFile"/>.</exception>
+        /// <remarks>If the icon cannot be loaded from an open file or stream handle, the driver should implement this function through a temporary file.</remarks>
+        void Load(IconHandle handle, Stream stream);
+
+        /// <summary>
+        /// Load a common system icon.
+        /// </summary>
+        /// <param name="handle">Handle to an icon object.</param>
+        /// <param name="name">To be replaced by an enumeration.</param>
+        /// <exception cref="ArgumentException"><paramref name="handle"/> is null.</exception>
+        /// <exception cref="Exception">System does not provide an icon for the given value of <paramref name="name"/>.</exception>
+        /// <exception cref="PalNotImplementedException">Driver does not implement loading system icons. See <see cref="CanLoadSystemIcon"/>.</exception>
+        void LoadSystemIcon(IconHandle handle, SystemIconType name);
+    }
+}
---
+++ src/OpenTK.Core/Platform/SystemIconType.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK.Core.Platform
+{
+    /// <summary>
+    /// Enumeration of system icons based on the Win32 base icon set.
+    /// </summary>
+    public enum SystemIconType
+    {
+        /// <summary>
+        /// Default icon for applications.
+        /// </summary>
+        Default = 1,
+
+        /// <summary>
+        /// An icon with an asterisk.
+        /// </summary>
+        Asterisk = 2,
+
+        /// <summary>
+        /// Error icon.
+        /// </summary>
+        Error = 3,
+
+        /// <summary>
+        /// Icon with an exclamation mark.
+        /// </summary>
+        Exclamation = 4,
+
+        /// <summary>
+        /// An icon with a hand.
+        /// </summary>
+        Hand = 5,
+
+        /// <summary>
+        /// An icon with an encircled letter I.
+        /// </summary>
+        Information = 6,
+
+        /// <summary>
+        /// An icon with a question mark.
+        /// </summary>
+        Question = 7,
+
+        /// <summary>
+        /// An icon related to security and super user priveleges.
+        /// </summary>
+        Shield = 8,
+
+        /// <summary>
+        /// An icon which represents a warning.
+        /// </summary>
+        Warning = 9,
+
+        /// <summary>
+        /// An icon displaying the operating system logo.
+        /// </summary>
+        OperatingSystem = 10
+    }
+}
