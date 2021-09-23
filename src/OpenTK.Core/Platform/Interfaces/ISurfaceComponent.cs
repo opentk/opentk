57d9fb18c2e71fffcb72c130bf79e844501ff1e5
H. Utku Maden <utkumaden@hotmail.com>
2021-09-23 18:23:00 +0300

    Add surface component handle and interface.

---
+++ src/OpenTK.Core/Platform/Interfaces/ISurfaceComponent.cs
@@ -0,0 +1,34 @@
+namespace OpenTK.Core.Platform
+{
+    /// <summary>
+    /// Interface for drivers which provide the surface component of the platform abstraction layer.
+    /// </summary>
+    public interface ISurfaceComponent : IPalComponent
+    {
+        /// <summary>
+        /// Create a surface object.
+        /// </summary>
+        /// <returns>A surface object.</returns>
+        SurfaceHandle Create();
+
+        /// <summary>
+        /// Destroy a surface object.
+        /// </summary>
+        /// <param name="handle">Handle to a surface.</param>
+        void Destroy(SurfaceHandle handle);
+
+        /// <summary>
+        /// Get the display the surface is in.
+        /// </summary>
+        /// <param name="handle">Handle to a surface.</param>
+        /// <returns>Handle to the display the surface is in.</returns>
+        object GetDisplay(SurfaceHandle handle);
+
+        /// <summary>
+        /// Set the display the surface is in.
+        /// </summary>
+        /// <param name="handle">Handle to a surface.</param>
+        /// <param name="display">Handle to the display to set the surface to.</param>
+        void SetDisplay(SurfaceHandle handle, object display);
+    }
+}
