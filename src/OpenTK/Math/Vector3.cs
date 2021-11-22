52e837de42610aa5b82e050a57d99cd2efcbda21
Julius Häger <julius_hager@hotmail.com>
2021-11-22 22:30:11 +0100

    Added TransformRow() methods and fixed multiplication implementations.

--- src/OpenTK/Math/Vector3.cs
+++ src/OpenTK/Math/Vector3.cs
@@ -1019,6 +1019,29 @@ namespace OpenTK
             result.Z = mat.Row2.X * vec.X + mat.Row2.Y * vec.Y + mat.Row2.Z * vec.Z;
         }
 
+        /// <summary>
+        /// Transform a Vector by the given Matrix using right-handed notation.
+        /// </summary>
+        /// <param name="vec">The vector to transform.</param>
+        /// <param name="mat">The desired transformation.</param>
+        /// <returns>The transformed vector.</returns>
+        public static Vector3 TransformRow(Vector3 vec, Matrix3 mat)
+        {
+            TransformColumn(ref mat, ref vec, out Vector3 result);
+            return result;
+        }
+
+        /// <summary>Transform a Vector by the given Matrix using right-handed notation</summary>
+        /// <param name="vec">The vector to transform</param>
+        /// <param name="mat">The desired transformation</param>
+        /// <param name="result">The transformed vector</param>
+        public static void TransformRow(ref Vector3 vec, ref Matrix3 mat, out Vector3 result)
+        {
+            result.X = vec.X * mat.Row0.X + vec.Y * mat.Row1.X + vec.Z * mat.Row2.X;
+            result.Y = vec.X * mat.Row0.Y + vec.Y * mat.Row1.Y + vec.Z * mat.Row2.Y;
+            result.Z = vec.X * mat.Row0.Z + vec.Y * mat.Row1.Z + vec.Z * mat.Row2.Z;
+        }
+
         /// <summary>Transform a Vector3 by the given Matrix, and project the resulting Vector4 back to a Vector3</summary>
         /// <param name="vec">The vector to transform</param>
         /// <param name="mat">The desired transformation</param>
@@ -1333,7 +1356,7 @@ namespace OpenTK
         public static Vector3 operator *(Vector3 vec, Matrix3 mat)
         {
             Vector3 result;
-            Vector3.Transform(ref vec, ref mat, out result);
+            Vector3.TransformRow(ref vec, ref mat, out result);
             return result;
         }
 
@@ -1346,7 +1369,7 @@ namespace OpenTK
         public static Vector3 operator *(Matrix3 mat, Vector3 vec)
         {
             Vector3 result;
-            Vector3.Transform(ref mat, ref vec, out result);
+            Vector3.TransformColumn(ref mat, ref vec, out result);
             return result;
         }
 
