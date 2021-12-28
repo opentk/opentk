79a7e44f02f24b58ee20bcbc069231f4da127f86
Christopher Lees <leezer3@gmail.com>
2021-12-28 20:55:55 +0000

    Change: Style tweak

--- src/OpenTK/Platform/DisplayDeviceBase.cs
+++ src/OpenTK/Platform/DisplayDeviceBase.cs
@@ -39,13 +39,15 @@ namespace OpenTK.Platform
         // Gets the DisplayDevice that corresponds to the specified index.
         public virtual DisplayDevice GetDisplay (DisplayIndex index)
         {
-            if (index == DisplayIndex.Primary) {
+            if (index == DisplayIndex.Primary) 
+            {
                 return Primary;
-            } else if ((int)index >= 0 && (int)index < AvailableDevices.Count) {
+            } 
+            if ((int)index >= 0 && (int)index < AvailableDevices.Count) 
+            {
                 return AvailableDevices [(int)index];
-            } else {
-                return null;
-            }
+            } 
+            return null;
         }
     }
 }
