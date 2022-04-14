b58f05ef02fa99a8c3f7b0652d2e69924690a5ee
Julius Häger <julius_hager@hotmail.com>
2022-04-14 18:37:03 +0200

    Add proper check for initializing GLFW from the main thread.

--- src/OpenTK.Windowing.Desktop/GLFWProvider.cs
+++ src/OpenTK.Windowing.Desktop/GLFWProvider.cs
@@ -8,6 +8,8 @@
 //
 
 using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Threading;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
@@ -16,7 +18,7 @@ namespace OpenTK.Windowing.Desktop
     /// <summary>
     /// Singleton providing easy GLFW implementation access.
     /// </summary>
-    internal static class GLFWProvider
+    public static class GLFWProvider
     {
         private static readonly GLFWCallbacks.ErrorCallback ErrorCallback =
             (errorCode, description) => throw new GLFWException(description, errorCode);
@@ -29,16 +31,48 @@ namespace OpenTK.Windowing.Desktop
         /// <summary>
         /// Gets a value indicating whether the <see cref="Thread.CurrentThread"/> is the same as the GLFW main thread.
         /// </summary>
-        public static bool IsOnMainThread => _mainThread == Thread.CurrentThread;
+        public static bool IsOnMainThread => _mainThread?.ManagedThreadId == Thread.CurrentThread.ManagedThreadId;
 
+        /// <summary>
+        /// Whether or not to check that GLFW is initialzed on the main thread.
+        /// </summary>
+        public static bool CheckForMainThread { get; set; } = true;
+
+        /// <summary>
+        /// Makes sure GLFW is intialized.
+        /// </summary>
+        /// <exception cref="GLFWException">If this function is not called from the main thread and <see cref="CheckForMainThread"/> is true.</exception>
         public static void EnsureInitialized()
         {
-            if (_mainThread != null)
+            if (CheckForMainThread)
             {
-                return;
+                if (_mainThread == null)
+                {
+                    if (Thread.CurrentThread.IsBackground == false &&
+                        Thread.CurrentThread.IsThreadPoolThread == false &&
+                        Thread.CurrentThread.IsAlive)
+                    {
+                        MethodInfo correctEntryMethod = Assembly.GetEntryAssembly().EntryPoint;
+                        StackTrace trace = new StackTrace();
+                        StackFrame[] frames = trace.GetFrames();
+                        for (int i = frames.Length - 1; i >= 0; i--)
+                        {
+                            MethodBase method = frames[i].GetMethod();
+                            if (correctEntryMethod == method)
+                            {
+                                _mainThread = Thread.CurrentThread;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (_mainThread?.ManagedThreadId != Thread.CurrentThread.ManagedThreadId)
+                {
+                    throw new GLFWException("GLFW can only be called from the main thread!");
+                }
             }
 
-            _mainThread = Thread.CurrentThread;
             GLFW.Init();
             GLFW.SetErrorCallback(ErrorCallback);
         }
