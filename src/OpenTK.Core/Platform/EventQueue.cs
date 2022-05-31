2fc9daeca34dca3b5fae4f5da0e3a473370af8db
H. Utku Maden <utkumaden@hotmail.com>
2022-05-31 09:26:02 +0300

    [PAL-2.0] Put out new class for the global event queue stuff.

---
+++ src/OpenTK.Core/Platform/EventQueue.cs
@@ -0,0 +1,9 @@
+namespace OpenTK.Core.Platform
+{
+    /// <summary>
+    /// Global event queue for interacting with platform events.
+    /// </summary>
+    public class EventQueue
+    {
+    }
+}
