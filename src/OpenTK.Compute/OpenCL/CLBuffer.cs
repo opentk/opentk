920cd2e787019a47e2284dfe4dbeee608b605c15
Julius Häger <julius_hager@hotmail.com>
2022-11-25 14:23:28 +0100

    Suppress a lot of OpenCL warnings.

--- src/OpenTK.Compute/OpenCL/CLBuffer.cs
+++ src/OpenTK.Compute/OpenCL/CLBuffer.cs
@@ -2,40 +2,42 @@ using System;
 
 namespace OpenTK.Compute.OpenCL
 {
-	public readonly struct CLBuffer : IEquatable<CLBuffer>
-	{
-		public readonly IntPtr Handle;
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public readonly struct CLBuffer : IEquatable<CLBuffer>
+    {
+        public readonly IntPtr Handle;
 
-		public CLBuffer(IntPtr handle)
-		{
-			Handle = handle;
-		}
+        public CLBuffer(IntPtr handle)
+        {
+            Handle = handle;
+        }
 
-		public bool Equals(CLBuffer other)
-		{
-			return Handle.Equals(other.Handle);
-		}
+        public bool Equals(CLBuffer other)
+        {
+            return Handle.Equals(other.Handle);
+        }
 
-		public override bool Equals(object obj)
-		{
-			return obj is CLBuffer other && Equals(other);
-		}
+        public override bool Equals(object obj)
+        {
+            return obj is CLBuffer other && Equals(other);
+        }
 
-		public override int GetHashCode()
-		{
-			return HashCode.Combine(Handle);
-		}
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Handle);
+        }
 
-		public static bool operator ==(CLBuffer left, CLBuffer right)
-		{
-			return left.Equals(right);
-		}
+        public static bool operator ==(CLBuffer left, CLBuffer right)
+        {
+            return left.Equals(right);
+        }
 
-		public static bool operator !=(CLBuffer left, CLBuffer right)
-		{
-			return !(left == right);
-		}
+        public static bool operator !=(CLBuffer left, CLBuffer right)
+        {
+            return !(left == right);
+        }
 
-		public static implicit operator IntPtr(CLBuffer buffer) => buffer.Handle;
-	}
+        public static implicit operator IntPtr(CLBuffer buffer) => buffer.Handle;
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
--- src/OpenTK.Compute/OpenCL/CLImage.cs
+++ src/OpenTK.Compute/OpenCL/CLImage.cs
@@ -2,40 +2,42 @@ using System;
 
 namespace OpenTK.Compute.OpenCL
 {
-	public readonly struct CLImage : IEquatable<CLImage>
-	{
-		public readonly IntPtr Handle;
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    public readonly struct CLImage : IEquatable<CLImage>
+    {
+        public readonly IntPtr Handle;
 
-		public CLImage(IntPtr handle)
-		{
-			Handle = handle;
-		}
+        public CLImage(IntPtr handle)
+        {
+            Handle = handle;
+        }
 
-		public bool Equals(CLImage other)
-		{
-			return Handle.Equals(other.Handle);
-		}
+        public bool Equals(CLImage other)
+        {
+            return Handle.Equals(other.Handle);
+        }
 
-		public override bool Equals(object obj)
-		{
-			return obj is CLImage other && Equals(other);
-		}
+        public override bool Equals(object obj)
+        {
+            return obj is CLImage other && Equals(other);
+        }
 
-		public override int GetHashCode()
-		{
-			return HashCode.Combine(Handle);
-		}
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Handle);
+        }
 
-		public static bool operator ==(CLImage left, CLImage right)
-		{
-			return left.Equals(right);
-		}
+        public static bool operator ==(CLImage left, CLImage right)
+        {
+            return left.Equals(right);
+        }
 
-		public static bool operator !=(CLImage left, CLImage right)
-		{
-			return !(left == right);
-		}
+        public static bool operator !=(CLImage left, CLImage right)
+        {
+            return !(left == right);
+        }
 
-		public static implicit operator IntPtr(CLImage image) => image.Handle;
-	}
+        public static implicit operator IntPtr(CLImage image) => image.Handle;
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
