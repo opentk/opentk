304974ccc3f507a214ff277007afb72ce6dcaff2
Julius Häger <julius_hager@hotmail.com>
2022-11-29 18:43:04 +0100

    Try fix build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+# https://www.appveyor.com/docs/build-configuration/#interpreters-and-scripts
+$ErrorActionPreference = "Stop";
+
 dotnet tool restore
 dotnet paket restore
 dotnet fake run build.fsx $($args)
