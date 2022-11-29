fa12e0ee0562fe4fad46768bf339e9c63e914160
Julius Häger <julius_hager@hotmail.com>
2022-11-29 18:37:54 +0100

    Removed dotnet install from build.ps1

--- build.ps1
+++ build.ps1
@@ -1,12 +1,3 @@
-# Install .NET Core (https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-install-script)
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
-
-&(
-    [scriptblock]::Create((Invoke-WebRequest -useb 'https://dot.net/v1/dotnet-install.ps1'))
-) -Channel 6.0
-
-$env:PATH += ";%LOCALAPPDATA%\Microsoft\dotnet"
 dotnet tool restore
 dotnet paket restore
 dotnet fake run build.fsx $($args)
