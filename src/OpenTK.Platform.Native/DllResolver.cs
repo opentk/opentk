0ba31647324eb09c8e32a6521bb5033ceafe0241
H. Utku Maden <utkumaden@hotmail.com>
2023-01-03 11:22:12 +0300

    Remove commented out code.

--- src/OpenTK.Platform.Native/DllResolver.cs
+++ src/OpenTK.Platform.Native/DllResolver.cs
@@ -22,17 +22,6 @@ namespace OpenTK.Platform.Native
 
         public static IntPtr DllImportResolver(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
         {
-            // if (OperatingSystem.IsLinux() && libraryName == "GL")
-            // {
-            //     // On linux we want to try libGL.so and libGL.so.1
-            //     IntPtr lib;
-            //     if (NativeLibrary.TryLoad("libGL.so", assembly, searchPath, out lib))
-            //         return lib;
-
-            //     if (NativeLibrary.TryLoad("libGL.so.1", assembly, searchPath, out lib))
-            //         return lib;
-            // }
-
             if (OperatingSystem.IsLinux() && LinuxLibraryList.TryGetValue(libraryName, out IReadOnlyCollection<string>? names))
             {
                 foreach(string name in names)
