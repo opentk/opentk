f2c430beedcc2d9f96995e4b5b5564346598a9ee
Julius Häger <julius_hager@hotmail.com>
2023-03-18 21:59:19 +0100

    Enable getting delta time in OnResize and OnMove

--- tests/LocalTest/Program.cs
+++ tests/LocalTest/Program.cs
@@ -51,12 +51,20 @@ namespace LocalTest
 
         }
 
+        float time = 0;
+
         protected override void OnRenderFrame(FrameEventArgs args)
         {
             base.OnRenderFrame(args);
 
-            GL.ClearColor(Color4.Coral);
+            time += (float)args.Time;
+            if (time > 8) time = 0;
+
+            Color4 color = Color4.FromHsv(new Vector4(time / 8f, 1, 1, 1));
+
+            GL.ClearColor(color);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
 
             SwapBuffers();
         }
@@ -64,8 +72,16 @@ namespace LocalTest
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
             base.OnUpdateFrame(args);
+        }
 
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+        }
 
+        protected override void OnMove(WindowPositionEventArgs e)
+        {
+            base.OnMove(e);
         }
     }
 }
