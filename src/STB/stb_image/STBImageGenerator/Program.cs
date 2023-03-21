d35e1b8f92ddf4429bb8659d2ce912d63c1663f2
khhs167 <khhs1671@gmail.com>
2023-03-21 07:20:05 +0100

    Began work on STB, I think...

---
+++ src/STB/stb_image/STBImageGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace STBImageGenerator
+{
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
