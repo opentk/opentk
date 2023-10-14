90d07f18c7c6f95d2d9fc8b3d55a94e87cccaeab
Diogo Silva <46566693+ActualDio@users.noreply.github.com>
2023-10-14 14:28:12 +0100

    Added some more documentation to the CL namespace

--- src/OpenTK.Compute/OpenCL/CL.cs
+++ src/OpenTK.Compute/OpenCL/CL.cs
@@ -19,8 +19,10 @@ namespace OpenTK.Compute.OpenCL
         #region Platform API
 
         /// <summary>
-        /// Introduced in OpenCL 1.0
-        /// See more: https://www.khronos.org/registry/OpenCL/sdk/2.2/docs/man/html/clGetPlatformIDs.html
+        /// Introduced in OpenCL 1.0.
+        /// Used to query the machine for the available platforms (Intel, AMD, Nvidia, etc...).
+        /// Displayed documentation is for OpenCL 3.0.
+        /// See more: https://www.khronos.org/registry/OpenCL/sdk/2.2/docs/man/html/clGetPlatformIDs.html.
         /// </summary>
         /// <param name="numberOfEntries">number of <see cref="CLPlatform"/> entries that can be added to platforms. If platforms is not NULL, numberOfEntries must be greater than zero.</param>
         /// <param name="platforms">returns a list of OpenCL platforms found. The CLPlatform values returned in platforms can be used to identify a specific OpenCL platform. If platforms is NULL, this argument is ignored. The number of OpenCL platforms returned is the minimum of the value specified by numberOfEntries or the number of OpenCL platforms available.</param>
@@ -35,10 +37,10 @@ namespace OpenTK.Compute.OpenCL
             [Out] out uint numberOfPlatforms);
 
         /// <summary>
-        /// Introduced in OpenCL 1.0
+        /// Introduced in OpenCL 1.0.
         /// </summary>
-        /// <param name="platformIds">returns a list of OpenCL platforms found. The CLPlatform values returned in platforms can be used to identify a specific OpenCL platform.</param>
-        /// <returns>returns Success if the function is executed successfully. Otherwise, it returns one of the following errors:
+        /// <param name="platformIds">returns a list of OpenCL platforms found. The <see cref="CLPlatform"/> values returned in platforms can be used to identify a specific OpenCL platform.</param>
+        /// <returns>returns <see cref="CLResultCode.Success"/> if the function is executed successfully. Otherwise, it returns one of the following errors:
         /// -InvalidValue if numberOfEntries is equal to zero and platforms is not NULL or if both numberOfPlatforms and platforms are NULL.
         /// -OutOfHostMemory if there is a failure to allocate resources required by the OpenCL implementation on the host.</returns>
         public static CLResultCode GetPlatformIDs(out CLPlatform[] platformIds)
@@ -51,8 +53,18 @@ namespace OpenTK.Compute.OpenCL
         }
 
         /// <summary>
-        /// Introduced in OpenCL 1.0
+        /// Introduced in OpenCL 1.0.
+        /// Used to query a specific platform for a specified piece of information. The information queried is defined by <see cref="PlatformInfo"/>
+        /// Displayed documentation is for OpenCL 3.0.
+        /// See more: https://registry.khronos.org/OpenCL/sdk/3.0/docs/man/html/clGetPlatformInfo.html.
         /// </summary>
+        /// <param name="platform">refers to the platform returned by <see cref="GetPlatformIds(out CLPlatform[])"/> or can be NULL. If platform is NULL, the behavior is implementation-defined.</param>
+        /// <param name="paramName"></param>
+        /// <param name="numberOfPlatforms">returns the number of OpenCL platforms available. If numberOfPlatforms is NULL, this argument is ignored.</param>
+        /// <returns>returns <see cref="CLResultCode.Success"/> if the function is executed successfully. Otherwise, it returns one of the following errors:
+        /// -InvalidValue if numberOfEntries is equal to zero and platforms is not NULL or if both numberOfPlatforms and platforms are NULL.
+        /// -OutOfHostMemory if there is a failure to allocate resources required by the OpenCL implementation on the host.</returns>
+        /// 
         [DllImport(LibName, CallingConvention = CallingConvention, EntryPoint = "clGetPlatformInfo")]
         public static extern CLResultCode GetPlatformInfo(
             [In] CLPlatform platform,
--- src/OpenTK.Compute/OpenCL/Enums.cs
+++ src/OpenTK.Compute/OpenCL/Enums.cs
@@ -58,13 +58,47 @@ namespace OpenTK.Compute.OpenCL
 
     #region Platform
 
+    /// <summary>
+    /// The information that can be queried using <see cref="CL.GetPlatformInfo(CLPlatform, PlatformInfo, out byte[])"/>.
+    /// For more info see: https://registry.khronos.org/OpenCL/specs/3.0-unified/html/OpenCL_API.html#platform-queries-table.
+    /// </summary>
     public enum PlatformInfo : uint
     {
+        /// <summary>
+        /// OpenCL profile string. Returns the profile name supported by the implementation. The profile name returned can be one of the following strings:
+        /// FULL_PROFILE - if the implementation supports the OpenCL specification with no need for extensions.
+        /// EMBEDDED_PROFILE - if the implementation supports the OpenCL embedded profile. The embedded profile for OpenCL is described in https://www.khronos.org/registry/OpenCL/specs/3.0-unified/html/OpenCL_API.html#opencl-embedded-profile.
+        /// </summary>
         Profile = 0x0900,
+
+        /// <summary>
+        /// OpenCL version string. Returns the OpenCL version supported by the implementation. This version string has the following format:
+        /// "OpenCL {major_version.minor_version} {platform-specific information}"
+        /// The major_version.minor_version value returned will be one of 1.0, 1.1, 1.2, 2.0, 2.1, 2.2 or 3.0.
+        /// </summary>
         Version = 0x0901,
+
+        /// <summary>
+        /// Platform name string.
+        /// </summary>
         Name = 0x0902,
+
+        /// <summary>
+        /// Platform vendor string.
+        /// </summary>
         Vendor = 0x0903,
+
+        /// <summary>
+        /// Returns a space separated list of extension names (the extension names themselves do not contain any spaces) supported by the platform. Each extension that is supported by all devices associated with this platform must be reported here.
+        /// </summary>
         Extensions = 0x0904,
+
+        /// <summary>
+        /// Introduced in version 2.1.
+        /// Returns the resolution of the host timer in nanoseconds as used by <see cref="CL.GetHostTimer(CLDevice, IntPtr)"/>.
+        /// This value must be 0 for devices that do not support device and host timer synchronization.
+        /// </summary>
+        PlatformHostTimerResolution = 0x0905,
         HostTimerResolution = 0x0905,
         NumericVersion = 0x0906,
         ExtensionsWithVersion = 0x0907,
