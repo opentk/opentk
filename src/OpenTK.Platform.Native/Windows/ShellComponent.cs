4bb6d192f7f37ca6781705e5c76b261a28a9c1b2
Julius Häger <julius_hager@hotmail.com>
2023-10-14 15:53:26 +0200

    Add comment

--- src/OpenTK.Platform.Native/Windows/ShellComponent.cs
+++ src/OpenTK.Platform.Native/Windows/ShellComponent.cs
@@ -131,6 +131,7 @@ namespace OpenTK.Platform.Native.Windows
         }
 
         // FIXME: Move this to the window component itself?
+        // FIXME: DWMWA_BORDER_COLOR? DWMWA_WINDOW_CORNER_PREFERENCE?
         /// <summary>
         /// Sets the <c>DWMWA_USE_IMMERSIVE_DARK_MODE</c> flag on the window causing the titlebar be rendered in dark mode colors.
         /// </summary>
