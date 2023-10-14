ef1a7d5c4cad87e7084a15ff2a9f6ccdcacabcac
Julius Häger <julius_hager@hotmail.com>
2023-10-15 00:00:39 +0200

    Try other error reporting method in appveyor

--- build.ps1
+++ build.ps1
@@ -1,8 +1,13 @@
 # https://www.appveyor.com/docs/build-configuration/#interpreters-and-scripts
-$ErrorActionPreference = "Stop";
+# $ErrorActionPreference = "Stop";
 # This causes any failure inside of build.fsx to cause things to stop. 
 # This is not great as fake tends to print quite a few error messages.
 
 dotnet tool restore
 dotnet paket restore
 dotnet run --project ./build/build.fsproj $($args)
+
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "dotnet run failed with: {0}" -f $LASTEXITCODE
+}
