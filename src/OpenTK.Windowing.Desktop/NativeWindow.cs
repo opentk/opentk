5beeb3de5a9d20be1b90893181fd102495c246bb
Julius Häger <julius_hager@hotmail.com>
2023-12-02 17:57:36 +0100

    Remove accidental newline

--- src/OpenTK.Windowing.Desktop/NativeWindow.cs
+++ src/OpenTK.Windowing.Desktop/NativeWindow.cs
@@ -6,7 +6,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Reflection;
-
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 using System.Threading;
