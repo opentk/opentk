36ffbc23895385aa3832d1a9a961685b0232f447
Diogo Silva <46566693+ActualDio@users.noreply.github.com>
2024-02-29 12:39:35 +0000

    Added docs to ChannelType enum

--- src/OpenTK.Compute/OpenCL/Enums.cs
+++ src/OpenTK.Compute/OpenCL/Enums.cs
@@ -3716,7 +3716,7 @@ namespace OpenTK.Compute.OpenCL
 
     /// <summary>
     /// <para>
-    ///     Specifies the number of channels and the channel layout i.e. the memory layout in which channels are stored an image
+    ///     Specifies the number of channels and the channel layout i.e. the memory layout in which channels are stored an image.
     /// </para>
     /// <para>
     ///     Original documentation
@@ -3872,24 +3872,99 @@ namespace OpenTK.Compute.OpenCL
         Abgr = 0x10C3
     }
 
+    /// <summary>
+    /// <para>
+    ///     Describes the size of the channel data type.
+    /// </para>
+    /// <para>
+    ///     Original documentation
+    ///     <b><u><see href="https://registry.khronos.org/OpenCL/specs/3.0-unified/html/OpenCL_API.html#image-channel-data-types-table">here</see></u></b>.
+    /// </para>
+    /// </summary>
     public enum ChannelType : uint
     {
+        /// <summary>
+        ///     Each channel component is a normalized signed 8-bit integer value.
+        /// </summary>
         NormalizedSignedInteger8 = 0x10D0,
+
+        /// <summary>
+        ///     Each channel component is a normalized signed 16-bit integer value.
+        /// </summary>
         NormalizedSignedInteger16 = 0x10D1,
+
+        /// <summary>
+        ///     Each channel component is a normalized unsigned 8-bit integer value.
+        /// </summary>
         NormalizedUnsignedInteger8 = 0x10D2,
+
+        /// <summary>
+        ///     Each channel component is a normalized unsigned 16-bit integer value.
+        /// </summary>
         NormalizedUnsignedInteger16 = 0x10D3,
+
+        /// <summary>
+        ///     Represents a normalized 5-6-5 3-channel RGB image. The channel order must be
+        ///     <c><see cref="ChannelOrder.Rgb">Rgb</see></c> or <c><see cref="ChannelOrder.Rgbx">Rgbx</see></c>.
+        /// </summary>
         NormalizedUnsignedShortFloat565 = 0x10D4,
+
+        /// <summary>
+        ///     Represents a normalized x-5-5-5 4-channel xRGB image. The channel order must be
+        ///     <c><see cref="ChannelOrder.Rgb">Rgb</see></c> or <c><see cref="ChannelOrder.Rgbx">Rgbx</see></c>.
+        /// </summary>
         NormalizedUnsignedShortFloat555 = 0x10D5,
+
+        /// <summary>
+        ///     Represents a normalized x-10-10-10 4-channel xRGB image. The channel order must be
+        ///     <c><see cref="ChannelOrder.Rgb">Rgb</see></c> or <c><see cref="ChannelOrder.Rgbx">Rgbx</see></c>.
+        /// </summary>
         NormalizedUnsignedInteger101010 = 0x10D6,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized signed 8-bit integer value.
+        /// </summary>
         SignedInteger8 = 0x10D7,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized signed 16-bit integer value.
+        /// </summary>
         SignedInteger16 = 0x10D8,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized signed 32-bit integer value.
+        /// </summary>
         SignedInteger32 = 0x10D9,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized unsigned 8-bit integer value.
+        /// </summary>
         UnsignedInteger8 = 0x10DA,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized unsigned 16-bit integer value.
+        /// </summary>
         UnsignedInteger16 = 0x10DB,
+
+        /// <summary>
+        ///     Each channel component is an unnormalized unsigned 32-bit integer value.
+        /// </summary>
         UnsignedInteger32 = 0x10DC,
+
+        /// <summary>
+        ///     Each channel component is a 16-bit half-float value´.
+        /// </summary>
         HalfFloat = 0x10DD,
+
+        /// <summary>
+        ///     Each channel component is a single precision floating-point value.
+        /// </summary>
         Float = 0x10DE,
-        NormalizedUnsignedInteger24 = 0x10DF,
+
+        /// <summary>
+        ///     Represents a normalized 10-10-10-2 four-channel RGBA image.
+        ///     The channel order must be <c><see cref="ChannelOrder.Rgba">Rgba</see></c>.
+        /// </summary>
         NormalizedUnsignedInteger101010Version2 = 0x10E0
     }
 
