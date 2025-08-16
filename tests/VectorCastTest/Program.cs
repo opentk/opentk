79d090d26f576e70c164e9b312b0b1deba3dbfbb
Pale-Gray <73047946+Pale-Gray@users.noreply.github.com>
2025-08-15 22:25:32 -0400

    added explicit cast support from Vector2b to Vector2i and Vector2

--- tests/VectorCastTest/Program.cs
+++ tests/VectorCastTest/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace VectorCastTest;
+﻿using OpenTK.Mathematics;
+
+namespace VectorCastTest;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Vector2b booleanVec2 = (true, false);
+        Console.WriteLine($"The Vector2b is {booleanVec2}");
+        Console.WriteLine($"To Vector2i is {(Vector2i)booleanVec2}");
+        Console.WriteLine($"To Vector2 is {(Vector2)booleanVec2}");
     }
 }
