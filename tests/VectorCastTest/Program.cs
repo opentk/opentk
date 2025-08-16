9cdce9be2a7e3418b3ffd2824d1095ab78a5d297
Pale-Gray <73047946+Pale-Gray@users.noreply.github.com>
2025-08-15 22:29:14 -0400

    added explicit cast support from Vector3b to Vector3i and Vector3

--- tests/VectorCastTest/Program.cs
+++ tests/VectorCastTest/Program.cs
@@ -10,5 +10,10 @@ class Program
         Console.WriteLine($"The Vector2b is {booleanVec2}");
         Console.WriteLine($"To Vector2i is {(Vector2i)booleanVec2}");
         Console.WriteLine($"To Vector2 is {(Vector2)booleanVec2}");
+
+        Vector3b booleanVec3 = (true, false, true);
+        Console.WriteLine($"The Vector3b is {booleanVec3}");
+        Console.WriteLine($"To Vector3i is {(Vector3i)booleanVec3}");
+        Console.WriteLine($"To Vector3 is {(Vector3)booleanVec3}");
     }
 }
