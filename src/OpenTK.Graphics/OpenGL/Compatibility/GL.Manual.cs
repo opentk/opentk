469792a8a444b9892a6b3ff210d2a1a2482a30a2
Julius Häger <julius_hager@hotmail.com>
2025-12-07 15:16:29 +0100

    Add GetShaderInfoLog/GetProgramInfoLog overload. Add ClipOrigin and ClipDepthMode to the GetPName enum.

--- src/OpenTK.Graphics/OpenGL/Compatibility/GL.Manual.cs
+++ src/OpenTK.Graphics/OpenGL/Compatibility/GL.Manual.cs
@@ -42,6 +42,15 @@ namespace OpenTK.Graphics.OpenGL.Compatibility
         }
 
         /// <summary>
+        /// This is a convenience function that calls <see cref="GL.GetShaderi(int, ShaderParameterName, out int)"/> followed by <see cref="GL.GetShaderInfoLog(int, int, out int, out string)"/>.
+        /// </summary>
+        public static string GetShaderInfoLog(int shader)
+        {
+            GetShaderInfoLog(shader, out string info);
+            return info;
+        }
+
+        /// <summary>
         /// This is a convenience function that calls <see cref="GL.GetProgrami(int, ProgramProperty, out int)"/> followed by <see cref="GL.GetProgramInfoLog(int, int, out int, out string)"/>.
         /// </summary>
         public static void GetProgramInfoLog(int program, out string info)
@@ -55,6 +64,15 @@ namespace OpenTK.Graphics.OpenGL.Compatibility
             {
                 GL.GetProgramInfoLog(program, length, out length, out info);
             }
+        }
+
+        /// <summary>
+        /// This is a convenience function that calls <see cref="GL.GetProgrami(int, ProgramProperty, out int)"/> followed by <see cref="GL.GetProgramInfoLog(int, int, out int, out string)"/>.
+        /// </summary>
+        public static string GetProgramInfoLog(int program)
+        {
+            GetProgramInfoLog(program, out string info);
+            return info;
         }
 
         /// <inheritdoc cref="CreateShaderProgramv(ShaderType, int, byte**)"/>
